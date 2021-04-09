a295b6e2902bc6ec9542aa0b16deda537587cf21
guinhoanjos <iago.anjos@hotmail.com>
2021-04-09 16:23:42 -0300

    criado pixel de outra forma

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-function createDivBlockInline (number, local) {
-    for(let index = 0; index < number; index += 1) {
-        let createdDiv = document.createElement('div');
-        createdDiv.className = "pixels";
-        local.appendChild(createdDiv);
+function createDiv(number) {
+  for (let index = 0; index < number; index += 1) {
+    let createdDiv = document.createElement("div");
+    createdDiv.className = "pixels";
+    document.getElementById("pixel-board").appendChild(createdDiv)
     }
 }
-let boardPixelLocate = document.getElementById("pixel-board");
-
-function createDivBlockParent (number2, local2) {
-    for(let index2 = 0; index2 < number2; index2 += 1) {
-        let createdDiv2 = document.createElement('div');
-        createdDiv2.className = "pixel-parent";
-        local2.appendChild(createdDiv2);
-        let pixelBoardparent = document.querySelectorAll(".pixel-parent")[index2];
-        createDivBlockInline(5, pixelBoardparent);
-    }
-}
-createDivBlockParent(5, boardPixelLocate)
+createDiv(25);
