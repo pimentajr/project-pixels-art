20531e5a90d65f812a23f165488fd94dcb0d1a40
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-09 16:44:06 -0300

    Feito requisito obrigatório 4

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const colorDivs = document.querySelectorAll('.color');
-
+const pixelBoard = document.querySelector('#pixel-board');
+// Requisito 2
 function setColorToPalete() {
   for (let index = 0; index < colorDivs.length; index += 1) {
     const colorDivsIDValue = colorDivs[index].id;
@@ -7,3 +8,16 @@ function setColorToPalete() {
   }
 }
 setColorToPalete();
+// Requisito 4
+let pixelBoardSize = 5;
+
+for (let lineIndex = 1; lineIndex <= pixelBoardSize; lineIndex += 1){
+  const createColumnDivs = document.createElement('div');
+  createColumnDivs.className = 'column-pixel';
+  for (let columnIndex = 1; columnIndex <= pixelBoardSize; columnIndex += 1) {
+    const createDivs = document.createElement('div');
+    createDivs.className = 'pixel';
+    createColumnDivs.appendChild(createDivs);
+  }
+  pixelBoard.appendChild(createColumnDivs);
+}
