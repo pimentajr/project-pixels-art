1ffe381f06ad414308cfc14dabff12d6ecc13ee3
rubia braga <rubiaemilly29@gmail.com>
2021-04-09 18:52:46 -0300

    requesito 4

---
+++ script.js
@@ -0,0 +1,18 @@
+function createColunnLines() {
+    
+    for (let index = 0; index < 5; index++) {
+        let colunn = document.createElement('div');
+        colunn.className = 'colunn'
+        document.querySelector('#pixel-board').appendChild(colunn)
+        let table = document.querySelectorAll('.colunn')
+
+        for (let index = 0; index < 5; index++) {
+            let lines = document.createElement('div');
+            lines.className = 'pixel';
+            colunn.appendChild(lines)
+            
+        }
+    }
+
+    
+}createColunnLines()
