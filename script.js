8af88a2c6c1e440986b34132ae798786ac473a98
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-09 14:47:52 -0300

    adiciona título

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+let elementH1 = document.createElement('h1');
+elementH1.id = 'title';
+elementH1.innerHTML = 'Paleta de Cores'
+document.body.appendChild(elementH1);
+}
