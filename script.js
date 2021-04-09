9d6265b17968f88531f97513449f548261cd46dc
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-04-09 17:02:20 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-window.onload = function () {
-
-  function selectColor () {
+function selectColor() {
     let blackColor = document.getElementById('black');
     blackColor.classList.add('selected');
-
 
     let colorOptions = document.getElementsByClassName('color');
     for (let index = 0; index < colorOptions.length; index += 1) {
       colorOptions[index].addEventListener('click', function (event) {
         for (let index2 = 0; index2 < colorOptions.length; index2 += 1) {
           if (colorOptions[index2].classList.contains('selected') === true) {
-            colorOptions[index2].classList.remove('selected')
+            colorOptions[index2].classList.remove('selected');
           }
           event.target.classList.add('selected');
         }
-      })
+      });
     }
-  }
-  selectColor()
-};
+    let selectedColor = document.querySelector('.selected').id;
+   
+    let pixelGrid = document.getElementById('table');
+    table.addEventListener('click', function(event) {
+    event.target.style.backgroundColor = selectedColor;
+  })
+}
+selectColor();
+
+function fillPixels() {
+  let pixelGrid = document.getElementById('table');
+  pixelGrid.addEventListener('click', function(event) {
+    event.target.style.backgroundColor = document.querySelector('.selected').id;
+  })
+}
+fillPixels()
