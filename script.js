e3930cdfaa868e34d308b2074f9e8a5a3098b7a5
Halysson2 <halyssonsena83@gmail.com>
2021-04-09 15:06:16 -0300

    resolvendo a primeira questão

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let getBody = document.querySelector('body')
-let title = document.createElement('h1')
-title.id = 'title'
-title.innerHTML = 'Paleta de Cores'
-getBody.appendChild(title)
+function titlePalet() {
+    let getBody = document.querySelector('body')
+    let title = document.createElement('h1')
+    title.id = 'title'
+    title.innerHTML = 'Paleta de Cores'
+    getBody.appendChild(title)
+
+} 
+titlePalet()
