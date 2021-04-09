d525228f44090940e38b6762ff37bfa80bd25c66
israeltonholo <israeltonholo@hotmail.com>
2021-04-09 19:48:24 -0300

    corrige lint

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.onload = function onLoadWindow() {
-  const divSelected = document.getElementById('color-palette');
+  let divSelected = document.getElementById('color-palette');
   let divSelectedUpdate = document.querySelector('.selected');
   let colorSelected = document.querySelector('.selected').classList[1];
   console.log(divSelectedUpdate);
   console.log(document.getElementById('color-palette'));
-  divSelected.addEventListener('click',function eventFunction(event) {
+  divSelected.addEventListener('click', function eventFunction(event) {
     divSelectedUpdate.classList.remove('selected');
     divSelectedUpdate = event.target;
     divSelectedUpdate.classList.add('selected');
     console.log(divSelectedUpdate);
     console.log(document.getElementById('color-palette'));
     colorSelected = divSelectedUpdate.classList[1];
-  })
-}
+  });
+};
