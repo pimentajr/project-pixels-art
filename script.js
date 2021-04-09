04ccd5764f8ac635aed5bf369f99543eeecbaa2f
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-09 17:38:04 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onload = function() {
+window.onload = function () {
   function primaryColors(colors) {
-    let colorBox = document.querySelectorAll('.color');
+    const colorBox = document.querySelectorAll('.color');
 
     for (let index = 0; index < colors.length; index += 1) {
       colorBox[index].style.backgroundColor = colors[index];
@@ -9,4 +9,21 @@ window.onload = function() {
 
   primaryColors(['black', 'yellow', 'green', 'blue']);
 
+  function pixelsTable (width, height) {
+    const tableWidth = width;
+    const tableHeight = height;
+    const table = document.querySelector('#pixel-board');
+    for (let index = 1; index <= height; index += 1) {
+      let newColumn = document.createElement('tr');
+      for (let index2 = 1; index2 <= width; index2 += 1) {
+        let newLine = document.createElement('td');
+        newLine.className = 'pixel';
+        newColumn.appendChild(newLine);
+      }
+      table.appendChild(newColumn);
+    }
+  }
+
+  pixelsTable (5, 5);
+  console.log(document.body);
 }
