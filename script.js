33f52821d140ebf38df8f794e9e3cf8f840e37ee
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-09 16:35:41 -0300

    cria uma tablela de 5x5 embaixo da paleta de cores

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+const getBody = document.body;
+const makeTable = document.createElement('table');
+getBody.appendChild(makeTable);
+makeTable.id = 'pixel-board';
+
+for (let index = 0; index < 5; index += 1) {
+  const makeTr = document.createElement('tr');
+  makeTable.appendChild(makeTr);
+  for (let jndex = 0; jndex < 5; jndex += 1) {
+    const makeTd = document.createElement('td');
+    makeTr.appendChild(makeTd);
+    makeTd.className = 'pixel branco';
+  }
+}
 // npm run lint
 // npm run lint:styles
