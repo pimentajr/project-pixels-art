19c7fc5c7a51a931dcccf0fb816cf06d66c8e3fa
rubia braga <rubiaemilly29@gmail.com>
2021-04-09 20:18:09 -0300

    requesito 4 mudando css

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function createColunnLines() {
-    
-    for (let index = 0; index < 5; index++) {
-        let colunn = document.createElement('div');
-        colunn.className = 'colunn'
-        document.querySelector('#pixel-board').appendChild(colunn)
-        let table = document.querySelectorAll('.colunn')
-
-        for (let index = 0; index < 5; index++) {
+for (let index = 0; index < 5; index += 1) {
+    let colunn = document.createElement('div');
+    colunn.className = 'colunn'
+    document.querySelector('#pixel-board').appendChild(colunn)
+    let table = document.querySelectorAll('.colunn')
+        for (let index = 0; index < 5; index += 1) {
             let lines = document.createElement('div');
             lines.className = 'pixel';
             colunn.appendChild(lines)
