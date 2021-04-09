64295951ba3df47fddd5ad23ef236c3599c515ca
Renan <renan.vamo@gmail.com>
2021-04-09 15:45:41 -0300

    Seta a classe selected na div da cor ao clicar na cor da paleta

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-
-let selectedColor = document.getElementById('black-box');
-selectedColor.className += ' selected';
+let firstSelectedColor = document.getElementById('black-box');
+firstSelectedColor.classList = ('color selected');
 
 function colorSelection() {
+  let tableColor = document.querySelector("#color-palette");
+  tableColor.addEventListener('click', function(event) {
+    let actualColor = document.querySelector('.selected');
+    actualColor.className = 'color';
+    event.target.className = 'color selected';
+  })
 }
+colorSelection()
