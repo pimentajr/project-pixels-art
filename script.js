26104ab5a512ddab1e10a0605c5461fece16af38
Savio Moraes <smmsavio@hotmail.com>
2021-04-09 20:54:54 -0300

    Retira o a function do requisito 6 que afetou os requisitos 2 e 3 para refazer

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-onload = function() {
-  function changesToBlack() {
-    const blackColor = document.querySelectorAll('.color');
-    if (blackColor[0].style.backgroundColor !== 'black') {
-      blackColor[0].style.backgroundColor = 'black';
-    }
+function changesToBlack() {
+  const blackColor = document.querySelectorAll('.color');
+  if (blackColor[0].style.backgroundColor !== 'black') {
+    blackColor[0].style.backgroundColor = 'black';
   }
-  changesToBlack();
-
-  function fixedInitialSelection(color) {
-    let initialSelection = document.querySelectorAll('.color');
-    initialSelection[0].className = 'selected';
-  }
-  fixedInitialSelection();
 }
+changesToBlack();
