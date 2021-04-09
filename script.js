b66ff8430e993ecfdfac604d6e5667f3459c8734
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-04-09 18:13:19 -0300

    Adiciona classe a cor preta

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  
+  document.querySelector('#black').classList.add('selected')
+
+  // let colorSection = document.getElementById('color-palette')
+
+  // colorSection.addEventListener('click', function(event){
+  //   let element = event.target
+  //   element.classList.add('selected')
+  //   console.log(element)
+    
+  // })
+
+}
