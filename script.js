c27c0f44030be73ce8fd103494f693199cfd87a4
igordmf <igordanielmf@gmail.com>
2021-04-09 16:08:20 -0300

    Adiciona requisito 4

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// 1.
 const body = document.querySelector('body');
 const title = document.createElement('h1');
 title.setAttribute('id', 'title');
 title.innerText = 'Paleta de Cores';
 body.appendChild(title);
 
+// 2.
 const colorPalette = document.createElement('div');
 colorPalette.setAttribute('id', 'color-palette');
 body.appendChild(colorPalette);
@@ -17,5 +19,18 @@ for (let index = 1; index <= differentColors; index += 1) {
   colorPalette.appendChild(colorBox);
 }
 
+// 3.
 const firstColor = document.querySelector('.color');
 firstColor.style.backgroundColor = 'black';
+
+// 4.
+const pixelBoard = document.createElement('div');
+pixelBoard.setAttribute('id', 'pixel-board');
+body.appendChild(pixelBoard);
+const numOfPixels = 25;
+for (let index = 1; index <= numOfPixels; index += 1) {
+  const pixelBox = document.createElement('div');
+  pixelBox.classList.add('pixel');
+  pixelBox.style.backgroundColor = 'white';
+  pixelBoard.appendChild(pixelBox);
+}
