b4caa3b0dfc18aabb8ad2852b7061225d0afeffb
Hugo Ferreira <hugonardo@gmail.com>
2021-04-09 17:51:21 -0300

    Requisito 7
    7. Clicar em uma das cores da paleta faz com que ela seja selecionada e
       utilizada para preencher os pixels no quadro

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-function restoreColorSelection() {
-  const colorPallet = document.querySelector('#color-palette');
+const colorSelectedClass = 'selected';
+let colorPallet;
 
+function clearColorSelection() {
   for (let index = 0; index < colorPallet.childElementCount; index += 1) {
-    colorPallet.children[index].classList.remove('selected');
+    colorPallet.children[index].classList.remove(colorSelectedClass);
   }
+}
 
-  colorPallet.children[0]
+function restoreColorSelection() {
+  clearColorSelection();
+  colorPallet
+    .children[0]
     .classList
-    .add('selected');
+    .add(colorSelectedClass);
+}
+
+function selectColor(colorId) {
+  clearColorSelection();
+  colorPallet.querySelector(`#${colorId}`)
+    .classList
+    .add(colorSelectedClass);
+}
+
+function onClickColorPallet(event) {
+  selectColor(event.target.id);
+}
+
+function bindListeners() {
+  colorPallet.onclick = onClickColorPallet;
 }
 
 window.onload = () => {
+  colorPallet = document.querySelector('#color-palette');
   restoreColorSelection();
+  bindListeners();
 };
