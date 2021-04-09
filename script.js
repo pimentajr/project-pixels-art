22377958826fb6c428325f72e6452e47018d2277
Daniel <danielfdoficial@gmail.com>
2021-04-09 16:59:20 -0300

    atualiza o desafio 4

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+function createSquares() {
 const elemento = document.querySelectorAll('.color');
-elemento[0].style.backgroundColor = 'black';
-elemento[1].style.backgroundColor = 'yellow';
-elemento[2].style.backgroundColor = 'blue';
-elemento[3].style.backgroundColor = 'orange';
-
-const quadro = document.querySelector('#pixel-board');
-for (let index = 0; index < 26; index += 1) {
-	const elementsQ = document.createElement('div');
-	elementsQ.classList.add('pixel');
-	quadro.appendChild(elementsQ);
+	elemento[0].style.backgroundColor = 'black';
+	elemento[1].style.backgroundColor = 'yellow';
+	elemento[2].style.backgroundColor = 'blue';
+	elemento[3].style.backgroundColor = 'orange';
 }
+createSquares();
+
+function createTable() {
+	const quadro = document.querySelector('#pixel-board');
+	for (let index = 0; index < 11; index += 1) {
+		const elementsQ = document.createElement('div');
+		elementsQ.classList.add('pixel');
+		quadro.appendChild(elementsQ);
+}
+}
+createTable();
