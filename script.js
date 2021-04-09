e0a4d9553fa4a1861af5d4f1ec65c241be9991df
Guilherme Nunes <guiicnunes@gmail.com>
2021-04-09 15:47:00 -0300

    Correcoes Lint

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-window.onload = function() {
+window.onload = function () {
   function makePixelBoard(parent, line, column) {
     for (let index = 1; index <= line; index += 1) {
       let divLine = document.createElement('div');
       divLine.class = 'pixel-board-line';
-      for (let index = 1; index <= column; index += 1) {
+      for (let indexLine = 1; indexLine <= column; indexLine += 1) {
         let divPixel = document.createElement('div');
         divPixel.className = 'pixel';
-        divPixel.style.backgroundColor = 'magenta';
         divLine.appendChild(divPixel);
       }
       parent.appendChild(divLine);
@@ -15,5 +14,4 @@ window.onload = function() {
 
   let pixelBoard = document.querySelector('#pixel-board');
   makePixelBoard(pixelBoard, 5, 5);
-
-}
+};
