26f3bc44d7a51aefa49b0d0904d27a75b1079b97
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-09 19:01:14 -0300

    Correcao requisito 5

--- script.js
+++ script.js
@@ -16,10 +16,11 @@ function pixelsTable(width, height) {
   table.style.width = [width * 40, 'px'].join('');
   table.style.height = [height * 40, 'px'].join('');
   for (let index = 1; index <= height; index += 1) {
-    const newColumn = document.createElement('tr');
+    const newColumn = document.createElement('div');
+    newColumn.className = 'tr';
     for (let index2 = 1; index2 <= width; index2 += 1) {
-      const newLine = document.createElement('td');
-      newLine.className = 'pixel white';
+      const newLine = document.createElement('div');
+      newLine.className = 'pixel white td';
       newColumn.appendChild(newLine);
     }
     table.appendChild(newColumn);
