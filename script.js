a3e8dda9ce41741cd1ea8e10e14278d0e328976f
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-04-09 16:16:07 -0300

    Adiciona a classe selected ao elemento .color clicado

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
 const pixelBoard = document.getElementById('pixel-board');
 const boardLength = 25;
+let pixel = document.querySelectorAll('.pixel');
+const colorPalette = document.getElementById('color-palette');
+let colorPick = document.querySelectorAll('.color');
 
 for (let index = 0; index < boardLength; index += 1) {
-  const pixelDiv = document.createElement('div');
+  let pixelDiv = document.createElement('div');
 
   pixelDiv.className = 'pixel';
 
   pixelBoard.appendChild(pixelDiv);
 }
+
+function defaultColor() {
+  document.querySelector('.black').classList.add('selected');
+}
+
+defaultColor();
+
+let selectedColor;
+
+colorPalette.addEventListener('click', function (event) {
+  selectedColor = document.querySelectorAll('.selected');
+
+  if (event.target.classList.contains('color')) {
+    if (selectedColor.length > 0) {
+      for (let index = 0; index < selectedColor.length; index += 1) {
+        selectedColor[index].classList.remove('selected');
+      }
+    }
+    event.target.classList.add('selected');
+  }
+});
+
+/*let selectedColor = document.querySelector('.black');
+
+function selectColor(element) {
+  if (colorPick.length > 1) {
+    for (let index = 0; index < colorPick('.color').length; index += 1) {
+      colorPick[index].classList.remove('select');
+    }
+
+    selectedColor = element;
+    return selectedColor;
+  } else {
+    selectedColor = element;
+    return selectedColor;
+  }
+};
+
+selectedColor.classList.add('selected');
+*/
