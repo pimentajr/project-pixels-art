ea264f55a697d676313ed7c38123e0f0ada3000d
IsaacMagno <isaacmagno98@gmail.com>
2021-04-09 17:31:49 -0300

    desafio 2 correcao.2

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ window.onload = function() {
   let select = document.querySelector('.color')
   select.className = 'color selected'
 }
+
+
+function selectColor(nameOfColor){
+  nameOfColor.onclick
+  
+}
+
+
+function selectedColor(){
+
+}
