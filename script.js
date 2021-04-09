31c8b5f86b16cb03436cdb06dd9efc4b74625dcf
Hugo Mouto <hmoutol@gmail.com>
2021-04-09 17:53:19 -0300

    Critério 8

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 
+// get Color Palette
 let colorPalette = document.getElementById('color-palette')
+// get Colors
 let colors = document.getElementsByClassName('color')
-console.log(colorPalette);
+// get Selected Color
+let selectedColor = document.querySelector('.selected')
+// get Color Board 
+let pixelBoard = document.getElementById('pixel-board')
 
+// Color Select
 colorPalette.addEventListener('click', function(event){
   for (key of colors) {
     key.classList.remove('selected');
   }
   event.target.classList.add('selected')
 })
-// let selectedColor = 
+
+// Color Drop
+pixelBoard.addEventListener('click', function(event){
+  selectedColor = document.querySelector('.selected');
+  let selectedColorStyle = window.getComputedStyle(selectedColor).getPropertyValue('background-color');
+  // event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+  event.target.style.backgroundColor = selectedColorStyle;
+  })
+
+
