0b162204a78f22fab32c312fb90efbb2245fa8c4
Daniel <danielfdoficial@gmail.com>
2021-04-09 19:22:00 -0300

    conclui desafio 5

--- script.js
+++ script.js
@@ -8,15 +8,13 @@ const elemento = document.querySelectorAll('.color');
 createSquares();
 
 function createTable() {
-	const table = document.querySelectorAll('tr');
-	/* let total = 0; */
+	const tableLine = document.querySelectorAll('tr');
 	for (let linha = 0; linha < 5; linha += 1) {
 		for (let coluna = 0; coluna < 5; coluna += 1) {
 			const elementsQ = document.createElement('td');
 			elementsQ.classList.add('pixel');
-		table[linha].appendChild(elementsQ);
+		tableLine[linha].appendChild(elementsQ);
  }
-	/* total += 1; */
 }
 }
 createTable();
