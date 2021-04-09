1e9c16fb5d1e1689aa0a7303a77cdaf67285b170
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-04-09 16:20:19 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 window.onload = function () {
-  let blackColor = document.getElementById('black');
-  blackColor.classList.add('selected');
-}
+
+  function selectColor () {
+    let blackColor = document.getElementById('black');
+    blackColor.classList.add('selected');
+
+
+    let colorOptions = document.getElementsByClassName('color');
+    for (let index = 0; index < colorOptions.length; index += 1) {
+      colorOptions[index].addEventListener('click', function (event) {
+        for (let index2 = 0; index2 < colorOptions.length; index2 += 1) {
+          if (colorOptions[index2].classList.contains('selected') === true) {
+            colorOptions[index2].classList.remove('selected')
+          }
+          event.target.classList.add('selected');
+        }
+      })
+    }
+  }
+  selectColor()
+};
