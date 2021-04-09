c260e69a1d89d3c0c74949f567593c6424a3a07f
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-09 15:33:32 -0300

    Requisitos 5 e 6 Realizados

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ window.onload = function() {
 
   let boardSection = document.getElementById('pixel-board');
 
-  for (let index = 0; index < 5; index += 1) {
-    let everyRow = document.createElement('div');
-    everyRow.setAttribute('class', 'boardRow');
-    boardSection.appendChild(everyRow);
-    for (let index = 0; index < 5; index += 1) {
-        let everyPixel = document.createElement('div');
-        everyPixel.setAttribute('class', 'pixel');
-        everyRow.appendChild(everyPixel);
-    }
+  //Cria o quadro com os quadrados brancos;
+  for (let index = 0; index < 25; index += 1) {
+      let everyPixel = document.createElement('div');
+      everyPixel.setAttribute('class', 'pixel');
+      boardSection.appendChild(everyPixel);
   }
-
-
 }
+
+
