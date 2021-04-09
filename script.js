7219354f83089d49fc6764a23950ad57d651e6d6
Edson <edson.exe@outlook.com>
2021-04-09 16:05:26 -0300

    Desafio 6

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.onload = function () {
   const pixelBoard = document.querySelector('#pixel-board').children;
-  let color = document.querySelector('#black');
-  const quant = 5;
-
+  const initialColor = document.querySelector('#black');
+  let color = initialColor.id;
   color.className = 'selected';
-
+  
+  
+  
+  const quant = 5;
+  
   for (let index = 0; index < quant; index += 1) {
     for (let index2 = 0; index2 < quant; index2 += 1) {
       const pixel = document.createElement('div');
