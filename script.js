d1db39377ffd40446704fa8d8eaf4db57c679f79
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-09 15:49:01 -0300

    conserto de classes

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 var actualMouseColor = "Black"
-const colorPallete = document.getElementById("color-pallete")
+const colorPallete = document.getElementById("color-palette")
 const pixelBoard = document.getElementById("pixel-board")
 const palleteColors = document.getElementsByClassName("color");
 const pixels = document.getElementsByClassName("pixel")
 const button = document.getElementById("clear-board")
 
-for(let index = 0; index < palleteColors.length; index += 1){
-	if(index === 0){
-		palleteColors[index].style.backgroundColor = "black"
-	}else{
-		const randomColor = Math.floor(Math.random()*16777215).toString(16);
-	
-		palleteColors[index].style.backgroundColor = "#" + randomColor;
+function colorPalletDefinition(){
+	for(let index = 0; index < palleteColors.length; index += 1){
+		if(index === 0){
+			palleteColors[index].style.backgroundColor = "black"
+		}else{
+			if(palleteColors[index - 1].style.backgroundColor === palleteColors[index].style.backgroundColor){
+				const randomColor = Math.floor(Math.random()*16777215).toString(16);
+				palleteColors[index].style.backgroundColor = "#" + randomColor + 30;
+			}else{
+				const randomColor = Math.floor(Math.random()*16777215).toString(16);
+				palleteColors[index].style.backgroundColor = "#" + randomColor;
+			}
+		}
 	}
-	}
+}
 
 function getColor(element){
 	const pencil = element.target;
