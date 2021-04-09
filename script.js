8d083ad736db0a982f7f93361b9ce0d5cf6f16bb
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-09 15:05:30 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-window.onload = function() {
-  
-}
+window.onload = function () {
+  const colorPalette = document.querySelector('#color-palette');
+  const colors = ['black', 'blue', 'yellow', 'magenta'];
+  for (let index = 0; index < 4; index += 1) {
+    const colorBox = document.createElement('div');
+    colorBox.className = 'color';
+    colorBox.style.backgroundColor = colors[index];
+    colorPalette.appendChild(colorBox);
+  }
+};
