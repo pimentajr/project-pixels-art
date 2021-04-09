133da567bd0ac6964ef37d58c782682c27b7a14f
Renan <renan.vamo@gmail.com>
2021-04-09 15:50:48 -0300

    Ajusta alguns pontos do linter

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let firstSelectedColor = document.getElementById('black-box');
+const firstSelectedColor = document.getElementById('black-box');
 firstSelectedColor.classList = ('color selected');
 
 function colorSelection() {
-  let tableColor = document.querySelector("#color-palette");
-  tableColor.addEventListener('click', function(event) {
-    let actualColor = document.querySelector('.selected');
+  const tableColor = document.querySelector('#color-palette');
+  tableColor.addEventListener('click', selectingNewColor)
+  function selectingNewColor(event) {
+    const actualColor = document.querySelector('.selected');
     actualColor.className = 'color';
     event.target.className = 'color selected';
-  })
+  }
 }
-colorSelection()
+colorSelection();
