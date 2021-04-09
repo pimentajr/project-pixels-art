ec82d40703916323787449067c0b9b33bc27dbd6
josanjohnata <josanjohnata@gmail.com>
2021-04-09 18:29:23 -0300

    remove error

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.onload = function() {
     let blackColor = document.getElementById('black');
-    blackColor.className += ' selected';
+    blackColor.className += 'selected';
 }
-function setBoarSize () {
+function setBoardSize () {
   let vqv = document.querySelector('#generete-board');   
   let input = document.querySelector('input');
   let pixelBoard = document.querySelector('#pixel-board');
-  vqv.addEventListener ('click', function()) {
+  vqv.addEventListener ('click', function() {
     let boardSize = input.value;
     if (boardSize) {
       pixelBoard.innerText = '';
@@ -33,7 +33,7 @@ function setBoarSize () {
     else {
       alert('Board inválido!');
     }
-  }
+  })
 }
 function selectedColorChoise () {
   let colorPalette = document.getElementById('color-palette');
@@ -47,7 +47,7 @@ function selectedColorChoise () {
 }
 function paintPixel () {
   let pixelsBoard = document.querySelector('#pixel-board');
-  pixelsBoard.addEventListener('click', function (event){
+  pixelsBoard.addEventListener('click', function (event) {
     let selectedColor = document.querySelector('.selected');
     let selectedColorStyle = window.getComputedStyle(selectedColor).getPropertyValue('background-color');
     event.target.style.backgroundColor = selectedColorStyle;
@@ -67,10 +67,29 @@ function makeBoard () {
     let line2 = document.createElement('tr');
     let pixelBoard = document.getElementById('pixel-board');
     pixelBoard.appendChild(line2);
-    for (index6 = index6 < 5; index6 += 1) {
+    for (index6 = 0; index6 < 5; index6 += 1) {
       let colum2 = document.createElement('td')
       colum2.className = 'pixel';
       document.getElementById('pixel-board').appendChild(colum2);
     }
   }
 }
+function randomColor () {
+  let pixelArt = document.getElementsByClassName ('color');
+  for (i = 0; i < pixelArt.length; i += 1) {
+    if (pixelArt[i].id === 'black') {
+    }
+    else {
+      let color1 = Math.floor(Math.random() * 265);
+      let color2 = Math.floor(Math.random() * 265);
+      let color3 = Math.floor(Math.random() * 265);
+      pixelArt[i].style.backgroundColor = 'rgb ('+ color1 +', ' + color2 + ', '+ color3 +')';
+    }
+  }
+}
+setBoarSize()
+selectedColorChoise()
+paintPixel()
+makeBoard()
+clearBoard()
+randomColor()
