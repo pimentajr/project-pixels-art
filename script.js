757fc03ca4e286e4c86581e029c184ca56180adc
Edson <edson.exe@outlook.com>
2021-04-09 15:30:10 -0300

    Desafio 5

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-window.onload = function() {
-  
-  let pixelBoard = document.querySelector('#pixel-board').children;
-  let quant = 5;
+window.onload = function () {  
+  const pixelBoard = document.querySelector('#pixel-board').children;
+  const quant = 5;
 
   for (let index = 0; index < quant; index += 1) {
     for (let index2 = 0; index2 < quant; index2 += 1) {
-      let pixel = document.createElement('div');
+      const pixel = document.createElement('div');
       pixel.className = 'pixel';
       pixelBoard[index].appendChild(pixel);
     }
