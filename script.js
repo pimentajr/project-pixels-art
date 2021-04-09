c39ac034d3ba1f6fc7c8f9f298defe6689409621
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-09 17:41:31 -0300

    requisito 7 completo, porem nao otimizado

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const blackColor = document.querySelector('#black');
 blackColor.classList.add('selected');
 
-const colorPalette = document.querySelectorAll('.selected');
-function selectColor() {
-  for (let index = 0; index < colorPalette.length; index += 1) {
-    if (colorPalette.className === 'color selected') {
-      colorPalette.classList.remove('selected');
-    } else {
-      colorPalette.classList.add('selected');
-    }
-  }
+const blueColor = document.querySelector('#blue');
+const greenColor = document.querySelector('#green');
+const yellowColor = document.querySelector('#yellow');
+
+function selectBlackColor() {
+  blueColor.classList.remove('selected');
+  greenColor.classList.remove('selected');
+  yellowColor.classList.remove('selected');
+  blackColor.classList.add('selected');
 }
-colorPalette.addEventListener('click', selectColor);
+blackColor.addEventListener('click', selectBlackColor);
+
+function selectBlueColor() {
+  greenColor.classList.remove('selected');
+  yellowColor.classList.remove('selected');
+  blackColor.classList.remove('selected');
+  blueColor.classList.add('selected');
+}
+blueColor.addEventListener('click', selectBlueColor);
+
+function selectGreenColor() {
+  blueColor.classList.remove('selected');
+  yellowColor.classList.remove('selected');
+  blackColor.classList.remove('selected');
+  greenColor.classList.add('selected');
+}
+greenColor.addEventListener('click', selectGreenColor);
+
+function selectYellowColor() {
+  blueColor.classList.remove('selected');
+  greenColor.classList.remove('selected');
+  blackColor.classList.remove('selected');
+  yellowColor.classList.add('selected');
+}
+yellowColor.addEventListener('click', selectYellowColor);
