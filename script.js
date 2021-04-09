610e52137a962787eef5053b18840acadc6f7b23
Marco Galindo <marco1408@gmail.com>
2021-04-09 16:53:52 -0300

    requisito 7 passado parametro if para evitar null

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const colorPalette = document.getElementById('color-palette');
 
-colorPalette.onclick = function changeColor(event) {
+colorPalette.onclick = function selectColor(event) {
   const selectedColor = document.querySelector('.selected');
   const colorAux = selectedColor;
+  if (selectedColor) colorAux.classList.remove('selected');
   event.target.classList.add('selected');
-  colorAux.classList.remove('selected');
 };
+
+// const pixelBoard = document.getElementById('pixel-board');
+
+// pixelBoard.onclick = function paint(e) {
+//   const selectedColor = document.getElementsByClassName('color selected');
+//   console.log(selectedColor[0].style.backgroundColor);
+//   e.target.style.backgroundColor = selectedColor[0].style.backgroundColor;
+// };
