968f761c50003cd9e7c4f6e449c600d2927e6aa6
Vitor <vitorguima@gmail.com>
2021-04-09 18:35:10 -0300

    exercicio 5 usando cor como constante

--- script.js
+++ script.js
@@ -13,10 +13,54 @@ getColorsPallete.addEventListener('click', function(event) {
 })
 }
 
-showColorSelected();
 // evento click para selecionar cor
 // esse deve ocorrer dentro de #collor-pallete
+// o click precisa atribuir uma cor para alguma variável
+
+
 
 // evento click para aplicar cor
-// esse deve ocorrer dentro de #pixel board
+// esse deve ocorrer dentro de #pixel-board
 
+function applySelectedColor() {
+  let getPaintTable = document.querySelector('#pixel-board');
+
+  getPaintTable.addEventListener('click', function(event) {
+    if (event.target.style.backgroundColor === 'black') {
+      event.target.classList.remove('black');
+    } else {
+      event.target.classList.add('black');
+    }
+    console.log(event.target);
+  })
+}
+
+applySelectedColor();
+
+
+function addSelectedToBlack() {
+  let countSelectClass = document.getElementsByClassName('selected');
+  let getBlackColor = document.querySelector('.black');
+  if (countSelectClass.length === 0) {
+    getBlackColor.classList.add('select');
+  }
+}
+
+addSelectedToBlack();
+
+
+
+function removeSelectedClass() {
+  let countSelectClass = document.getElementsByClassName('selected')
+  if (countSelectClass.length !== 0) {
+    for (let index = 0; index < countSelectClass.length; index += 1) {
+      countSelectClass[index].classList.remove('selected');
+    }
+  }
+}
+
+removeSelectedClass() 
+
+
+// ao clicar em uma nova cor:
+// remover class selected do array da palheta
