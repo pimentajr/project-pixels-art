1d694118f232fafdc83d9c1fb47ee2ac79c64195
igordmf <igordanielmf@gmail.com>
2021-04-09 15:28:17 -0300

    Adiciona requisito 2

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let body = document.querySelector('body');
-let title = document.createElement('h1');
+const body = document.querySelector('body');
+const title = document.createElement('h1');
 title.setAttribute('id', 'title');
 title.innerText = 'Paleta de Cores';
 body.appendChild(title);
+
+const colorPalette = document.createElement('div');
+colorPalette.setAttribute('id', 'color-palette');
+body.appendChild(colorPalette);
+const differentColors = 4;
+for (let index = 1; index <= differentColors; index += 1) {
+  let colorBox = document.createElement('div');
+  colorBox.classList.add('color');
+  const maxColorValue = 16777215;
+  randomColor = Math.floor(Math.random()*maxColorValue).toString(16);
+  colorBox.style.backgroundColor = '#' + randomColor;
+  colorPalette.appendChild(colorBox);
+}
