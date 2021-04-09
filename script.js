6ccce4dad644c6fd8144dcc3120181bf99fb44c3
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-09 15:54:42 -0300

    adiciona paleta de cores

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-window.onload = function() {
 let elementH1 = document.createElement('h1');
 elementH1.id = 'title';
 elementH1.innerHTML = 'Paleta de Cores'
 document.body.appendChild(elementH1);
-}
+
+let elementTable = document.createElement('table');
+elementTable.id = 'color-palette';
+elementH1.appendChild(elementTable);
+
+let tableColor1 = document.createElement('th');
+tableColor1.className = 'color';
+tableColor1.style.backgroundColor = 'red'
+elementTable.appendChild(tableColor1);
+
+let tableColor2 = document.createElement('th');
+tableColor2.className = 'color';
+tableColor2.style.backgroundColor = 'blue'
+elementTable.appendChild(tableColor2);
+
+let tableColor3 = document.createElement('th');
+tableColor3.className = 'color';
+tableColor3.style.backgroundColor = 'yellow'
+elementTable.appendChild(tableColor3);
+
+let tableColor4 = document.createElement('th');
+tableColor4.className = 'color';
+tableColor4.style.backgroundColor = 'pink'
+elementTable.appendChild(tableColor4);
+
