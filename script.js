885e117256890218e1639e86e304807fcc184b7b
Guilherme Nunes <guiicnunes@gmail.com>
2021-04-09 16:52:41 -0300

    Correcoes Lint Req 6 e 7

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-window.onload = function () {
-  function makePixelBoard(parent, line, column) {
-    for (let index = 1; index <= line; index += 1) {
-      const divLine = document.createElement('div');
-      divLine.class = 'pixel-board-line';
-      for (let indexLine = 1; indexLine <= column; indexLine += 1) {
-        const divPixel = document.createElement('div');
-        divPixel.className = 'pixel';
-        divLine.appendChild(divPixel);
-      }
-      parent.appendChild(divLine);
+function makePixelBoard(parent, line, column) {
+  for (let index = 1; index <= line; index += 1) {
+    const divLine = document.createElement('div');
+    divLine.class = 'pixel-board-line';
+    for (let indexLine = 1; indexLine <= column; indexLine += 1) {
+      const divPixel = document.createElement('div');
+      divPixel.className = 'pixel';
+      divLine.appendChild(divPixel);
     }
+    parent.appendChild(divLine);
   }
+}
 
+window.onload = function () {
   let pixelBoard = document.querySelector('#pixel-board');
   makePixelBoard(pixelBoard, 5, 5);
+};
 
-  document.querySelector('#color-palette').addEventListener('click', function (event) {
+document.querySelector('#color-palette').addEventListener('click', (event) => {
+  if (event.target.classList.contains('color')) {
     document.querySelector('.selected').classList.remove('selected');
     event.target.classList.add('selected');
-  } );
-};
+  }
+});
