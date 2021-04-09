22d5e8f9f483978b6fa4a363d5b65ee85242bba4
Vinicius <viniciusfod@gmail.com>
2021-04-09 17:03:38 -0300

    adiciona parte 4 do projeto

---
+++ script.js
@@ -0,0 +1,14 @@
+function createPixelsSize (size) {
+    let pixelBox = document.getElementById('pixel-board')
+    for (let index = 0; index < size; index += 1) {
+      let pixelColumn = document.createElement('div');
+      pixelBox.appendChild(pixelColumn);
+
+      for (let index2 = 0; index2 < size; index2 += 1) {
+        let pixelLine = document.createElement('div');
+        pixelBox.appendChild(pixelLine);
+        pixelLine.className = 'pixel';
+      }
+    }
+}
+createPixelsSize (5)
