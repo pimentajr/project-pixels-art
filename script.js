5ee25f121d7e2d912fa317322e3935501fd48094
Gisele Costa <jikinha@gmail.com>
2021-04-09 20:35:02 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,46 +1,12 @@
-// const colorPalette = document.querySelector('#color-palette');
-
-// Cria uma linha
-/*
-function createLine(className) {
-  const line = document.createElement('div');
-  line.className = className;
-  return line;
-}
-*/
-
-// Preenche uma div
-/*
-function populateDiv(div, className, qtd) {
-  for (let lines = 1; lines <= qtd; lines += 1) {
-    const line = createLine(className);
-    div.appendChild(line);
-  }
-}
-*/
-
-// populateDiv(colorPalette, 'palette', 1);
-
-// Cria uma caixa com base nas diferentes cores
-/*
-function createBox(boxColor) {
-  const element = document.createElement('div');
-
-  element.style.backgroundColor = boxColor;
-
-  return element;
-}
-*/
-
-// Colore as caixas
+// Cria e colore a paleta
 function createColor(boxColor) {
-  const container = document.querySelector('#color-palette');
-  const element = document.createElement('div');
+  const palette = document.querySelector('#color-palette');
+  const box = document.createElement('div');
 
-  element.style.backgroundColor = boxColor;
-  element.className = ('color');
+  box.style.backgroundColor = boxColor;
+  box.className = ('color');
 
-  container.appendChild(element);
+  palette.appendChild(box);
 }
 
 createColor('black');
@@ -48,25 +14,24 @@ createColor('red');
 createColor('blue');
 createColor('yellow');
 
+// Preenche uma grid
+function populateGrid(size) {
+  const pixelBox = document.querySelector('#pixel-board');
+
+  for (let index = 0; index < size; index += 1) {
+    const pixelColumn = document.createElement('div');
+    pixelBox.appendChild(pixelColumn);
+
+    for (let index2 = 0; index2 < size; index2 += 1) {
+      const pixelLine = document.createElement('div');
+      pixelColumn.appendChild(pixelLine);
+      pixelLine.className = 'pixel';
+    }
+  }
+}
+
+populateGrid(5);
+
 /*
-Adicione à página um quadro de pixels, com 25 pixels.
-
-    O quadro de "pixels" deve ter 5 elementos de largura e 5 elementos de comprimento;
-
-    O quadro de "pixels" deve possuir o id denominado pixel-board, ao passo que cada "pixel" individual dentro do quadro deve possuir a classe denominada pixel;
-
-    A cor inicial dos "pixels" dentro do quadro, ao abrir a página, deve ser branca;
-
-    O quadro de "pixels" deve aparecer abaixo da paleta de cores.
-
-O que será verificado:
-
-    Verifica se o quadro de pixels possui o id denominado pixel-board
-
-    Verifica se cada pixel individual dentro do quadro possui a classe denominada pixel.
-
-    Verifica se a cor inicial dos pixels dentro do quadro, ao abrir a página, é branca.
-
-    Verifica se o quadro de pixels aparece abaixo da paleta de cores
 
 */
