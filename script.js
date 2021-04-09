8d1ed26e39fb74eb63b137055b0a0fa3be0c891d
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-04-09 16:04:59 -0300

    task 2 1.1

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const palette = document.querySelector('#color-palette');
 for (let i = 0; i < palette.childElementCount; i += 1) {
   palette.children[i].className = 'color';
 }
+
+const colors = document.querySelectorAll('.color');
+
+function randomColors() {
+  for (let i = 0; i < colors.length; i += 1) {
+    const random = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+    colors[i].style.background = random;
+  }
+  colors[0].style.background = 'black';
+}
+
+function start() {
+  randomColors();
+}
+
+window.onload = () => start();
