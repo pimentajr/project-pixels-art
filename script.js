225d764dea6544af8998aa8d0a153fd6d87de45d
Renan <renan.vamo@gmail.com>
2021-04-09 18:49:19 -0300

    Ajusta pontos do linter

--- script.js
+++ script.js
@@ -15,30 +15,29 @@ colorSelection();
 
 // Pinta os pixels em branco com a cor selecionada
 function printBoard() {
-  const pixelBoard = document.getElementById('pixel-board');
-  pixelBoard.addEventListener('click', (event) => {
-    const printBox = event.target;
-    switch (document.querySelector('.selected').id) {
-    case 'black-box':
-      printBox.style.backgroundColor = 'black';
-      break;
-    case 'red-box':
-      printBox.style.backgroundColor = 'red';
-      break;
-    case 'green-box':
-      printBox.style.backgroundColor = 'green';
-      break;
-    case 'yellow-box':
-      printBox.style.backgroundColor = 'yellow';
-      break;
-    default:
+  document.getElementById('pixel-board')addEventListener('click', (event) => {
+  const printBox = event.target;
+  switch (document.querySelector('.selected').id) {
+  case 'black-box':
+    printBox.style.backgroundColor = 'black';
+    break;
+  case 'red-box':
+    printBox.style.backgroundColor = 'red';
+    break;
+  case 'green-box':
+    printBox.style.backgroundColor = 'green';
+    break;
+  case 'yellow-box':
+    printBox.style.backgroundColor = 'yellow';
+    break;
+  default:
     }
   });
 }
 printBoard();
 
 // Cria o botão limpar e insere na div
-function createButton () {
+function createButton() {
   const clearButton = document.createElement('input');
   clearButton.style.margin = '20px 120px';
   clearButton.id = 'clear-board';
@@ -53,7 +52,7 @@ createButton();
 // Ao clicar no botão, limpa a tela
 function buttonAction() {
   const button = document.getElementById('clear-board');
-  button.addEventListener('click', ()=> {
+  button.addEventListener('click', () => {
     const pixels = document.getElementsByClassName('pixel');
     for (let index = 0; index < pixels.length; index += 1) {
       pixels[index].style.backgroundColor = 'white';
