80c2e208244c777a8cff043520c04a7682bbef5b
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-09 18:04:00 -0300

    acertos lint

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let arrayColors = ['aqua', 'blue', 'fuchsia', 'gray', 'green','lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 'silver', 'teal', 'yellow', 'magenta', 'salmon'];
+const arrayColors = ['aqua', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 'silver', 'teal', 'yellow', 'magenta', 'salmon'];
 
-function getRandomColors (colors) {
-  randomColor = colors[Math.floor((Math.random()*colors.length))];
+function getRandomColors(colors) {
+  let randomColor = colors[Math.floor((Math.random() * colors.length))];
   return randomColor;
-
 }
 
-function createBoard() {
-  let board = document.getElementById('pixel-board');
+function createMatrix() {
+  const board = document.getElementById('pixel-board');
   for (let index = 0; index < 5; index += 1) {
-    let line = document.createElement('div');
+    const line = document.createElement('div');
     line.classList.add('line');
     board.appendChild(line);
+    
     for (let secondIndex = 0; secondIndex < 5; secondIndex += 1) {
-      let pixel = document.createElement('div');
+      const pixel = document.createElement('div');
       pixel.classList.add('pixel');
       line.appendChild(pixel);
     }
@@ -28,7 +28,6 @@ window.onload = function () {
   for (let index = 1; index < getColors.length; index += 1) {
     colorRemove = getRandomColors(arrayColors);
     getColors[index].style.backgroundColor = colorRemove;
-    
   }
-  createBoard();
+  createMatrix();
 }
