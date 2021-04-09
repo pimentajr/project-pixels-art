5db0406f77bdc13411011a2f811e11d1679f479e
Matheus_Alexandre <alexandremhm@gmail.com>
2021-04-09 18:31:17 -0300

    Adiciona evento para mudar cor dos pixels

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ colorPaletteContainer.addEventListener('click', (event) => {
   const colorPaletteSelected = event.target;
   colorPaletteSelected.classList.add('selected');
 });
+
+const pixelBoardSquare = document.querySelector('#pixel-board');
+pixelBoardSquare.addEventListener('click', (event2) => {
+  const colorSelected = document.querySelector('.selected');
+  console.log(colorSelected);
+  const pixelToPrint = event2.target;
+  console.log(pixelToPrint);
+  pixelToPrint.style.backgroundColor = window.getComputedStyle(colorSelected).backgroundColor;
+  console.log(window.getComputedStyle(colorSelected));  
+});
+
+// const buttonClearColor = document.querySelector('#clear-board');
+// buttonClearColor.addEventListener
