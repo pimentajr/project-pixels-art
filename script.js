543e9200186c7db6a5d8ef9c6cb9991fc3b847d8
MIkaela Braga <bragamikaela@gmail.com>
2021-04-09 20:32:20 -0300

    requisito 6 com 1 problema de lint

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const getPixels = document.getElementsByTagName('td');
 for (let index = 0; index < getPixels.length; index += 1) {
   getPixels[index].className = 'pixel';
 }
+
+window.onload = function () {
+  const getColorBlack = document.getElementById('cor1');
+  getColorBlack.className += ' selected';
+};
