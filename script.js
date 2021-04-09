fcc948605b6a76d57bccfee45a27e68b0bbbb9e2
Uriel Bochi <urielbochi@gmail.com>
2021-04-09 15:49:59 -0300

    Tentativa exercício 2 e 3

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function (){
+
+    let black = document.getElementById("black").style.backgroundColor = "black"
+    let red = document.getElementById("red").style.backgroundColor="red"
+    let green = document.getElementById("green").style.backgroundColor="green"
+    let purple = document.getElementById("purple").style.backgroundColor="purple"
+
+}
