8550867a099ab57fa0f25f4a3028e966cc8ae086
LeoFuna <leo.funa@gmail.com>
2021-04-09 15:21:21 -0300

    [ADD]Clear button

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ for (let index = 0; index < pixelColor.length; index += 1) {
   pixelColor[index].addEventListener('click', setAsSelected);
 }
 
-function paintPixel(event) { // pintar o pixel do quadro pixel-board
+function paintPixel(event) {
   const pixelToChange = event.target;
   pixelToChange.style.backgroundColor = document.querySelectorAll('.selected')[0].id;
 }
-
 const pixels = document.querySelectorAll('.pixel');
 for (let index = 0; index < pixels.length; index += 1) {
   pixels[index].addEventListener('click', paintPixel);
 }
+
+function clearPixels() {
+  const pixelBoard = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixelBoard.length; index += 1) {
+    pixelBoard[index].style.backgroundColor = 'white';
+  }
+}
+const buttonClear = document.querySelector('#clear-board');
+buttonClear.addEventListener('click', clearPixels);
