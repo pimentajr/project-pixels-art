846e6a8d1c30c2da299ed269cc4725bf019e8a72
LeoFuna <leo.funa@gmail.com>
2021-04-09 14:13:14 -0300

    [ADD]JS with click sensitive on pixels

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(document.querySelector('.selected').classList[1]);
+const pixelColor = document.querySelectorAll('.color');
+function setAsSelected(event) {
+  const pixelClicked = event;
+  document.querySelectorAll('.selected')[0].className = 'color';
+  pixelClicked.target.className = 'color selected';
+}
+for (let index = 0; index < pixelColor.length; index += 1) {
+  pixelColor[index].addEventListener('click', setAsSelected);
+}
