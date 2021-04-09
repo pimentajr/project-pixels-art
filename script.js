2ba693525d87c9a4463f0908bafe0a29969700f6
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-09 15:14:40 -0300

    Feito requisito obrigatório 2

---
+++ script.js
@@ -0,0 +1,10 @@
+const colorDivs = document.querySelectorAll('.color');
+
+function setColorToPalete () {
+  for (let index = 0; index < colorDivs.length; index += 1) {
+    let colorDivsIDValue = colorDivs[index].id;
+    colorDivs[index].style.backgroundColor = colorDivsIDValue;
+  }
+}
+
+setColorToPalete();
