92e9547fc3a5d8f49aa66362c691b18c204be9ec
Luís Seixas <luiseixas2@gmail.com>
2021-04-09 16:51:37 -0300

    requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let elementH1 =  document.getElementById('title');
+let elementH1 = document.getElementById('title');
 elementH1.innerText = 'Paleta de Cores';
+
+function createPalettes() {
+  for (index = 0; index < 4; index += 1) {
+    let palettes = document.createElement('div');
+    let allPalettes = document.getElementById('color-palette');
+    allPalettes.appendChild(palettes);
+    palettes.className = 'color';
+  }
+}
+createPalettes();
+
+let squareColors = document.getElementsByClassName('color');
+console.log(squareColors);
+squareColors[0].style.backgroundColor = 'black';
+squareColors[1].style.backgroundColor = 'red';
+squareColors[2].style.backgroundColor = 'yellow';
+squareColors[3].style.backgroundColor = 'green';
