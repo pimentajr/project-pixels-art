68f5509be0891771d33487df6cb2179efc5e3749
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-09 17:19:31 -0300

    reenviando o trabalho

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function getRandomColors (colors) {
   return randomColor;
 }
 
-function createBoard() {
-  let board = document.getElementById('pixel-board');
-  for (let index = 0; index < 5; index += 1) {
-    let line = document.createElement('div');
-    line.classList.add('line');
-    board.appendChild(line);
-    for (let secondIndex = 0; secondIndex < 5; secondIndex += 1) {
-      let pixel = document.createElement('div');
-      pixel.classList.add('pixel');
-      line.appendChild(pixel);
-    }
-  }
-}
+// function createBoard() {
+//   let board = document.getElementById('pixel-board');
+//   for (let index = 0; index < 5; index += 1) {
+//     let line = document.createElement('div');
+//     line.classList.add('line');
+//     board.appendChild(line);
+//     for (let secondIndex = 0; secondIndex < 5; secondIndex += 1) {
+//       let pixel = document.createElement('div');
+//       pixel.classList.add('pixel');
+//       line.appendChild(pixel);
+//     }
+//   }
+// }
 
 window.onload = function () {
   const getColors = document.getElementsByClassName('color');
@@ -25,5 +25,5 @@ window.onload = function () {
   for (let index = 1; index < getColors.length; index += 1) {
     getColors[index].style.backgroundColor = getRandomColors(arrayColors);
   }
-  createBoard();
+  // createBoard();
 }
