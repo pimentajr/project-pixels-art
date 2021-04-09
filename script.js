2e1a9ba43fca80de5391cf188ff6bab526ba5427
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-09 15:22:28 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var actualMouseColor = "Black"
+const colorPallete = document.getElementById("color-pallete")
+const pixelBoard = document.getElementById("pixel-board")
+const palleteColors = document.getElementsByClassName("color");
+
+for(let index = 0; index < palleteColors.length; index += 1){
+	if(index === 0){
+		palleteColors[index].style.backgroundColor = "black"
+	}else{
+		const randomColor = Math.floor(Math.random()*16777215).toString(16);
+	
+		palleteColors[index].style.backgroundColor = "#" + randomColor;
+	}
+	}
+
+function getColor(element){
+	const pencil = element.target;
+	const color = getComputedStyle(pencil).backgroundColor
+	if(pencil.parentNode === colorPallete){
+		actualMouseColor = color;
+	}
+};
+
+addEventListener("click", getColor);
+
+function paint(element){
+	const paintedBlock = element.target;
+	if(paintedBlock.parentNode === pixelBoard){
+		if(actualMouseColor !== ""){
+			paintedBlock.style.backgroundColor = actualMouseColor;
+		}
+	}
+}
+
+addEventListener("click", paint);
