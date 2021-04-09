fdeca5a6de3e4421866528f7418eaaa324b4bda3
Alberto Candido <albertocandido98@gmail.com>
2021-04-09 18:08:26 -0300

    Requisito 8 - alterando cor dos quadrados com base na cor selecionada

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function squareColuns() {
   for (let index = 0; index < coluns.length; index += 1) {
     for (let secondIndex = 1; secondIndex <= lines; secondIndex += 1) {
       const newColun = document.createElement('div');
-      newColun.className = 'pixel';
+      newColun.className = 'pixel white';
       coluns[index].appendChild(newColun);
     }
   }
@@ -24,6 +24,7 @@ function squareColuns() {
 
 squareColuns();
 
+// 7
 function changeClass(event) {
   const classToRemove = document.getElementsByClassName('color');
   for (let index = 0; index < classToRemove.length; index += 1) {
@@ -38,3 +39,18 @@ function selectColor() {
 }
 
 selectColor();
+
+// 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
+
+function addColor(event) {
+  const colorselected = document.querySelector('.selected');
+  const newClass = colorselected.classList[1];
+  event.target.className = `pixel ${newClass}`;
+}
+
+function changeColor() {
+  const pixelSquare = document.getElementById('pixel-board');
+  pixelSquare.addEventListener('click', addColor);
+}
+
+changeColor();
