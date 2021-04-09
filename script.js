8d7e5c340d1488d36ab74de4fef03d48dd86ccb6
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-09 17:15:42 -0300

    cria uma classe para mexer nas propriedades da tabela, cria uma classe borda e adiciona ela para todos os elementos que precisam

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const getBody = document.body;
 const makeTable = document.createElement('table');
 getBody.appendChild(makeTable);
 makeTable.id = 'pixel-board';
-makeTable.className = 'center';
+makeTable.className = 'center table';
 
 for (let index = 0; index < 5; index += 1) {
   const makeTr = document.createElement('tr');
@@ -11,7 +11,7 @@ for (let index = 0; index < 5; index += 1) {
   for (let jndex = 0; jndex < 5; jndex += 1) {
     const makeTd = document.createElement('td');
     makeTr.appendChild(makeTd);
-    makeTd.className = 'pixel branco';
+    makeTd.className = 'pixel branco borda';
   }
 }
 // npm run lint
