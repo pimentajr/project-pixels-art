94b6d357967f1637b319e89317ffe60ce2818179
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-09 17:06:29 -0300

    altera o tamanho de cada quadrado e centraliza a tabela

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const getBody = document.body;
 const makeTable = document.createElement('table');
 getBody.appendChild(makeTable);
 makeTable.id = 'pixel-board';
+makeTable.className = 'center';
 
 for (let index = 0; index < 5; index += 1) {
   const makeTr = document.createElement('tr');
   makeTable.appendChild(makeTr);
+  makeTr.className = 'center';
   for (let jndex = 0; jndex < 5; jndex += 1) {
     const makeTd = document.createElement('td');
     makeTr.appendChild(makeTd);
