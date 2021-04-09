05f7d41b9fcd40a4373a6ac734c6f2b819a4eb1e
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2021-04-09 18:07:44 -0400

    Adicionando uma paleta de cores distintas e fixando o primeiro pixel com a cor preta.

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-window.onload = function () {
-  const getTitle = document.getElementById('title');
-  getTitle.innerText = 'Paleta de cores';
-  console.log(getTitle);
+window.onload = function start() {
+  document.getElementById('color-palette');
+  document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+
+  function randomBgCcolor() {
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    const bgColor = `rgb(${x}, ${y}, ${z})`;
+    return bgColor;
+  }
+
+  document.getElementsByClassName('color')[1].style.backgroundColor = randomBgCcolor();
+  document.getElementsByClassName('color')[2].style.backgroundColor = randomBgCcolor();
+  document.getElementsByClassName('color')[3].style.backgroundColor = randomBgCcolor();
 };
