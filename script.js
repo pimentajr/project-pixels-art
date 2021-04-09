1b8d99f8597225a6eec85d4090f3550cccfedef2
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-09 17:06:24 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const colorPallet = document.querySelector('body');
+const colorPallet = document.querySelector('#color-palette');
 
 function createColor(array) {
   for (let i = 0; i < array.length; i += 1) {
     const color = document.createElement('div');
-    color.id = 'color-palette';
+    // color.id = 'color-palette';
     color.className = 'color';
     color.style.backgroundColor = array[i];
     colorPallet.appendChild(color);
   }
 }
 createColor(['black', 'red', 'blue', 'green']);
+
+function createBoard() {
+  const board = document.querySelector('#pixel-board');
+
+  for (let row = 0; row < 5; row += 1) {
+    const rowLine = document.createElement('div');
+
+    for (let column = 0; column < 5; column += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      rowLine.appendChild(pixel);
+    }
+    board.appendChild(rowLine);
+  }
+}
+createBoard();
