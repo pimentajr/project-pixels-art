7576414a74ff234e3bf912b307cd8a37aa454a02
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-09 16:09:53 -0300

    adiciona requisito 4

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const boardSize = 5;
 const colors = document.getElementsByClassName('color');
+const pixelBoard = document.getElementById('pixel-board');
 
 function setColors() {
   for (let index = 0; index < colors.length; index += 1) {
@@ -6,8 +8,28 @@ function setColors() {
   }
 }
 
+function setBoardSize(size) {
+  pixelBoard.innerHTML = '';
+
+  for (let index = 0; index < size; index += 1) {
+    const div = document.createElement('div');
+    const row = div.cloneNode();
+    row.classList.add('table-row');
+    const cell = div.cloneNode();
+    cell.classList.add('pixel');
+
+    for (let secondIndex = 0; secondIndex < size; secondIndex += 1) {
+      const newCell = cell.cloneNode();
+      row.appendChild(newCell);
+    }
+
+    pixelBoard.appendChild(row);
+  }
+}
+
 function onPageLoad() {
   setColors();
+  setBoardSize(boardSize);
 }
 
 window.onload = onPageLoad;
