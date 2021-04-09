16a2602055afc23f3b970c2a8d5624edebf28ace
Hamander <1hamander@gmail.com>
2021-04-09 17:06:24 -0300

    Exercicio_4 Completo. Base Adicionada

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ colorPallet('black');
 colorPallet('red');
 colorPallet('green');
 colorPallet('blue');
+
+function pixelBoard() {
+  const divPixel= document.createElement('div');
+  const divBlock = document.querySelector('#pixel_board');
+  let pixelCreator = divBlock.appendChild(divPixel)
+  divPixel.className = 'pixel';
+  divPixel.style.backgroundColor = 'white';
+}
+function pixelBoarIn() {
+for (let index = 0; index < 25; index += 1) {
+  pixelBoard()
+}
+}
+pixelBoarIn()
+
