b6aca9572370027b5eccb46cf7aa1a5823bd6139
Uriel Bochi <urielbochi@gmail.com>
2021-04-09 17:46:47 -0300

    ex 4

--- script.js
+++ script.js
@@ -5,4 +5,21 @@ window.onload = function (){
     let green = document.getElementById("green").style.backgroundColor="green"
     let purple = document.getElementById("purple").style.backgroundColor="purple"
 
+    let pixelTable = document.getElementById('pixel-board')
+
+    for (let i = 0;  i < 5; i += 1) {
+        let blockRow = document.createElement('div')
+        blockRow.className = 'blockRow'
+        pixelTable.appendChild(blockRow)
+    
+    
+    for (let j =0; j < 5; j +=1) {
+        let blockCell = document.createElement('div')
+        blockCell.className = 'blockCell'
+        blockRow.appendChild(blockCell)
+        blockCell.style.backgroundColor = 'White'
+        
+
+    }
+}
 }
