c8b496cad849098f7579bdfec478fa2d3b14c2a2
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-09 15:16:43 -0300

    Requisito 3 - trocando a cor sem criar

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 // Requisito 3 - Criação da cor de paleta preta na primeira posição
-function addBlackColor () {
-	let listPallete = document.getElementById('color-palette');
-	let blackColor = document.createElement('div');
-	blackColor.className = 'color';
-	blackColor.style.backgroundColor = 'black';
-	listPallete.appendChild(blackColor);
-	// Colocando o preto na primeira posição
-	listPallete.insertBefore(blackColor, listPallete.childNodes[0])
+function addBlackColor() {
+	let firstColor = document.getElementById('green');
+	firstColor.style.backgroundColor = 'black';
 }
 addBlackColor();
 
