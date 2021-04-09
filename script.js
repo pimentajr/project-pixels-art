3114075830b3bdc142f7fd1a5db9581e17631fb9
IgorMendez <igor.emp@hotmail.com>
2021-04-09 16:39:59 -0300

    Resolução exercício 1

---
+++ script.js
@@ -0,0 +1,12 @@
+const bodyElement = document.body;
+
+const title = document.createElement('h1');
+bodyElement.appendChild(title);
+title.innerText = "Paleta de Cores"
+title.id = 'title'
+
+// (function colorMatriz(selectColorMatriz) {
+//   for(let index = 0; index < 25; index += 1) {
+//     document.createElement('td')
+//   }
+// })();
