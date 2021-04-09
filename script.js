a58ddbedf4c2b35072f937a62510295ce87b9d82
Júlio Mota <juliomotacampos@gmail.com>
2021-04-09 16:14:01 -0300

    Correções de lint

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let colorPalette = document.querySelector('#color-palette');
-let colors = ['black', 'red', 'green', 'blue'];
+const colorPalette = document.querySelector('#color-palette');
+const colors = ['black', 'red', 'green', 'blue'];
 
 for (let index = 0; index < colorPalette.children.length; index += 1) {
   colorPalette.children[index].innerHTML = colors[index];
   colorPalette.children[index].style.backgroundColor = colors[index];
-  colorPalette.children[index].style.color = colors[index]; 
+  colorPalette.children[index].style.color = colors[index];
 }
