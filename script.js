b7d48fc5dba36cc7a25421b8f81621b29a75c3ae
nandovbr <nandovbr15@hotmail.com>
2021-04-09 17:47:51 -0300

    correção requisitos 1 e 2

---
+++ script.js
@@ -0,0 +1,6 @@
+let colors = document.getElementsByClassName('color');
+
+colors[0].style.background = 'black';
+colors[1].style.background = 'red';
+colors[2].style.background = 'yellow';
+colors[3].style.background = 'orange';
