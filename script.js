bc112ca4133edbb699a6a5df7ab59bf1bff404c5
Halysson2 <halyssonsena83@gmail.com>
2021-04-09 15:33:21 -0300

    primeira questão

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function titlePalet() {
+function titleContent() { 
     let getBody = document.querySelector('body')
-    let title = document.createElement('h1')[0]
+    let title = document.createElement('h1')
     title.id = 'title'
     title.innerHTML = 'Paleta de Cores'
     getBody.appendChild(title)
-
-} 
-titlePalet()
+  }
+  titleContent()
