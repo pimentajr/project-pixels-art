82b67761375bd534d0cf8db33d3b47c330b9ee39
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-04-09 20:44:05 -0300

    Ajusta erros do lint

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-window.onload = function() {
-  
-  document.querySelector('#black').classList.add('selected')
+window.onload = function load() {
+  document.querySelector('#black').classList.add('selected');
+};
 
-  let colorSection = document.getElementById('color-palette')
+const colorSection = document.getElementById('color-palette');
 
-  colorSection.addEventListener('click', function(event){
-    for (let index = 1; index < 5; index +=1){
-      let divChild = document.querySelector(`#color-palette div:nth-child(${index})`)
-      divChild.className = 'color'
-    }
+function addClassToPallete(event) {
+  for (let index = 1; index < 5; index += 1) {
+    const divChild = document.querySelector(`#color-palette div:nth-child(${index})`);
+    divChild.className = 'color';
+  }
 
-    if(event.target !== colorSection){
-      event.target.classList.add('selected')
-    }
-  })
-
-  let pixels = document.querySelector('#pixel-board')
-
-  pixels.addEventListener('click', function(event){
-    let actualColor = document.querySelector('.selected')
-    let pixel = event.target
-    pixel.id = actualColor.id
-  })
-
-  let button = document.querySelector('#clear-board')
-
-
-  button.addEventListener('click', function(){
-    let allPixels = document.querySelectorAll('.pixel')
-    for(let index = 0; index < allPixels.length; index += 1){
-      allPixels[index].removeAttribute('id')
-    }
-  })
-
+  if (event.target !== colorSection) {
+    event.target.classList.add('selected');
+  }
 }
+
+colorSection.addEventListener('click', addClassToPallete);
+
+const pixels = document.querySelector('#pixel-board');
+
+function addColorToPixels(event) {
+  const actualColor = document.querySelector('.selected');
+  const pixel = event.target;
+  pixel.id = actualColor.id;
+}
+
+pixels.addEventListener('click', addColorToPixels);
+
+const button = document.querySelector('#clear-board');
+
+function clearPixels() {
+  const allPixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < allPixels.length; index += 1) {
+    allPixels[index].removeAttribute('id');
+  }
+}
+
+button.addEventListener('click', clearPixels);
