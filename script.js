8e7f22335932535253948e5c1af1407aaaba7549
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-04-09 19:02:48 -0300

    Corrige o requisito 06

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const divColors = document.getElementsByClassName('.color');
 
 function setColorToPalette() {
-    for (let index = 0; index < divColors.length; index++) {
-      const idColorDivs = divColors[index].id;
-      if(divColors[index].getElementsByClassName.backgroundColor !== 'black') {
-          divColors[0].getElementsByClassName.backgroundColor = 'black';  
-        }
-        divColors[index].getElementsByClassName.backgroundColor = idColorDivs;        
+  for (let index = 0; index < divColors.length; index += 1) {
+  const idColorDivs = divColors[index].id;
+    if (divColors[index].getElementsByClassName.backgroundColor !== 'black') {
+        divColors[0].getElementsByClassName.backgroundColor = 'black';  
     }
+    divColors[index].getElementsByClassName.backgroundColor = idColorDivs;        
+  }
 }
 setColorToPalette();
 
-window.onload = setBlack
-
-  function setBlack() {
-    const elementBlack = document.getElementsById('black');
-    elementBlack.classList.add('selected');
-  }
-
+window.onload = function () {
+  const elementBlack = document.getElementsById('black');
+  elementBlack.classList.add('selected');
+}
