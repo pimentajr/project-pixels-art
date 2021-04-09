975eba5e0a89f380ad2548df50481ba1d4e7c0d0
Luís Seixas <luiseixas2@gmail.com>
2021-04-09 18:33:12 -0300

    quesito 4 e 5 e ajustes nos restantes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// quesito 1
 let elementH1 = document.getElementById('title');
 elementH1.innerText = 'Paleta de Cores';
 
+// quesito 2
 function createPalettes() {
-  for (index = 0; index < 4; index += 1) {
+  for (let index = 0; index < 4; index += 1) {
     let palettes = document.createElement('div');
     let allPalettes = document.getElementById('color-palette');
     allPalettes.appendChild(palettes);
@@ -11,9 +13,24 @@ function createPalettes() {
 }
 createPalettes();
 
+// quesito 3
 let squareColors = document.getElementsByClassName('color');
-console.log(squareColors);
 squareColors[0].style.backgroundColor = 'black';
 squareColors[1].style.backgroundColor = 'red';
 squareColors[2].style.backgroundColor = 'yellow';
 squareColors[3].style.backgroundColor = 'green';
+
+// quesito 4
+function createMainBoard() {
+  let mainBoard = document.getElementById('pixel-board');
+  for (let index = 0; index < 5; index += 1) {
+    let columns = document.createElement('div');
+    mainBoard.appendChild(columns);
+    for (let jindex = 0; jindex < 5; jindex += 1) {
+      let lines = document.createElement('div');
+      columns.appendChild(lines);
+      lines.className = 'pixel';
+    }
+  }
+}
+createMainBoard();
