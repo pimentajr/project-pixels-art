5a870e0994df42fca4fa5a44a9349affedd4c6b7
Filizzola <juliofilizzola@hotmail.com>
2021-04-09 15:20:08 -0300

    primeiro desafio refaturado 1

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-window.onload = function () {
-  // Desafio 1
-  function creatH1(tag, id, Name) {
-    const create = document.createElement(tag);
-    create.id = id;
-    create.innerText = Name;
-    document.body.appendChild(create);
-  }
-  creatH1('h1', 'title', 'Paleta De Cores!');
-};
+// Desafio 1
+function creatH1(tag, id, Name) {
+  const create = document.createElement(tag);
+  create.id = id;
+  create.innerText = Name;
+  document.body.appendChild(create);
+}
+creatH1('h1', 'title', 'Paleta De Cores!');
