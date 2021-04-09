4c3c4a4e98a917ff3efc1c31203995f049480dd5
GustavoReginato <gureginatopd@gmail.com>
2021-04-09 19:01:57 -0300

    adiciona exercicio 7 e 8

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-let pixel = [];
+const numberLines = 5;
+const pixelBoard = document.getElementById("pixel-board");
+
+const pixel = [];
+const linha = [];
+
+let k = 0;
+for (let i = 0; i < numberLines; i += 1) {
+  linha[i] = document.createElement('div');
+  linha[i].className = 'linha';
+  pixelBoard.appendChild(linha[i]);
+
+  for (let j = 0; j < numberLines; j += 1) {
+    pixel[k] = document.createElement('div');
+    pixel[k].className = 'pixel';
+    linha[i].appendChild(pixel[k]);
+    k = k + 1;
+  }
+}
+
+const color = document.querySelectorAll('.color');
+
+for (let i = 0; i < color.length; i += 1) { 
+  color[i].addEventListener('click', function() {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  color[i].classList.add('selected');
+  });
+  
 const buttonClear = document.getElementById('botaoLimpar');
+
 buttonClear.addEventListener('click', function () {
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].style.backgroundColor = 'white';
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = 'white';
   }
 });
