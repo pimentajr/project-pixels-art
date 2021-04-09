eed73351867a4d7ac8a7e04d39e8d7df79e92eca
DADA <davimarque2000@gmail.com>
2021-04-09 19:36:24 -0300

    seleciona cor na paleta

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ window.onload = function() {
     let blackColor = document.querySelector('#preto')
     blackColor.classList.add("selected");
 }
+colorTable = document.querySelector('#color-palette')
+colorTable.addEventListener('click', function (event){
+    let colorSelected = document.querySelector('.selected')
+    colorSelected.classList.remove('selected');
+    event.target.classList.add('selected')
+})
