346a92de828ec60107b142b2301c343aceaac8a4
Júlio Mota <juliomotacampos@gmail.com>
2021-04-09 17:21:08 -0300

    Adicionados os requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const colorPalette = document.querySelector('#color-palette');
 const colors = ['black', 'red', 'green', 'blue'];
 
-for (let index = 0; index < colorPalette.children.length; index += 1) {
-  colorPalette.children[index].innerHTML = colors[index];
-  colorPalette.children[index].style.backgroundColor = colors[index];
-  colorPalette.children[index].style.color = colors[index];
+function createColorPalette() {
+  
+  for (let index = 0; index < colorPalette.children.length; index += 1) {
+    colorPalette.children[index].innerHTML = colors[index];
+    colorPalette.children[index].style.backgroundColor = colors[index];
+    colorPalette.children[index].style.color = colors[index];
+  }
+
 }
+
+createColorPalette();
+
+const pixelBoard = document.querySelector('#pixel-board');
+
+function fillPixelBoard(boardSize) {
+  for (let lineIndex = 0; lineIndex < boardSize; lineIndex += 1) {
+    let line = document.createElement('div');
+    line.className = 'line';
+    pixelBoard.appendChild(line);
+    
+    for (let pixelIndex = 0; pixelIndex < boardSize; pixelIndex += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.style.backgroundColor = 'white';
+      line.appendChild(pixel);
+    }
+  }
+}
+
+fillPixelBoard(5);
