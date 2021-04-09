2ae0e8286c740378296d4e33732c9805617383e3
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-09 18:54:48 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function primaryColors(colors) {
 
   for (let index = 0; index < colors.length; index += 1) {
     colorBox[index].style.backgroundColor = colors[index];
+    if (index === 0) {
+      colorBox[index].className = 'selected';
+    }
   }
 }
 
@@ -11,7 +14,7 @@ primaryColors(['black', 'yellow', 'green', 'blue']);
 function pixelsTable(width, height) {
   const table = document.querySelector('#pixel-board');
   table.style.width = [width * 40, 'px'].join('');
-  table.style.height = [height * 40 + 'px'].join('');
+  table.style.height = [height * 40, 'px'].join('');
   for (let index = 1; index <= height; index += 1) {
     const newColumn = document.createElement('tr');
     for (let index2 = 1; index2 <= width; index2 += 1) {
