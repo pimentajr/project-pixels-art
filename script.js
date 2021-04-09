e9ea31c434917dd33fe4844e74000d2c540c4220
anaclarabck <ackyotoku@gmail.com>
2021-04-09 18:27:03 -0300

    3 [UPDATE] Ajuestes requisitos 1, 2 e 3

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-
-let listOfColors = ['lightsalmon', 'lightgreen', 'lightskyblue', 'khaki'];
-let colors = document.querySelectorAll('.color');
-let colorsPalette = document.querySelector('#collor-palette');
+const listOfColors = ['black', 'lightgreen', 'lightskyblue', 'khaki'];
+const colorsPalette = document.querySelector('#color-palette');
 
 function createBox(arrayColors) {
   for (let index = 0; index < arrayColors.length; index += 1) {
-    let box = document.createElement('div');
+    const box = document.createElement('div');
     box.className = 'color';
     box.style.background = listOfColors[index];
-    colorsPalette.appendChild(box)
-    }
-}
+    colorsPalette.appendChild(box);
+  };
+};
 
-createBox(listOfColors)
-
-
-// function callBox(box) {
-//   for (let index = 0; index < colors.length; index += 1) {
-//     color = index
-//     let box = defineColor(color)
-
-
-//   }
-
-// }
-
-// function defineColor(color) {
-//   let index = color
-//   color.style.background = listOfColors[index]
-  
-
-// }
+createBox(listOfColors);
