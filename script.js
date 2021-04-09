eaae3bf90745dd2acf6c47129fe3dafe44667627
Vinicius <viniciusfod@gmail.com>
2021-04-09 17:37:34 -0300

    adiciona parte 6 do projeto

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const colorPalette = document.getElementById('color-palette');
+let colorToPaint = document.getElementsByClassName('selected')[0];
+
+
 function createPixelsSize (size) {
     let pixelBox = document.getElementById('pixel-board')
     for (let index = 0; index < size; index += 1) {
@@ -12,3 +16,15 @@ function createPixelsSize (size) {
     }
 }
 createPixelsSize (5)
+
+function selectedColor (event) {
+  colorToPaint.classList.remove('selected');
+  colorToPaint = event.target;
+  colorToPaint.classList.add('selected');
+}
+
+colorPalette.addEventListener('click', selectedColor);
+
+function changeColor (event) {
+  
+}
