6f07b85b618fa8ae03912f7b9be0071915fd8078
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-09 17:39:45 -0300

    Melhoras no lint

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
-window.onload = function() {
+window.onload = function start() {
+  const boardSection = document.getElementById('pixel-board');
+  const colorPalette = document.getElementById('color-palette');
+  const btnRest = document.getElementById('clear-board');
 
-  let boardSection = document.getElementById('pixel-board');
-  let colorPalette = document.getElementById('color-palette');
-  let btnRest = document.getElementById('clear-board');
-  
-  //Cria o quadro com os quadrados brancos;
+  // Cria o quadro com os quadrados brancos;
   for (let index = 0; index < 25; index += 1) {
-      let everyPixel = document.createElement('div');
-      everyPixel.setAttribute('class', 'pixel');
-      boardSection.appendChild(everyPixel);
+    const everyPixel = document.createElement('div');
+    everyPixel.setAttribute('class', 'pixel');
+    boardSection.appendChild(everyPixel);
   }
 
-  
-  colorPalette.addEventListener('click', changeSelectedColor);
-
-  boardSection.addEventListener('click', changePixelColor);
-
-  btnRest.addEventListener('click', clearBoard);
-
-}
-
-function changeSelectedColor(event) {
-  let allDivsColorPallete = document.getElementById('color-palette').children;
-  for (let index = 0; index < allDivsColorPallete.length; index += 1) {
-    allDivsColorPallete[index].setAttribute('class', 'color');
-  }
-  event.target.className += ' selected';
-}
-
-function changePixelColor(event) {
-  let colorSelected = window.getComputedStyle(document.querySelector('.selected'));
-  event.target.style.backgroundColor =  colorSelected.backgroundColor;
-}
-
-function clearBoard() {
-  let AllDivsPixels = document.getElementById('pixel-board').children;
-  for (let index = 0; index < AllDivsPixels.length; index += 1) {
-    AllDivsPixels[index].style.backgroundColor = 'white';
-    
-  }
-}
+  colorPalette.addEventListener('click', (event) => {
+    const allDivsColorPallete = document.getElementById('color-palette').children;
+    for (let index = 0; index < allDivsColorPallete.length; index += 1) {
+      allDivsColorPallete[index].setAttribute('class', 'color');
+    }
+    event.target.className += ' selected';
+  });
+  boardSection.addEventListener('click', (event) => {
+    const colorSelected = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
+    event.target.style.backgroundColor = colorSelected;
+  });
+  btnRest.addEventListener('click', () => {
+    const AllDivsPixels = document.getElementById('pixel-board').children;
+    for (let index = 0; index < AllDivsPixels.length; index += 1) {
+      AllDivsPixels[index].style.backgroundColor = 'white';
+    }
+  });
+};
