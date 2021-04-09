d504b3848952ae2c6166a54770d252f9107f710e
Renan <renan.vamo@gmail.com>
2021-04-09 17:16:32 -0300

    Adiciona a funcionalidade para pintar os box com a cor selecionada

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ firstSelectedColor.classList = ('color selected');
 
 function colorSelection() {
   const tableColor = document.querySelector('#color-palette');
-  tableColor.addEventListener('click', selectingNewColor)
   function selectingNewColor(event) {
     const actualColor = document.querySelector('.selected');
     actualColor.className = 'color';
     event.target.className = 'color selected';
   }
+  tableColor.addEventListener('click', selectingNewColor);  
 }
 colorSelection();
+
+function printBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
+  
+  pixelBoard.addEventListener('click', function(event) {
+    let selectedDiv = document.querySelector('.selected').id;
+    console.log(selectedDiv);
+    let printBox = event.target;
+    console.log(printBox);
+    switch (selectedDiv) {
+      case 'black-box':
+        printBox.style.backgroundColor = 'black';
+        break;
+      case 'red-box':
+        printBox.style.backgroundColor = 'red';
+        break;
+      case 'green-box':
+        printBox.style.backgroundColor = 'green';
+        break;
+      case 'yellow-box':
+        printBox.style.backgroundColor = 'yellow'
+        break;
+    }
+  })
+}
+printBoard()
