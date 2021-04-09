e37f575d83266d4e13aac7bf3172ad93d600be9e
Antonio Malato <malatoneto@gmail.com>
2021-04-09 19:15:31 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ let box0 = document.querySelector('#box-color0').style.backgroundColor = 'rgb(00
 let box1 = document.querySelector('#box-color1').style.backgroundColor = 'rgb(61, 128, 235)';
 let box2 = document.querySelector('#box-color2').style.backgroundColor = 'rgb(204, 152, 31)';
 let box3 = document.querySelector('#box-color3').style.backgroundColor = 'rgb(204, 31, 106)';
+
+let table = document.querySelector('#pixel-board');
+
+for (let index = 0; index < 5; index++) {
+    let createPixel = document.createElement('tr');
+    createPixel.className = 'base';
+    table.appendChild(createPixel);
+    
+    for (let index = 0; index < 5; index += 1) {
+        let createLine = document.createElement('th');
+        createLine.className = 'pixel';
+        createLine.style.backgroundColor = 'rgb(255, 255, 255)'
+        createPixel.appendChild(createLine);
+        
+    }
+}
