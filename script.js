de35cc0b7a0b1662005f8cb91bb0cf12df19b354
Rodolfo <rodolforrcont@gmail.com>
2021-04-09 16:21:57 -0300

    Requisitos Obrigatórios - 2

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-window.onload = function() {
-  let titulo = document.getElementById('title');
-  titulo.innerText = 'Paleta de Cores'
-}
+window.onload = function () {
+  const titulo = document.getElementById('title');
+  titulo.innerText = 'Paleta de Cores';
+
+  const divColors = document.querySelectorAll('.color');
+
+  function paleteColors () {
+    for (let index = 0; index < divColors.length; index += 1) {
+      const colorsValue = divColors[index].id;
+      divColors[index].style.backgroundColor = colorsValue;
+    }
+  }
+  paleteColors();
+};
