82daa4232887c78ca5464b04d0dde23251d32c25
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-09 16:17:39 -0300

    requisito 4 implementado

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function selectColors() {
-  const colors = ['black', 'blue', 'red', 'yellow', 'green'];
+  const colors = ['black', 'blue', 'red', 'green'];
   const paletlist = document.getElementsByClassName('color');
   for (let index = 0; index < paletlist.length; index += 1) {
     paletlist[index].style.backgroundColor = colors[index];
@@ -7,8 +7,19 @@ function selectColors() {
 }
 
 function createBoard() {
-  
+  const board = document.getElementById('pixel-board');
+  for (let index = 0; index < 5; index += 1) {
+    let line = document.createElement('div');
+    line.classList.add('line');
+    board.appendChild(line);
+    for (let secondIndex = 0; secondIndex < 5; secondIndex += 1) {
+      let pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      line.appendChild(pixel);
+    }
+  }
 }
 window.onload = function () {
   selectColors();
+  createBoard();
 };
