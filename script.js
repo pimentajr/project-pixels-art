1d4986fc51ff432c0124ce930062f096d0ce67a5
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-09 15:49:30 -0300

    Passo 2

---
+++ script.js
@@ -0,0 +1,5 @@
+let color = document.getElementsByClassName('color');
+color[0].style.backgroundColor = 'pink';
+color[1].style.backgroundColor = 'yellow';
+color[2].style.backgroundColor = 'lightblue';
+color[3].style.backgroundColor = 'lightgreen';
