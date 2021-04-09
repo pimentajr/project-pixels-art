dcf0ada203c64260d00f27f3af97dd43d3ee39c4
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-09 16:19:48 -0300

    Requisitos 7 Realizado

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 window.onload = function() {
 
   let boardSection = document.getElementById('pixel-board');
-
+  let colorPalette = document.getElementById('color-palette');
+  
   //Cria o quadro com os quadrados brancos;
   for (let index = 0; index < 25; index += 1) {
       let everyPixel = document.createElement('div');
       everyPixel.setAttribute('class', 'pixel');
       boardSection.appendChild(everyPixel);
   }
+
+  
+  colorPalette.addEventListener('click', changeSelectedColor);
+
+
+
 }
 
+
+function changeSelectedColor(event) {
+  let allDivsColorPallete = document.getElementById('color-palette').children;
+  for (let index = 0; index < allDivsColorPallete.length; index += 1) {
+    console.log(allDivsColorPallete[index].setAttribute('class', 'color'));
+  }
+  event.target.className += ' selected';
+}
 
