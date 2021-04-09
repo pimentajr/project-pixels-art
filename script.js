9b809a822e2c117a8df654f2e191faffd58b077d
Gabriela Azevedo <gabiazevedoms@gmail.com>
2021-04-09 17:30:11 -0300

    Adiciona requisito 2

---
+++ script.js
@@ -0,0 +1,14 @@
+const title = document.getElementById('title');
+title.style = 'font-family: New Tegomin';
+
+const blackSquare = document.getElementById('black');
+blackSquare.style = 'background-color: black';
+
+const redSquare = document.getElementById('red');
+redSquare.style = 'background-color: red';
+
+const orangeSquare = document.getElementById('orange');
+orangeSquare.style = 'background-color: orange';
+
+const yellowSquare = document.getElementById('yellow');
+yellowSquare.style = 'background-color: yellow';
