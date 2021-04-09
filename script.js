43f635a81c600ffe42359a179b7ff00a27953a6f
Vinicius <viniciusfod@gmail.com>
2021-04-09 20:41:51 -0300

    adiciona parte 7 do projeto

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const colorPalette = document.getElementById('color-palette');
 let colorToPaint = document.getElementsByClassName('selected')[0];
+let pixelBoard = document.getElementById('pixel-board');
 
 
 function createPixelsSize (size) {
@@ -7,11 +8,12 @@ function createPixelsSize (size) {
     for (let index = 0; index < size; index += 1) {
       let pixelColumn = document.createElement('div');
       pixelBox.appendChild(pixelColumn);
+      
 
       for (let index2 = 0; index2 < size; index2 += 1) {
         let pixelLine = document.createElement('div');
         pixelBox.appendChild(pixelLine);
-        pixelLine.className = 'pixel';
+        pixelLine.className = 'pixelVazio';
       }
     }
 }
@@ -22,9 +24,26 @@ function selectedColor (event) {
   colorToPaint = event.target;
   colorToPaint.classList.add('selected');
 }
-
 colorPalette.addEventListener('click', selectedColor);
 
-function changeColor (event) {
-  
+function printBoard() {
+  document.getElementById('pixel-board').addEventListener('click', (event) => {
+    const printBox = event.target;
+    switch (document.querySelector('.selected').id) {
+    case 'black-selector':
+      printBox.style.backgroundColor = 'black';
+      break;
+    case 'red-selector':
+      printBox.style.backgroundColor = 'red';
+      break;
+    case 'green-selector':
+      printBox.style.backgroundColor = 'green';
+      break;
+    case 'blue-selector':
+      printBox.style.backgroundColor = 'blue';
+      break;
+    default:
+    }
+  });
 }
+printBoard();
