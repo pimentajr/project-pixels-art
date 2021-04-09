aadcc4847f53e33b8d130af5b156e52e69f5176e
Danilo Pereira <danilodps22@hotmail.com>
2021-04-09 16:25:56 -0300

    Req. 2: Paleta de cores

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+window.onload = function() {
+
+  function creatPalette(colorTitle){
+    let colorPalette = document.querySelector('#color-palette');
+    for (let index = 0; index < colorTitle.length; index += 1){
+      let colorBox = document.createElement('div');
+      colorBox.className = colorTitle[index] + ' color';
+      colorBox.style.backgroundColor = colorTitle[index];
+      console.log(colorBox);
+      colorPalette.appendChild(colorBox);
+    }
+  }
+
+  let colors = ['black', 'red', 'blue', 'green']; 
+  creatPalette(colors);
+}
+
+
