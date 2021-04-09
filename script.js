6a41b7aeea16d29c3d9451076fa43c2ea76f12f6
Hugo Ferreira <hugonardo@gmail.com>
2021-04-09 18:33:55 -0300

    Requisito 8
    8. Clicar em um pixel dentro do quadro após selecionar uma cor na paleta
       faz com que o pixel seja preenchido com a cor selecionada

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ function getColorPallet() {
   return document.querySelector('#color-palette');
 }
 
+function getPixelBoard() {
+  return document.querySelector('#pixel-board');
+}
+
 function clearColorSelection() {
   const colorPallet = getColorPallet();
   for (let index = 0; index < colorPallet.childElementCount; index += 1) {
-    colorPallet.children[index].classList.remove(colorSelectedClass);
+    colorPallet
+      .children[index]
+      .classList
+      .remove(colorSelectedClass);
   }
 }
 
@@ -17,6 +24,13 @@ function restoreColorSelection() {
     .children[0]
     .classList
     .add(colorSelectedClass);
+}
+
+function getSelectedColor() {
+  return getComputedStyle(
+    getColorPallet().querySelector('.selected'),
+  )
+    .backgroundColor;
 }
 
 function selectColor(colorId) {
@@ -30,8 +44,25 @@ function onClickColorPallet(event) {
   selectColor(event.target.id);
 }
 
+function setPixelColor(pixel) {
+  const selectedColor = getSelectedColor();
+  console.log(selectedColor);
+  if (pixel.style.backgroundColor === selectedColor) {
+    return;
+  }
+
+  // Como resolver este lint warning?
+  // eslint-disable-next-line no-param-reassign
+  pixel.style.backgroundColor = selectedColor;
+}
+
+function onClickPixelBoard(event) {
+  setPixelColor(event.target);
+}
+
 function bindListeners() {
   getColorPallet().onclick = onClickColorPallet;
+  getPixelBoard().onclick = onClickPixelBoard;
 }
 
 window.onload = () => {
