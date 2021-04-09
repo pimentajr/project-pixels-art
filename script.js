e06e8b3c79b137f73cdd2db4cc559a1ae762cf3b
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-09 17:06:43 -0300

    requisito 3 e 4

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function() {
   for (let index = 1; index < getColors.length; index += 1) {
     getColors[index].style.backgroundColor = getRandomColors(arrayColors);
   }
-  
+  createBoard();
 } 
 
 let arrayColors = ['aqua', 'blue', 'fuchsia', 'gray', 'green','lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 'silver', 'teal', 'yellow'];
@@ -13,3 +13,18 @@ function getRandomColors (colors) {
   randomColor = colors[Math.floor((Math.random()*colors.length))];
   return randomColor;
 } 
+
+
+function createBoard() {
+  let board = document.getElementById('pixel-board');
+  for (let index = 0; index < 5; index += 1) {
+    let line = document.createElement('div');
+    line.classList.add('line');
+    board.appendChild(line);
+    for (let secondIndex = 0; secondIndex < 5; secondIndex += 1) {
+      let pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      line.appendChild(pixel);
+    }
+  }
+}
