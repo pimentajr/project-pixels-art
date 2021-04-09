a457522e41da7bf5c51fbe473b827d757c042dab
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-04-09 14:32:57 -0300

    Criando Paletas e Cores

---
+++ script.js
@@ -0,0 +1,36 @@
+const pixelIdentification = 'pixel-board'
+
+function createPaletteContainer(num) {
+  for (let index = 0; index < num; index += 1) {
+    const palettesBox = document.createElement('div');
+    const allPalettes = document.getElementById('color-palette');
+    allPalettes.appendChild(palettesBox);
+    palettesBox.className = 'color';
+  }
+}
+createPaletteContainer(4);
+
+function giveCollorPalettesBox() {
+  const paletteColor = document.getElementsByClassName('color');
+  paletteColor[0].style.backgroundColor = 'black';
+  paletteColor[1].style.backgroundColor = 'red';
+  paletteColor[2].style.backgroundColor = 'yellow';
+  paletteColor[3].style.backgroundColor = 'pink';
+  paletteColor[0].classList.add('selected');
+
+}
+giveCollorPalettesBox();
+
+function createPixels(size) {
+  const pixelBox = document.getElementById(pixelIdentification);
+  for (let index = 0; index < size; index += 1) {
+    const pixelColumn = document.createElement('div');
+    pixelBox.appendChild(pixelColumn);
+    for (let index = 0; index < size; index += 1) {
+      const pixelLine = document.createElement('div');
+      pixelColumn.appendChild(pixelLine);
+      pixelLine.className = 'pixel';
+    }
+  }
+}
+createPixel(5);
