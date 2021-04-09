41c9abf48e59c2106c0546e6bee0e2d442d64bba
igordmf <igordanielmf@gmail.com>
2021-04-09 14:54:24 -0300

    Adiciona requisito 1

---
+++ script.js
@@ -0,0 +1,5 @@
+let body = document.querySelector('body');
+let title = document.createElement('h1');
+title.setAttribute('id', 'title');
+title.innerText = 'Paleta de Cores';
+body.appendChild(title);
