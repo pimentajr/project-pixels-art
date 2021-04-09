fcc4a4a6a2db91360d7b565cb8b4b986da3a1af9
guinhoanjos <iago.anjos@hotmail.com>
2021-04-09 16:28:41 -0300

    pancando o lint do js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function createDiv(number) {
   for (let index = 0; index < number; index += 1) {
-    let createdDiv = document.createElement("div");
-    createdDiv.className = "pixels";
-    document.getElementById("pixel-board").appendChild(createdDiv)
-    }
+    const createdDiv = document.createElement('div');
+    createdDiv.className = 'pixels';
+    document.getElementById('pixel-board').appendChild(createdDiv);
+  }
 }
 createDiv(25);
