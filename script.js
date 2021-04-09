e100aa2c8d066562abda0b95ea25f7168c88854f
VictorS <czpask@gmail.com>
2021-04-09 17:31:38 -0300

    Exercicio 8

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ getColors.addEventListener('click', function(event) {
     event.target.className = 'color selected';
   } 
 })
+//set colors
+let getBlack = document.getElementById('black').style.backgroundColor = 'black';
+let getYellow = document.getElementById('yellow').style.backgroundColor = 'yellow';
+let getGreen = document.getElementById('green').style.backgroundColor = 'green';
+let getBlue = document.getElementById('blue').style.backgroundColor = 'blue';
 
+
+//8
+let getPixelBoard = document.querySelector('#pixel-board')
+getPixelBoard.addEventListener('click', function(event) {
+  let getSelected = document.querySelector('.selected');
+    if (event.target.className === 'pixel') {
+    event.target.style.backgroundColor = getSelected.style.backgroundColor;
+  } 
+})
