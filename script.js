9c99e80534854dc0122e42df97c7467a36bc6a3e
Víctor Faria <vitilevi@hotmail.com>
2021-04-09 16:15:40 -0300

    Feito requisito 4

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const liColors = document.getElementsByClassName('color');
-
+const pixelSection = document.getElementById('pixel-board');
 const colors = ['black', 'red', 'green', 'blue'];
 
 function makeLiColors(param) {
   const liProperties = param;
   for (let index = 0; index < liColors.length; index += 1) {
     liProperties[index].style.backgroundColor = colors[index];
-    liProperties[index].innerText = colors[index];
     liProperties[index].style.border = '1px solid black';
   }
 }
 
+function createDivBoard(param) {
+  const numberOfTiles = param;
+  for (let index = 0; index < numberOfTiles; index += 1) {
+    const tile = document.createElement('div');
+    tile.classList.add('pixel');
+    tile.style.float = 'left';
+    tile.style.border = '1px solid black';
+    pixelSection.appendChild(tile);
+  }
+}
+
 makeLiColors(liColors);
+createDivBoard(25);
