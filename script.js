fcbe157e7f762b31434a2c4035b5fd6150e80e55
Ghislaine <glenox79@gmail.com>
2021-04-09 15:07:04 -0300

    Exercicio 01

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // Exercicio 01
-const h1Title = document.createElement('h1')
-h1Title.id = 'title';
-h1Title.innerText = 'Paleta de Cores';
-document.body.appendChild(h1Title);
-
-
-
-
-
-
+function createH1Title() {
+  const h1Title = document.createElement('h1');
+  h1Title.id = 'title';
+  h1Title.innerText = 'Paleta de Cores';
+  document.body.appendChild(h1Title);
+}
+createH1Title();
