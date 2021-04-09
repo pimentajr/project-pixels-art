c5e7c60d15275caee2f305d698901fdc36a043c5
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-09 16:42:46 -0300

    Requisito 4: Ajuste para lint

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Requisito 3 - Criação da cor de paleta preta na primeira posição
 function addBlackColor() {
-	let firstColor = document.getElementById('green');
-	firstColor.style.backgroundColor = 'black';
+  const firstColor = document.getElementById('green');
+  firstColor.style.backgroundColor = 'black';
 }
 addBlackColor();
 
 // Requisito 4 - Criação do Pixel Board
 // 5 linhas div com 5 divs dentro - inline
 function createBoard() {
-	let pixelBoard = document.getElementById('pixel-board');
-	for (let index = 0; index < 5; index += 1) {
-		let lines = document.createElement('div');
-		lines.className = 'boardline';
-		pixelBoard.appendChild(lines);
-        for (let index = 0; index < 5; index += 1) {
-            let pixelItem = document.createElement('div');
-            pixelItem.className = 'pixel';
-            lines.appendChild(pixelItem);
-        }
-	}
+  const pixelBoard = document.getElementById('pixel-board');
+  for (let index = 0; index < 5; index += 1) {
+    const lines = document.createElement('div');
+    lines.className = 'boardline';
+    pixelBoard.appendChild(lines);
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      const pixelItem = document.createElement('div');
+      pixelItem.className = 'pixel';
+      lines.appendChild(pixelItem);
+    }
+  }
 }
-createBoard(); 
+createBoard();
