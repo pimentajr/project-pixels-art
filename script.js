c6254cea70307cecbe2f6885060fb5b500980579
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-09 18:40:37 -0300

    Correçoes para lint

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-window.onload = function () {
-  function primaryColors(colors) {
-    const colorBox = document.querySelectorAll('.color');
+function primaryColors(colors) {
+  const colorBox = document.querySelectorAll('.color');
 
-    for (let index = 0; index < colors.length; index += 1) {
-      colorBox[index].style.backgroundColor = colors[index];
-    }
+  for (let index = 0; index < colors.length; index += 1) {
+    colorBox[index].style.backgroundColor = colors[index];
   }
-
-  primaryColors(['black', 'yellow', 'green', 'blue']);
-
-  function pixelsTable (width, height) {
-    const tableWidth = width;
-    const tableHeight = height;
-    const table = document.querySelector('#pixel-board');
-    table.style.width = width * 40 + 'px';
-    table.style.height = height * 40 + 'px';
-    for (let index = 1; index <= height; index += 1) {
-      let newColumn = document.createElement('tr');
-      for (let index2 = 1; index2 <= width; index2 += 1) {
-        let newLine = document.createElement('td');
-        newLine.className = 'pixel white';
-        newColumn.appendChild(newLine);
-      }
-      table.appendChild(newColumn);
-    }
-  }
-
-  pixelsTable (5, 5);
 }
+
+primaryColors(['black', 'yellow', 'green', 'blue']);
+
+function pixelsTable(width, height) {
+  const table = document.querySelector('#pixel-board');
+  table.style.width = [width * 40, 'px'].join('');
+  table.style.height = [height * 40 + 'px'].join('');
+  for (let index = 1; index <= height; index += 1) {
+    const newColumn = document.createElement('tr');
+    for (let index2 = 1; index2 <= width; index2 += 1) {
+      const newLine = document.createElement('td');
+      newLine.className = 'pixel white';
+      newColumn.appendChild(newLine);
+    }
+    table.appendChild(newColumn);
+  }
+}
+
+pixelsTable(5, 5);
