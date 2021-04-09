2b2c7b3f12d4d34fd306f28be78f233c83d8fa62
Daniel <danielfdoficial@gmail.com>
2021-04-09 16:09:55 -0300

    conclui desafio 4

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const elemento = document.querySelectorAll('.color');
-	elemento[0].style.backgroundColor = 'black';
-	elemento[1].style.backgroundColor = 'yellow';
-	elemento[2].style.backgroundColor = 'blue';
-	elemento[3].style.backgroundColor = 'orange';
+elemento[0].style.backgroundColor = 'black';
+elemento[1].style.backgroundColor = 'yellow';
+elemento[2].style.backgroundColor = 'blue';
+elemento[3].style.backgroundColor = 'orange';
+
+const quadro = document.querySelector('#pixel-board');
+for (let index = 0; index < 26; index += 1) {
+	const elementsQ = document.createElement('div');
+	elementsQ.classList.add('pixel');
+	quadro.appendChild(elementsQ);
+}
