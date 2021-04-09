700a4edf64bcb5558c0ed7388391238675e22449
Raphael <raphaellfpg@gmail.com>
2021-04-09 17:52:43 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,14 @@
+function createPixel(tagName) {
+    let element = document.createElement(tagName);
+    return element
+}
+
+let pixelBoard = document.querySelector('#pixel-board');
+
+for (let index = 1; index <= 25; index += 1) {
+    let pixel = pixelBoard.appendChild(createPixel('div'));
+    pixel.className = 'pixel'
+    pixel[index]
+}
+
+console.log(pixelBoard)
