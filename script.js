043600ce9fa481d500be21494dd4c20ee776e295
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-04-09 17:48:00 -0300

    Desafio 4 concluido - Matriz de pixels criada

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+  function createPixel(column, line, pixelBoard) {
+    for (let index = 0; index < column; index += 1) {
+      for (let jIndex = 0; jIndex < line; jIndex += 1) {
+        const elementCreated = document.createElement('div');
+        elementCreated.className = 'pixel color white';
+        console.log(elementCreated);
+        pixelBoard.appendChild(elementCreated);
+      }
+    }
+  }
+
+  const columnElements = 5;
+  const lineElements = 5;
+  const pixelBoard = document.querySelector('#pixel-board');
+
+  createPixel(columnElements, lineElements, pixelBoard);
+};
