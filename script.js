44963f62b2290205d8f40f2f25c014981b0e13b5
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-04-09 15:26:03 -0300

    Remove o style backgroundColor via javascript do elemento pixel

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ for (let index = 0; index < boardLength; index += 1) {
   const pixelDiv = document.createElement('div');
 
   pixelDiv.className = 'pixel';
-  pixelDiv.style.backgroundColor = 'white';
 
   pixelBoard.appendChild(pixelDiv);
 }
