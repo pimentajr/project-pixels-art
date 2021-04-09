271e475394993950c9560e8ac45077d254abde55
Guilherme Nunes <guiicnunes@gmail.com>
2021-04-09 16:16:00 -0300

    Implementando o Display Table

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function () {
   function makePixelBoard(parent, line, column) {
     for (let index = 1; index <= line; index += 1) {
-      let divLine = document.createElement('div');
+      const divLine = document.createElement('div');
       divLine.class = 'pixel-board-line';
       for (let indexLine = 1; indexLine <= column; indexLine += 1) {
-        let divPixel = document.createElement('div');
+        const divPixel = document.createElement('div');
         divPixel.className = 'pixel';
         divLine.appendChild(divPixel);
       }
