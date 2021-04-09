72e3ecf133c3dd2118ae818cf8205ac809c0eb11
VictorS <czpask@gmail.com>
2021-04-09 16:25:32 -0300

    Exercicio 7

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//6
+//7
+let getColors = document.querySelector('#color-palette');
+getColors.addEventListener('click', function(event) {
+  let getSelected = document.querySelector('.selected');
+  if (getSelected.classList[0] === 'color') {
+    getSelected.className = 'color';
+    event.target.className = 'color selected';
+  } 
+})
 
-window.onload = function() {
-  let getBlack = document.querySelector('.black');
-  if (getBlack.className === 'color black selected') {
-    getBlack.className = 'color black';
-  } else {
-    getBlack.className = 'color black selected';
-  }
-}
