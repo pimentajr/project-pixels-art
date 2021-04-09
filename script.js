030f11de9399fe32204802f02fed69cf83880eb3
Júlio Mota <juliomotacampos@gmail.com>
2021-04-09 16:10:01 -0300

    Refatoração - Inclusão das cores por JS - Requisito 1 ao 3

---
+++ script.js
@@ -0,0 +1,8 @@
+let colorPalette = document.querySelector('#color-palette');
+let colors = ['black', 'red', 'green', 'blue'];
+
+for (let index = 0; index < colorPalette.children.length; index += 1) {
+  colorPalette.children[index].innerHTML = colors[index];
+  colorPalette.children[index].style.backgroundColor = colors[index];
+  colorPalette.children[index].style.color = colors[index]; 
+}
