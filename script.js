3841c97111a59fcdc2b17aae2c2ae56ecfc632b4
Daniel <danielfdoficial@gmail.com>
2021-04-09 15:26:36 -0300

    conclui o desafio 2

---
+++ script.js
@@ -0,0 +1,5 @@
+let elemento = document.querySelectorAll('.color');
+elemento[0].style.backgroundColor = 'red';
+elemento[1].style.backgroundColor = 'yellow';
+elemento[2].style.backgroundColor = 'blue';
+elemento[3].style.backgroundColor = 'orange';
