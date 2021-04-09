5a354f05c80e76f9799113f63f1d5b14caf6feaf
Julio Cesar <juliocesar@pop-os.localdomain>
2021-04-09 15:56:18 -0300

    ex4 first step

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-
-
 window.onload = function(){
+const lines = 5;
 
-let boxes = document.querySelectorAll('.color');
-const black = boxes[0];
-black.style.backgroundColor = 'black';
-const blue = boxes[1];
-blue.style.backgroundColor = 'lightblue';
-const grey = boxes[2];
-grey.style.backgroundColor = 'lightgrey';
-const green = boxes[3];
-green.style.backgroundColor = 'lightgreen';
+function colorBoxes(){
+    let boxes = document.querySelectorAll('.color');
+    const black = boxes[0];
+    black.style.backgroundColor = 'black';
+    const blue = boxes[1];
+    blue.style.backgroundColor = 'lightblue';
+    const grey = boxes[2];
+    grey.style.backgroundColor = 'lightgrey';
+    const green = boxes[3];
+    green.style.backgroundColor = 'lightgreen';
+
+}
+colorBoxes();
+
+function pixelBoard(){
+    let lineBoard = document.querySelectorAll('.line');
+    for (let i = 0; i < lines; i++){
+        let pixelBox = document.createElement('div');
+        pixelBox.className = 'pixel box';
+        pixelBox.style.backgroundColor = 'white';
+        lineBoard[i].appendChild(pixelBox)
+    }
+}
+pixelBoard();
+
 }
