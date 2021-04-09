36e591bd6c6d23b4c65c31ca78553c13f817a1a2
Tales Coelho <tales-coelho@live.com>
2021-04-09 15:21:58 -0300

    criando uma board de quadrados

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ window.onload = function() {
 //cria uma linha de quadrados
 function createLineSquares(lineLength) {
   let pixelBoard = document.getElementById('pixel-board');
-  let squares = [];
-  let line = document.createElement('div');
-  line.className = 'container';
-  pixelBoard.appendChild(line);
-  for (let index = 0; index < lineLength; index += 1) {
-    squares[index] = document.createElement('div');
-    squares[index].className = 'color pixel';
-    line.appendChild(squares[index]);
+  for (let lineIndex = 0; lineIndex < lineLength; lineIndex += 1) {
+    let squares = [];
+    let lines = [];
+    lines[lineIndex] = document.createElement('div');
+    lines[lineIndex].className = 'container';
+    pixelBoard.appendChild(lines[lineIndex]);
+    for (let index = 0; index < lineLength; index += 1) {
+      squares[index] = document.createElement('div');
+      squares[index].className = 'pixel color';
+      lines[lineIndex].appendChild(squares[index]);
+    }
   }
+  
 }
