2410f30ab094ff6ec6f96ced56190efdef7d6ea3
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-09 16:28:00 -0300

    requisito 1 concluído

---
+++ script.js
@@ -0,0 +1,6 @@
+const tagH1 = document.createElement('h1');
+tagH1.innerText = 'Paleta de Cores';
+document.body.appendChild(tagH1);
+
+const idTitle = document.querySelector('h1')
+idTitle.id = 'title';
