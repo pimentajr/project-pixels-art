3a6d0db9f18989f7c50e4aaa3865633e99e08da1
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-04-09 16:30:07 -0300

    creat pixel board html

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const colorPalette = document.querySelector('#color-palette');
-
 function creatPallete() {
-  let colors = ['black', 'red', 'orange', 'yellow'];
-
+  const colorPalette = document.querySelector('#color-palette');
+  const colors = ['black', 'red', 'orange', 'yellow'];
+  
   for (let index = 0; index < colors.length; index += 1) {
-    let createDiv = document.createElement('div');
+    const createDiv = document.createElement('div');
     createDiv.className = 'color';
     createDiv.style.backgroundColor = colors[index];
     colorPalette.appendChild(createDiv);
@@ -13,9 +12,9 @@ function creatPallete() {
 creatPallete();
 
 function pixelBoard() {
-  let createpixelBoard = document.createElement('div');
-  createpixelBoard.id = 'pixel-board';
-  createpixelBoard.className = 'pixel';
-  colorPalette.appendChild(createpixelBoard);
+  const pixelBoard = document.querySelector('#pixel-board');
+  const creatPixel = document.createElement('div');
+  creatPixel.className = 'pixel';
+  pixelBoard.appendChild(creatPixel);
 }
 pixelBoard();
