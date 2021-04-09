82949834e0bd2d0f9394a3f6cf074a0fed43714e
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-09 17:32:31 -0300

    totalmente perdido no requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const blackColor = document.querySelector('#black');
 blackColor.classList.add('selected');
+
+const colorPalette = document.querySelectorAll('.selected');
+function selectColor() {
+  for (let index = 0; index < colorPalette.length; index += 1) {
+    if (colorPalette.className === 'color selected') {
+      colorPalette.classList.remove('selected');
+    } else {
+      colorPalette.classList.add('selected');
+    }
+  }
+}
+colorPalette.addEventListener('click', selectColor);
