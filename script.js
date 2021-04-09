3fbd7ea99c816af9e8096f191a4995bfb217d5c4
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-04-09 16:02:33 -0300

    Corrigindo lint

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-let black = document.querySelector('.black');
-let green = document.querySelector('.green');
-let grey = document.querySelector('.grey');
-let magenta = document.querySelector('.magenta');
-let paleta = document.querySelector('#color-palette');
+const black = document.querySelector('.black');
+const green = document.querySelector('.green');
+const grey = document.querySelector('.grey');
+const magenta = document.querySelector('.magenta');
+const paleta = document.querySelector('#color-palette');
 
 black.className = 'color black selected';
 
 paleta.addEventListener('click', function color(event) {
-  let color = event.target;
+  const color = event.target;
   black.classList.remove('selected');
   green.classList.remove('selected');
   grey.classList.remove('selected');
   magenta.classList.remove('selected');
-  let colorName = color.classList[1];
-  color.className = 'color' + ' ' + colorName + ' selected';
+  const colorName = color.classList[1];
+  color.className = `color ${colorName} selected`;
   return colorName;
 });
 
 //  ---------------------
-let board = document.querySelector('#pixel-board');
+const board = document.querySelector('#pixel-board');
 
 board.addEventListener('click', function (event) {
-  let pixel = event.target;
-  let itemColor = document.querySelector('.selected');
-  let color = itemColor.classList[1];
+  const pixel = event.target;
+  const itemColor = document.querySelector('.selected');
+  const color = itemColor.classList[1];
   pixel.style.backgroundColor = color;
 });
 
-let clear = document.querySelector('#clear-board');
+const clear = document.querySelector('#clear-board');
 
 clear.addEventListener('click', function () {
-  let pixels = document.querySelectorAll('.pixel');
-  for (let pixel of pixels) {
+  const pixels = document.querySelectorAll('.pixel');
+  for (const pixel of pixels) {
     pixel.style.backgroundColor = 'white';
   }
 });
