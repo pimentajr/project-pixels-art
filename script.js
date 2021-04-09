b7801a4c262acbc5f523d830f14fef6b5e39dc3c
Ana Carolina Cavalheiro Giorgiani <cavalheiro.anacarolina@gmail.com>
2021-04-09 15:38:35 -0300

    Desafio 1 Project Pixels

---
+++ script.js
@@ -0,0 +1,5 @@
+const textBody = document.querySelector('body');
+const title = document.createElement('h1');
+title.innerText = 'Paleta de Cores';
+title.id = 'title';
+textBody.appendChild(title);
