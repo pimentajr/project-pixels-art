0a324d746c5ad807c821acb706222db0009584ef
Tales Coelho <tales-coelho@live.com>
2021-04-09 17:10:57 -0300

    ajustando as linhas

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-window.onload = function() {
-  createLineSquares(5);
-  const colorPaletteId = document.getElementById('color-palette');
-  colorPaletteId.addEventListener('click', function(event) {
-    let selectedElement = document.querySelector('.selected');
-    if(event.target.classList[0] === 'color') {
-      selectedElement.classList.remove('selected');
-      event.target.classList.add('selected');
-    }
-  });
-  const pixelBoard = document.getElementById('pixel-board');
-  pixelBoard.addEventListener('click', function(event) {
-    let selectedElement = document.querySelector('.selected');
-    let color = selectedElement.classList[1];
-    if(event.target.classList[0] === 'pixel') {
-      event.target.className = 'pixel color';
-      event.target.classList.add(`${color}`);
-    }
-  });
-  const clearPixelBoard = document.getElementById('clear-board');
-  clearPixelBoard.addEventListener('click', clearBoard);
-}
-
-//cria uma linha de quadrados
+// cria uma linha de quadrados
 function createLineSquares(lineLength) {
-  let pixelBoard = document.getElementById('pixel-board');
+  const pixelBoard = document.getElementById('pixel-board');
   for (let lineIndex = 0; lineIndex < lineLength; lineIndex += 1) {
-    let squares = [];
-    let lines = [];
+    const squares = [];
+    const lines = [];
     lines[lineIndex] = document.createElement('div');
     lines[lineIndex].className = 'container';
     pixelBoard.appendChild(lines[lineIndex]);
@@ -38,9 +15,43 @@ function createLineSquares(lineLength) {
   }
 }
 
+// Seleciona uma cor da minha paleta de cores
+function selectColor() {
+  const colorPaletteId = document.getElementById('color-palette');
+  colorPaletteId.addEventListener('click', (event) => {
+    const selectedElement = document.querySelector('.selected');
+    if (event.target.classList[0] === 'color') {
+      selectedElement.classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  });
+}
+
+// Pinta o quadrado selecionado
+function paintPixel() {
+  const pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.addEventListener('click', (event) => {
+    const selectedElement = document.querySelector('.selected');
+    const color = selectedElement.classList[1];
+    if (event.target.classList[0] === 'pixel') {
+      event.target.className = 'pixel color';
+      event.target.classList.add(`${color}`);
+    }
+  });
+}
+
+// cria uma função que limpa o board
 function clearBoard() {
-  let board = document.querySelectorAll('.pixel');
-  for (let index = 0; index < board.length; index++) {
+  const board = document.querySelectorAll('.pixel');
+  for (let index = 0; index < board.length; index += 1) {
     board[index].className = 'pixel color';
   }
 }
+
+window.onload = function () {
+  createLineSquares(5);
+  selectColor();
+  paintPixel();
+  const clearPixelBoard = document.getElementById('clear-board');
+  clearPixelBoard.addEventListener('click', clearBoard);
+};
