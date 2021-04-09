df66a0264f53ef5caaebd051db2e7aa94f6089b0
Ruben Lopes <rubenssouss@hotmail.com>
2021-04-09 18:07:29 -0300

    Starting pixel table

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function () {
   const colorPalette = document.getElementById('color-palette');
-  const arrColors = ['black', 'blue', 'magenta', 'yellow', 'red'];
+  const arrColors = ['black', 'blue', 'magenta', 'yellow'];
+  const pixelSquad = document.getElementById('pixel-board');
+  let pixelSquadSize = 25;
 
   for (let index = 0; index < arrColors.length; index += 1) {
     const colorPixel = document.createElement('div');
@@ -10,5 +12,27 @@ window.onload = function () {
     colorPixel.style.margin = '5px';
     colorPixel.style.border = 'solid 1px black';
     colorPalette.appendChild(colorPixel);
+  }
+
+  for (let index = 0; index < Math.sqrt(pixelSquadSize); index += 1) {
+    // const pixel = document.createElement('div');
+    // pixel.className = 'pixel';
+    // // colorPixel.classList.add(arrColors[index]);
+    // // colorPixel.style.backgroundColor = arrColors[index];
+    // // colorPixel.style.margin = '5px';
+    // pixel.style.border = 'solid 1px black';
+    // pixelSquad.appendChild(pixel);
+    let arrPixels = [];
+
+    for (let index2 = 1; index2 < Math.sqrt(pixelSquadSize); index2 += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      // colorPixel.classList.add(arrColors[index]);
+      // colorPixel.style.backgroundColor = arrColors[index];
+      // colorPixel.style.margin = '5px';
+      pixel.style.border = 'solid 1px black';
+      arrPixels.push(pixel);
+      pixelSquad.appendChild(arrPixels);
+    }
   }
 };
