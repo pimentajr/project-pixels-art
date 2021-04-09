58f80a0d1f715c9ce11a9b9b4fa61bd91fee5a2d
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-09 15:25:38 -0300

    Requisito 4 e 5

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-window.onload = function () {
-  const colorPalette = document.querySelector('#color-palette');
-  const colors = ['black', 'blue', 'yellow', 'magenta'];
-  for (let index = 0; index < 4; index += 1) {
-    const colorBox = document.createElement('div');
-    colorBox.className = 'color';
-    colorBox.style.backgroundColor = colors[index];
-    colorPalette.appendChild(colorBox);
+const colorPalette = document.querySelector('#color-palette');
+const colors = ['black', 'blue', 'yellow', 'magenta'];
+for (let index = 0; index < 4; index += 1) {
+  const colorBox = document.createElement('div');
+  colorBox.className = 'color';
+  colorBox.style.backgroundColor = colors[index];
+  colorPalette.appendChild(colorBox);
+}
+
+const pixelBoard = document.querySelector('#pixel-board');
+const lines = 5;
+const columns = 5;
+for (let lineIndex = 0; lineIndex < lines; lineIndex += 1) {
+  const newLine = document.createElement('div');
+  pixelBoard.appendChild(newLine);
+  for (let columnIndex = 0; columnIndex < columns; columnIndex += 1) {
+    const newPixel = document.createElement('div');
+    newPixel.className = 'pixel';
+    newLine.appendChild(newPixel);
   }
-};
+}
