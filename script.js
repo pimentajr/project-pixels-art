424d63d3c693fc1d33327bf4a77bc47033fd99f3
Julio Cesar <juliocesar@pop-os.localdomain>
2021-04-09 15:20:51 -0300

    ex2 concluido

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // 1 - Adicione à página o título "Paleta de Cores" - okay
 // 2 - Adicione à página uma paleta de quatro cores distintas.
+
+window.onload = function(){
+
+let boxes = document.querySelectorAll('.color');
+let pink = boxes[0];
+pink.style.backgroundColor = 'pink';
+let blue = boxes[1];
+blue.style.backgroundColor = 'lightblue';
+let grey = boxes[2];
+grey.style.backgroundColor = 'lightgrey';
+let green = boxes[3];
+green.style.backgroundColor = 'lightgreen';
+}
