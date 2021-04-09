34a8dfdb2ce16482123d41d65ecac47ac4dbe2d5
GustavoReginato <gureginatopd@gmail.com>
2021-04-09 17:32:45 -0300

    adiciona algumas coisas no css

--- script.js
+++
@@ -1,6 +0,0 @@
-//exercicio 7
-//o metodo getComputedStyle obtem as propriedades e valores CSS reais do elemento especificado, o estilo calculado é o estilo usado na exibição do elemento;
-pixelBoard.addEventListener("click", function(event){
-  let selected = document.querySelector(".selected");
-  event.target.style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
-});
