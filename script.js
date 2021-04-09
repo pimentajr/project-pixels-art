668a346acc15df0984a33e820cb8a40c77f6bb2a
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-04-09 17:09:59 -0300

    Adicionando pixels

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function setColorPalette() {
-  let color = document.querySelectorAll('.color');
-  let arrColors = ['black', 'red', 'blue', 'yellow'];
-
-  for (let index in arrColors) {
+  const color = document.querySelectorAll('.color');
+  const arrColors = ['black', 'red', 'blue', 'yellow'];
+  for (const index in arrColors) {
     color[index].style.backgroundColor = arrColors[index];
   }
-  console.log(color)
+  document.getElementsByClassName('color')[0].classList.add('selected');
+  console.log(color);
 }
 
 setColorPalette();
 
+function generatePixels() {
+  const board = document.querySelector('#pixel-board');
+
+  for (let index = 0; index < 25; index += 1) {
+    const div = document.createElement('div');
+    div.classList.add('pixel');
+    board.appendChild(div);
+  }
+}
+
+generatePixels();
