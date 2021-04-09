17befec8b80470b1edd74bead4b38ca05cec1f5f
murilodospacos <murilopacos@gmail.com>
2021-04-09 20:11:05 -0300

    Exercicio 4  tentativa

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const myPage = document.querySelectorAll('body');
+/*window.onload = function () {
+  function createBoard (rows, colunms) {
+    const myBoard = document.getElementById('pixel-board');
 
-const colorDivsPaletes = document.querySelectorAll('#color-palete');
+    for (let index = 0; index < rows; index += 1) {
+      let row = document.createElement('div');
+
+      for (let indexCol = 0; index < colunms; indexCol += 1) {
+      let myColunms = document.createElement('div');
+      myColunms.classList.add('pix')
+      rows.appendChild(mycolunms);
+      }
+      myBoard.appendChild(row);
+    }
+  }
+  console.log(createBoard(5, 5));
+}
+*/
