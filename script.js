c84f810205c4020053f78eea0b5ba4d0d55806f6
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-09 17:00:23 -0300

    Feito requisito obrigatório 4-Correções de nomes

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ setColorToPalete();
 const pixelBoardSize = 5;
 
 for (let lineIndex = 1; lineIndex <= pixelBoardSize; lineIndex += 1) {
-  const createColumnDivs = document.createElement('div');
-  createColumnDivs.className = 'column-pixel';
+  const createLineDivs = document.createElement('div');
+  createLineDivs.className = 'line-pixel';
   for (let columnIndex = 1; columnIndex <= pixelBoardSize; columnIndex += 1) {
     const createDivs = document.createElement('div');
     createDivs.className = 'pixel';
-    createColumnDivs.appendChild(createDivs);
+    createLineDivs.appendChild(createDivs);
   }
-  pixelBoard.appendChild(createColumnDivs);
+  pixelBoard.appendChild(createLineDivs);
 }
