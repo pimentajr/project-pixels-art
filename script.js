1ae2e11d544e01d73293b83070509869abad5bb2
Edson <edson.exe@outlook.com>
2021-04-09 17:16:43 -0300

    Desafio 7

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 window.onload = function () {
   const pixelBoard = document.querySelector('#pixel-board').children;
+  const colorPalette = document.querySelector('#color-palette');
   let black = document.querySelector('#black');
-
   black.classList.add('selected');  
-  
   const quant = 5;
   
   for (let index = 0; index < quant; index += 1) {
@@ -13,4 +12,13 @@ window.onload = function () {
       pixelBoard[index].appendChild(pixel);
     }
   }
-};
+
+  colorPalette.addEventListener('click', function(c) {
+    document.querySelector('.selected').classList.remove('selected');
+    c.target.classList.add('selected');
+  })
+
+
+
+}
+
