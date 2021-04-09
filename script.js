90d7fce2a31d62984548acf45de73c8a3cd7b8a4
VictorS <czpask@gmail.com>
2021-04-09 19:24:24 -0300

    Correcao lint1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//7
+//7 
 let getColors = document.querySelector('#color-palette');
 getColors.addEventListener('click', function (event) {
   let getSelected = document.querySelector('.selected');
@@ -6,35 +6,34 @@ getColors.addEventListener('click', function (event) {
     getSelected.className = 'color';
     event.target.className = 'color selected';
   }
-})
+});
 //set colors
-let getBlack = document.getElementById('black').style.backgroundColor = 'black';
-let getYellow = document.getElementById('yellow').style.backgroundColor = 'yellow';
-let getGreen = document.getElementById('green').style.backgroundColor = 'green';
-let getBlue = document.getElementById('blue').style.backgroundColor = 'blue';
+document.getElementById('black').style.backgroundColor = 'black';
+document.getElementById('yellow').style.backgroundColor = 'yellow';
+document.getElementById('green').style.backgroundColor = 'green';
+document.getElementById('blue').style.backgroundColor = 'blue';
 
 let getWhitePixel = document.getElementsByClassName('pixel');
 for (let i = 0; i < getWhitePixel.length; i += 1) {
-  let eachPixel = getWhitePixel[i]
+  let eachPixel = getWhitePixel[i];
   eachPixel.style.backgroundColor = 'white';
 }
 
-
 //8
-let getPixelBoard = document.querySelector('#pixel-board')
+let getPixelBoard = document.querySelector('#pixel-board');
 getPixelBoard.addEventListener('click', function (event) {
   let getSelected = document.querySelector('.selected');
   if (event.target.className === 'pixel') {
     event.target.style.backgroundColor = getSelected.style.backgroundColor;
   }
-})
+});
 
 //9
 let getButton = document.querySelector('#clear-board');
 getButton.addEventListener('click', function () {
   let getWhitePixel = document.getElementsByClassName('pixel');
   for (let i = 0; i < getWhitePixel.length; i += 1) {
-    let eachPixel = getWhitePixel[i]
+    let eachPixel = getWhitePixel[i];
     eachPixel.style.backgroundColor = 'white';
   }
-})
+});
