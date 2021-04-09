1b0cdb8a4b6d45961dcfdb58697441b503143327
guinhoanjos <iago.anjos@hotmail.com>
2021-04-09 16:01:44 -0300

    criado funcao q cria divs

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function createDivBlockInline (number) {
+function createDivBlockInline (number, local) {
     for(let index = 0; index < number; index += 1) {
         let createdDiv = document.createElement('div');
-        createdDiv.className = "pixels"
+        createdDiv.className = "pixels";
+        local.appendChild(createdDiv);
     }
 }
-function createDivBlock (numberBlock) {
-    for(let index2 = 0; index2 < numberBlock; index2 += 1) {
-       let divsIline = createDivBlockInline(5);
+let boardPixelLocate = document.getElementById("pixel-board");
+
+function createDivBlockParent (number2, local2) {
+    for(let index2 = 0; index2 < number2; index2 += 1) {
+        let createdDiv2 = document.createElement('div');
+        createdDiv2.className = "pixels pixel-parent";
+        local2.appendChild(createdDiv2);
+        let pixelBoardparent = document.querySelectorAll(".pixel-parent")[index2];
+        createDivBlockInline(5, pixelBoardparent);
     }
 }
+createDivBlockParent(5, boardPixelLocate)
