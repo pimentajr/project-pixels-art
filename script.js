776e51b325102c181370918e28f6264b50b6d989
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-09 19:28:37 -0300

    requisitos bonus

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
+window.onload = function() {
+ 	boardGenerator(5);
+};
+
 var actualMouseColor = "Black"
 const colorPallete = document.getElementById("color-palette")
 const pixelBoard = document.getElementById("pixel-board")
 const palleteColors = document.getElementsByClassName("color");
 const pixels = document.getElementsByClassName("pixel")
 const button = document.getElementById("clear-board")
+const createBoardButton = document.querySelector("#generate-board");
+const inputValue = (document.querySelector("input").value);
+
+
+function boardGenerator(value){
+	// pixelBoard.style.width = ((value * 42)) + "px";
+	// pixelBoard.style.height = ((value * 40) + 26) + "px";
+	if(isNaN(value)){
+		window.alert("Board inválido!");
+		return false
+	}else if(value > 50){
+		value = 50;
+	}else if(value < 5){
+		value = 5;
+	}
+
+
+	const pixelQuant = pixels.length
+	const br = document.getElementsByTagName("br");
+	const brQuant = br.length;
+
+	for(let index = 0; index < pixelQuant; index += 1){
+		pixelBoard.removeChild(pixels[0])
+	}
+	
+	for(let index = 0; index < brQuant; index += 1){
+		pixelBoard.removeChild(br[0])
+	}
+
+	for(let index = 0; index < value*value; index += 1){
+		if(index > 1 && index % value === 0){
+			const brNew = document.createElement("br");
+			pixelBoard.appendChild(brNew);
+		}
+		const pixel = document.createElement("div");
+		pixel.className = "pixel"
+		pixelBoard.appendChild(pixel);
+	}
+}
+
+createBoardButton.addEventListener("click", function(){
+	const inputValue = parseInt(document.querySelector("input").value);
+	boardGenerator(inputValue);
+})
 
 function colorPalletDefinition(){
 	for(let index = 0; index < palleteColors.length; index += 1){
@@ -34,9 +82,12 @@ addEventListener("click", getColor);
 function paint(element){
 	const paintedBlock = element.target;
 	if(paintedBlock.parentNode === pixelBoard){
-		if(actualMouseColor !== ""){
+		if(actualMouseColor === paintedBlock.style.backgroundColor){
+			paintedBlock.style.backgroundColor = "white";
+		}else if(actualMouseColor !== ""){
 			paintedBlock.style.backgroundColor = actualMouseColor;
 		}
+		
 	}
 }
 
