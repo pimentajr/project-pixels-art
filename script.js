0f136bdd7a102a3cafe54bed61cbb15b2108c1dd
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-04-09 18:38:44 -0300

    lol

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const magenta = document.querySelector('.magenta');
 const paleta = document.querySelector('#color-palette');
 const board = document.querySelector('#pixel-board');
 
+function generateDiv() {
+  for (let i = 0; i < 25; i += 1) {
+    if (i % 5 === 0) {
+      let br = document.createElement('br');
+      board.appendChild(br);
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    board.appendChild(newDiv);
+  }
+}
+generateDiv();
+
 black.className = 'color black selected';
 paleta.addEventListener('click', function (event) {
   const color = event.target;
@@ -36,3 +49,33 @@ clear.addEventListener('click', function () {
 });
 
 //  ---------------------- EX 10
+
+const newBoard = document.createElement('div');
+let btn = document.querySelector('#generate-board');
+btn.addEventListener('click', function () {
+  const board = document.querySelector('#pixel-board');
+  board.remove();
+  newBoard.id = 'pixel-board';
+  document.querySelector('.board-container').appendChild(newBoard);
+  let input = document.querySelector('#board-size');
+  let value = input.value;
+  if (value === '') {
+    alert('Board inválido!');
+  }
+  for (let i = 0; i < parseInt(value * value); i += 1) {
+    if (i % value === 0) {
+      let br = document.createElement('br');
+      newBoard.appendChild(br);
+    }
+    let newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    newBoard.appendChild(newDiv);
+  }
+});
+
+newBoard.addEventListener('click', function (event) {
+  const pixel = event.target;
+  const itemColor = document.querySelector('.selected');
+  const color = itemColor.classList[1];
+  pixel.style.backgroundColor = color;
+});
