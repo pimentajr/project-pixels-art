38f21fe7f8534164364821d209f66d391148b46c
Gustavo Lemes <gustavolemes997@gmail.com>
2021-04-09 15:29:49 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function createPalletColorDiv(color) {
+function createPaletteColorDiv(color) {
   const palletColorDiv = document.createElement('div');
   palletColorDiv.className = 'color';
   palletColorDiv.style.backgroundColor = color;
-  document.querySelector('#color-pallete').appendChild(palletColorDiv);
+  document.querySelector('#color-palette').appendChild(palletColorDiv);
+}
+function createBoardPixels() {
+  const boardPixelsDiv = document.createElement('div');
+  boardPixelsDiv.className = 'pixel';
+  document.querySelector('#pixel-board').appendChild(boardPixelsDiv);
 }
 
-createPalletColorDiv('black');
-createPalletColorDiv('yellow');
-createPalletColorDiv('red');
-createPalletColorDiv('green');
+for (let index = 1; index <= 25; index += 1) {
+  createBoardPixels();
+}
+
+createPaletteColorDiv('black');
+createPaletteColorDiv('yellow');
+createPaletteColorDiv('red');
+createPaletteColorDiv('green');
