b8ad160059136ca20c4db6066ae29e8d10779a4a
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-04-09 19:09:10 -0300

    s

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // transformando os quadrados em cores
-// let colors = document.querySelectorAll('.color');
+ const colors = document.querySelectorAll('.color');
 const color1 = document.querySelectorAll('.color')[0];
 const color2 = document.querySelectorAll('.color')[1];
 const color3 = document.querySelectorAll('.color')[2];
@@ -10,5 +10,12 @@ color3.style.backgroundColor = 'gold';
 color4.style.backgroundColor = 'red';
 
 // criando o botão na paleta
-window.onload = '.selected';
-document.getElementById('color black').className = '.selected';
+
+function chooseColor() {
+   const chosenColor = ['black', 'purple', 'gold', 'red'];
+  for(let index = 0; index < chooseColor.length; index += 1){
+     colors[index].style.backgroundColor =chosenColor[index];
+  }
+  document.getElementsByClassName('colors')[0].classList.add('selected');
+  console.log(colors);
+}
