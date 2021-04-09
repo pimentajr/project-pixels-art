7489a2b9002d03a4a6743cd00ce0788c5cec0749
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-09 16:24:52 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  function primaryColors(colors) {
+    let colorBox = document.querySelectorAll('.color');
+
+    for (let index = 0; index < colors.length; index += 1) {
+      colorBox[index].style.backgroundColor = colors[index];
+    }
+  }
+
+  primaryColors(['green', 'yellow', 'black', 'blue']);
+
+}
