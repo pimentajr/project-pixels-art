0a6195b133e0d0649a2aa1e047ed63a7a1afda7c
Marco Galindo <marco1408@gmail.com>
2021-04-09 17:35:08 -0300

    requisito 9 finalizado

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const colorPalette = document.getElementById('color-palette');
+const pixel = document.getElementsByClassName('pixel');
+for (let index = 0; index < pixel.length; index += 1) {
+  pixel[index].style.backgroundColor = 'white';
+}
 
 const color = document.getElementsByClassName('color');
 const listOfColors = ['black', 'orange', 'yellow', 'red'];
@@ -18,4 +22,12 @@ const pixelBoard = document.getElementById('pixel-board');
 pixelBoard.onclick = function paint(e) {
   const selectedColor = document.querySelector('.selected');
   e.target.style.backgroundColor = selectedColor.style.backgroundColor;
+};
+
+const clearButton = document.getElementById('clear-board');
+
+clearButton.onclick = function clearBoard() {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
+  }
 };
