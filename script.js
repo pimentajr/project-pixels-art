0fcd176f72c734a95ec0efac875e36364c2f7c0b
nandovbr <nandovbr15@hotmail.com>
2021-04-10 15:46:22 -0300

    requisitos 4 e 5 correção do lint

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function color() {
-  let colors = document.getElementsByClassName('color');
+  const colors = document.getElementsByClassName('color');
   colors[0].style.background = 'black';
   colors[1].style.background = 'red';
   colors[2].style.background = 'yellow';
   colors[3].style.background = 'orange';
 }
 color();
-
 
