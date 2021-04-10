a894eda1023625adbc96b4398576cfd0bb0890c7
Marco Galindo <marco1408@gmail.com>
2021-04-10 14:08:13 -0300

    requisito 12 finalizado e comentários adicionados

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// Setando alguns parâmetros iniciais
+
 const pixelBoard = document.getElementById('pixel-board');
 const generateButton = document.getElementById('generate-board');
 const boardSize = document.getElementById('board-size');
+
+// Função para gerar a board inicial
 
 function generateInitialBoard(input) {
   for (let index = 0; index < input; index += 1) {
@@ -19,6 +23,8 @@ function generateInitialBoard(input) {
 
 generateInitialBoard(5);
 
+// Função que implementa o botão de gerar a board de acordo com o número de pixels
+
 generateButton.onclick = function generateBoard() {
   pixelBoard.innerHTML = '';
   let input = boardSize.value;
@@ -28,21 +34,41 @@ generateButton.onclick = function generateBoard() {
   generateInitialBoard(input);
 };
 
-const colorPalette = document.getElementById('color-palette');
-const pixel = document.getElementsByClassName('pixel');
+// Função para atribuir cores aleatórias à paleta de cores, ideia adaptada de https://stackoverflow.com/questions/1484506/random-color-generator
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let randomColor = '#';
+  for (let index = 0; index < 6; index += 1) {
+    randomColor += letters[Math.floor(Math.random() * 16)];
+  }
+  return randomColor;
+}
+
+// Atribui as cores à paleta de cores:
+
+const listOfColors = ['black'];
+for (let index = 0; index < 3; index += 1) {
+  listOfColors.push(getRandomColor());
+}
 
 const color = document.getElementsByClassName('color');
-const listOfColors = ['black', 'orange', 'yellow', 'red'];
+
 for (let index = 0; index < color.length; index += 1) {
   color[index].style.backgroundColor = listOfColors[index];
 }
 
+// Atribui a classe 'selected' à cor selecionada
+
+const colorPalette = document.getElementById('color-palette');
 colorPalette.onclick = function selectColor(event) {
   const selectedColor = document.querySelector('.selected');
   const colorAux = selectedColor;
   if (selectedColor) colorAux.classList.remove('selected');
   event.target.classList.add('selected');
 };
+
+// Função para pintar os pixels da board
 
 pixelBoard.onclick = function paint(e) {
   const selectedColor = document.querySelector('.selected');
@@ -51,6 +77,9 @@ pixelBoard.onclick = function paint(e) {
   }
 };
 
+// Função para limpar a board
+
+const pixel = document.getElementsByClassName('pixel');
 const clearButton = document.getElementById('clear-board');
 
 clearButton.onclick = function clearBoard() {
