05718fc96a4089cf0007e4a0f11f8df4a4323138
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-10 00:15:07 -0300

    resolve o exercicio 7, adicionando classe quando clica em uma cor especifica

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const makeTable = document.createElement('table');
 getBody.appendChild(makeTable);
 makeTable.id = 'pixel-board';
 makeTable.className = 'center table';
+const numberLines = 5;
 
-for (let index = 0; index < 5; index += 1) {
+for (let index = 0; index < numberLines; index += 1) {
   const makeTr = document.createElement('tr');
   makeTable.appendChild(makeTr);
   makeTr.className = 'center';
-  for (let jndex = 0; jndex < 5; jndex += 1) {
+  for (let jndex = 0; jndex < numberLines; jndex += 1) {
     const makeTd = document.createElement('td');
     makeTr.appendChild(makeTd);
     makeTd.className = 'pixel branco';
@@ -21,5 +22,17 @@ function initBlack() {
 }
 
 initBlack();
+
+function trocarSelected(event) {
+  const paletaCores = document.querySelectorAll('.selected');
+  for (let index = 0; index < paletaCores.length; index += 1) {
+    paletaCores[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+const paleta = document.querySelector('#color-palette');
+paleta.addEventListener('click', trocarSelected);
+
 // npm run lint
 // npm run lint:styles
