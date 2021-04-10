df9ab7274f3e1e49c76934dfe2997f9fd696353d
Marcelo Leite <marsleite@gmail.com>
2021-04-10 01:02:37 -0300

    testando o codigo.

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ function colorPalette() {
     startColor.classList.add('selected');
   }
   startColorPage();
+
+  function setSelectClass(event) {
+    let setElement = document.querySelectorAll('.selected');
+    for(let index = 0; setElement.length; index += 1) {
+      setElement[index].classList.remove('selected')
+    }
+    event.target.classList.add('selected');
+  }
+
+  let colorFather = document.querySelector('color-palette');
+
+  colorFather.addEventListener('click', setSelectClass);
 }
 
-window.onload = colorPalette;
+onload = colorPalette;
