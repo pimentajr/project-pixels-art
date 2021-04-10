750848c20a21a362efdadb755b6cd9efbb97258c
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-10 12:16:48 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function inputClass() {
 }
 
 window.onload = inputClass;
+
+const color1 = document.getElementById('black');
+const color2 = document.getElementById('red');
+const color3 = document.getElementById('blue');
+const color4 = document.getElementById('green');
+
+function caughtedColor(event) {
+  const selectedElement = document.querySelector('.selected');
+  selectedElement.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+color1.addEventListener('click', caughtedColor);
+color2.addEventListener('click', caughtedColor);
+color3.addEventListener('click', caughtedColor);
+color4.addEventListener('click', caughtedColor);
