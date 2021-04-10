f520bb43338d2c4e20a274ab4c5d94507c25f524
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-10 13:25:22 -0300

    Adiciona Exercício 4

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ addTitle.id = 'title';
 addTitle.innerHTML = 'Paleta de Cores';
 document.body.appendChild(addTitle);
 
-function createPaletteDiv () {
+function createPaletteDiv() {
   const addColorPalette = document.createElement('div');
   addColorPalette.id = 'color-palette';
   document.body.appendChild(addColorPalette);
@@ -11,21 +11,40 @@ function createPaletteDiv () {
 }
 createPaletteDiv();
 
-function createBoxesDiv () {
+function createBoxesDiv() {
   const selectPalette = document.querySelector('#color-palette');
   for (let index = 0; index < 4; index += 1) {
     const createDivs = document.createElement('div');
     selectPalette.appendChild(createDivs);
     createDivs.classList.add('color');
   }
-}
-createBoxesDiv();
-
-function addColor () {
   const arrayOfColors = ['black', 'blue', 'purple', 'violet'];
   const selectDivs = document.querySelectorAll('.color');
   selectDivs.forEach((box, index) => {
     const div = box; div.style.backgroundColor = arrayOfColors[index];
   });
+  window.onload = 'white'
 }
-addColor();
+createBoxesDiv();
+
+function addPixels() {
+  const createPixels = document.createElement('section');
+  createPixels.id = 'pixel-board';
+  document.body.appendChild(createPixels);
+
+  const selectPixels = document.querySelector('#pixel-board');
+  for (let index = 0; index < 5; index += 1) {
+    const createPixelsRow = document.createElement('div');
+    selectPixels.appendChild(createPixelsRow);
+    createPixelsRow.classList.add('pixel');
+    createPixelsRow.innerText = 'Row';
+    for (let indice = 1; indice < 5; indice += 1) {
+      const createPixelsColumn = document.createElement('div');
+      selectPixels.appendChild(createPixelsColumn);
+      createPixelsColumn.classList.add('pixel');
+      createPixelsColumn.innerText = 'Column';
+    }
+  }
+  window.onload = 'white';
+}
+addPixels();
