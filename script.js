32520436cc438e9d6cfcc5ba59fec5ed4071c98c
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-04-10 18:58:59 -0300

    Ajustando erros de sintax eslint e stylelint.

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-
 // ex 7.
-let colors = document.querySelectorAll('.color');
-for (let item of colors) {
-  item.addEventListener('click', removeClass);
-  item.addEventListener('click', addClass);
-}
-
 function removeClass() {
-  for (let index = 0; index < colors.length; index +=1) {
-    colors[index].classList.remove('selected')
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].classList.remove('selected');
   }
-  
 }
 
 function addClass(event) {
   event.target.classList.add('selected');
-  
+}
+
+const colors = document.querySelectorAll('.color');
+
+for (const item of colors) {
+  item.addEventListener('click', removeClass);
+  item.addEventListener('click', addClass);
 }
