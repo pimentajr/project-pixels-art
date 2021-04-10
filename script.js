b4a73a2b60df3192076b0a18b3a8a03b7998f2df
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-10 10:57:34 -0300

    Altera exercício 2 em função própria

--- script.js
+++ script.js
@@ -3,40 +3,29 @@ addTitle.id = 'title';
 addTitle.innerHTML = 'Paleta de Cores';
 document.body.appendChild(addTitle);
 
-function createPaletteDiv () {
+function createPaletteDiv (){
   const addColorPalette = document.createElement('div');
   addColorPalette.id = 'color-palette';
   document.body.appendChild(addColorPalette);
-  addColorPalette.innerHTML = 'Hi'
+  addColorPalette.style.backgroundColor = 'rgb(30, 116, 119)';
 }
 createPaletteDiv();
 
-function createBoxesDiv() {
+function createBoxesDiv(){
   const selectPalette = document.querySelector('#color-palette');
-  
-  for (let index = 0; index < 4; index +=1) {
+  for (let index = 0; index < 4; index += 1) {
     const createDivs = document.createElement('div');
     selectPalette.appendChild(createDivs);
     createDivs.classList.add('color');
-  };
+  }
+}
+createBoxesDiv();
 
+function addColor (){
   const arrayOfColors = ['black', 'blue', 'purple', 'violet'];
   const selectDivs = document.querySelectorAll('.color');
   selectDivs.forEach((box, index) => {
     box.style.backgroundColor = arrayOfColors[index];
   });
-};
-
-createBoxesDiv();
-
-
-  
-  
- 
-  
-  
-
-
-
-
-
+}
+addColor();
