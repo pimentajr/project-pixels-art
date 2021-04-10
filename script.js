bfa79c789c230b258708731ace6a108e0568ac93
IsleneGomes <islenegomes@gmail.com>
2021-04-10 12:29:48 -0300

    Iniciando Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.onload = function carregar() {
   document.getElementById('black').classList.add('selected');
 };
+function selecionaClasse(event) {
+  const atribuiClasse = document.querySelectorAll('.selected');
+  for (let index = 0; index < atribuiClasse.length; index += 1) {
+    atribuiClasse[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
