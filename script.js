b8613c69071e8349df7378ccb77e1f8df7bc4109
Víctor Faria <vitilevi@hotmail.com>
2021-04-10 14:18:39 -0300

    10 funcionando mas o resto parou

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const pixelBoard = document.getElementById('pixel-board');
 const colors = ['black', 'red', 'green', 'blue'];
 const colorsList = document.getElementsByClassName('color');
 const selectedColor = document.getElementsByClassName('selected');
-const pixelList = document.getElementsByClassName('pixel');
+let pixelList = document.getElementsByClassName('pixel');
 const cleanButton = document.getElementById('clear-board');
-
+const inputNumber = document.getElementById('board-size');
+const generateButton = document.getElementById('generate-board');
 // ------
+
 // Create elements
 function createDivBoard(param) {
   const numberOfTiles = param;
@@ -29,13 +31,49 @@ function makeLiColors(param) {
   }
 }
 
-// Select function
+// Select black color at start function
 function selectColor() {
   liColors[0].classList.add('selected');
 }
 // ------
 
+// Error return
+function errorReturn() {
+  window.alert('Board inválido!');
+}
+// ------
+
+// Calculate board size Function
+function boardSizeCalculate(size) {
+  const originalSize = size;
+  let counter = 0;
+  const singleTileSize = 42;
+  if (counter === 0) {
+    const maxLateralSize = Math.ceil((singleTileSize * 5));
+    pixelBoard.style.width = `${maxLateralSize.toString()}px`;
+    const minHeightSize = maxLateralSize + 100;
+    pixelBoard.style.minHeight = `${minHeightSize.toString()}px`;
+    counter += 1;
+  } else {
+    const maxLateralSize = Math.ceil((singleTileSize * originalSize));
+    pixelBoard.style.width = `${maxLateralSize.toString()}px`;
+    const minHeightSize = maxLateralSize + 100;
+    pixelBoard.style.minHeight = `${minHeightSize.toString()}px`;
+  }
+  updateTileListEventListener();
+}
+
 // function of events
+function generateDynamicBoard() {
+  const size = inputNumber.value;
+  const tileNumber = size ** 2;
+  if (size < 1 || size > 50) {
+    return errorReturn();
+  }
+  createDivBoard(tileNumber);
+  boardSizeCalculate(size);
+}
+
 function lookForSelectedColor(event) {
   for (let index = 0; index < colorsList.length; index += 1) {
     colorsList[index].classList.remove('selected');
@@ -44,8 +82,8 @@ function lookForSelectedColor(event) {
 }
 
 function paintSingleTile(event) {
-  const param = event;
-  param.target.style.backgroundColor = selectedColor[0].style.backgroundColor;
+  const tile = event;
+  tile.target.style.backgroundColor = selectedColor[0].style.backgroundColor;
 }
 
 function cleanBoard() {
@@ -58,18 +96,29 @@ function createEventListeners() {
   for (let index = 0; index < colorsList.length; index += 1) {
     colorsList[index].addEventListener('click', lookForSelectedColor);
   }
+  pixelList = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixelList.length; index += 1) {
     pixelList[index].addEventListener('click', paintSingleTile);
   }
   cleanButton.addEventListener('click', cleanBoard);
+  generateButton.addEventListener('click', generateDynamicBoard);
+}
+
+// Update Function
+function updateTileListEventListener() {
+  pixelList = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixelList.length; index += 1) {
+    pixelList[index].addEventListener('click', paintSingleTile);
+  }
 }
 // ------
 
 function initialize() {
   selectColor();
   makeLiColors(liColors);
-  createDivBoard(25);
   createEventListeners();
+  createDivBoard(25);
+  boardSizeCalculate(25);
 }
 
 window.onload = initialize;
