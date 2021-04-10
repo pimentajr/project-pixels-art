92baf45e0c8852b6ffb34cac936e11e92ef91e60
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-10 14:25:16 -0300

    Adiciona exercícios 2, 3 e 6

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const addTitle = document.createElement('h1');
 addTitle.id = 'title';
 addTitle.innerHTML = 'Paleta de Cores';
 document.body.appendChild(addTitle);
+
+function createPaletteDiv() {
+  const addColorPalette = document.createElement('div');
+  addColorPalette.id = 'color-palette';
+  document.body.appendChild(addColorPalette);
+  addColorPalette.style.backgroundColor = 'rgb(30, 116, 119)';
+}
+createPaletteDiv();
+
+function createBoxesDiv() {
+  const selectPalette = document.querySelector('#color-palette');
+  for (let index = 0; index < 4; index += 1) {
+  const createDivs = document.createElement('div');
+  selectPalette.appendChild(createDivs);
+  createDivs.classList.add('color');
+  }
+  const arrayOfColors = ['black', 'blue', 'purple', 'violet'];
+  const selectDivs = document.querySelectorAll('.color');
+  selectDivs.forEach((box, index) => {
+  const div = box; div.style.backgroundColor = arrayOfColors[index];
+  });
+  const firstColor = document.querySelector('.color');
+  firstColor.classList.add('selected');
+}
+createBoxesDiv();
