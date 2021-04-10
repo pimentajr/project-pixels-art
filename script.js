ec1e3556a5df9d9c60f073df28bd37185896b3da
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-10 19:22:07 -0300

    Todos requisitos realizados + Bonus + lint OK

--- script.js
+++ script.js
@@ -1,74 +1,77 @@
-window.onload = function start() {
+window.onload = function startBoard() {
   const boardSection = document.getElementById('pixel-board');
-  const colorPalette = document.getElementById('color-palette');
-  const btnRest = document.getElementById('clear-board');
-  let btnResize = document.getElementById('generate-board');
 
   // Cria o quadro com os quadrados brancos;
-  for (let index = 0; index < 5; index += 1) {
+  for (let rows = 0; rows < 5; rows += 1) {
     const everyPixelRow = document.createElement('div');
     everyPixelRow.setAttribute('class', 'pixelRow');
     boardSection.appendChild(everyPixelRow);
-    for (let index = 0; index < 5; index += 1) {
+    for (let pixel = 0; pixel < 5; pixel += 1) {
       const everyPixel = document.createElement('div');
       everyPixel.setAttribute('class', 'pixel');
       everyPixelRow.appendChild(everyPixel);
     }
   }
-  // muda a classe do elemento colorido clickado para selected.
-  colorPalette.addEventListener('click', (event) => {
-    const allDivsColorPallete = document.getElementById('color-palette').children;
-    for (let index = 0; index < allDivsColorPallete.length; index += 1) {
-      allDivsColorPallete[index].setAttribute('class', 'color');
-    }
-    event.target.className += ' selected';
-  });
-
-
-  //Pinta o pixel!
-  boardSection.addEventListener('click', (event) => {
-    let colorSelected = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
-    event.target.style.backgroundColor = colorSelected;
-  });
-
-
-  //Limpa o pixel!
-  btnRest.addEventListener('click', function() {
-    const AllDivsPixels = document.getElementsByClassName('pixel');
-    for (let index = 0; index < AllDivsPixels.length; index += 1) {
-      AllDivsPixels[index].style.backgroundColor = 'white';
-    }
-  });
-
-  // aumenta ou diminui o board chamando a funçao abaixo.
-  btnResize.addEventListener('click', function(){
-    let numberRows = eval(document.getElementById('board-size').value);
-    if (numberRows === undefined) {
-      return alert('Board inválido!');
-    } else if (numberRows < 5) {
-      boardResize(5);
-    } else if (numberRows > 50) {
-      boardResize(50);
-    } else {
-      boardResize(numberRows);
-    }
-  });
-
-  
 };
 
-
-//Resize no tabalho da tabela.
 const boardSection = document.getElementById('pixel-board');
+const colorPalette = document.getElementById('color-palette');
+const btnRest = document.getElementById('clear-board');
+const btnResize = document.getElementById('generate-board');
+const last3Colors = document.getElementsByClassName('color');
+
+function generateRandomColor() {
+  return Math.floor(Math.random() * 26987215).toString(16);
+}
+
+// Cores aleatorios nas tres ultima opcoes toda hora que o navegador abre.
+function applyRandomColor() {
+  for (let colorBox = 1; colorBox < last3Colors.length; colorBox += 1) {
+    const newColor = generateRandomColor();
+    last3Colors[colorBox].style.backgroundColor = `#${newColor}`;
+  }
+}
+applyRandomColor();
+
+// muda a classe do elemento colorido clickado para selected.
+function changeSelectedColorClass(event) {
+  const eventElement = event;
+  const allDivsColorPallete = document.getElementById('color-palette').children;
+  for (let colorSquare = 0; colorSquare < allDivsColorPallete.length; colorSquare += 1) {
+    allDivsColorPallete[colorSquare].setAttribute('class', 'color');
+  }
+  eventElement.target.className += ' selected';
+}
+colorPalette.addEventListener('click', changeSelectedColorClass);
+
+// Pinta o pixel!
+function changePixelColor(event) {
+  const eventElement = event;
+  const computedStyle = window.getComputedStyle(document.querySelector('.selected'));
+  const colorSelected = computedStyle.backgroundColor;
+  eventElement.target.style.backgroundColor = colorSelected;
+}
+boardSection.addEventListener('click', changePixelColor);
+
+// Limpa o pixel!
+function clearPixels() {
+  const AllDivsPixels = document.getElementsByClassName('pixel');
+  for (let allPixels = 0; allPixels < AllDivsPixels.length; allPixels += 1) {
+    AllDivsPixels[allPixels].style.backgroundColor = 'white';
+  }
+}
+btnRest.addEventListener('click', clearPixels);
+
+// Funçao complementar a changeBoardSize, para a mesma não ficar tão complexa.
 function boardResize(num) {
   while (boardSection.firstChild) {
     boardSection.removeChild(boardSection.lastChild);
   }
-  for (let index = 0; index < num; index += 1) {
+  for (let newRowsSize = 0; newRowsSize < num; newRowsSize += 1) {
     const everyPixelRow = document.createElement('div');
     everyPixelRow.setAttribute('class', 'pixelRow');
     boardSection.appendChild(everyPixelRow);
-    for (let index = 0; index < num; index += 1) {
+    for (let newPixelsSize = 0; newPixelsSize < num; newPixelsSize += 1) {
       const everyPixel = document.createElement('div');
       everyPixel.setAttribute('class', 'pixel');
       everyPixelRow.appendChild(everyPixel);
@@ -76,13 +79,17 @@ function boardResize(num) {
   }
 }
 
-
-//Cores aleatorios nas tres ultima opcoes toda hora que o navegador abre.
-let last3Colors = document.getElementsByClassName('color');
-
-for (let n of last3Colors) {
-  let newColor = Math.floor(Math.random()*16777215).toString(16);
-  if (n.className === 'color'){
-    n.style.backgroundColor = "#" + newColor;
+// aumenta ou diminui o board chamando a funçao acima.
+function changeBoardSize() {
+  const numberRows = parseInt(document.getElementById('board-size').value, 10);
+  if (numberRows === undefined) {
+    return alert('Board inválido!');
+  } if (numberRows < 5) {
+    boardResize(5);
+  } else if (numberRows > 50) {
+    boardResize(50);
+  } else {
+    boardResize(numberRows);
   }
 }
+btnResize.addEventListener('click', changeBoardSize);
