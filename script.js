d51fa8a2cb88283700287a6f453505f37e72f745
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-10 10:21:27 -0300

    corrige requisito 5

---
+++ script.js
@@ -0,0 +1,8 @@
+function randomColors(){
+  let colors = document.getElementsByClassName('color');
+  for (let color of colors) {
+    if (color.id === 'color-1') {
+      color.style.b
+    }
+  }
+}
