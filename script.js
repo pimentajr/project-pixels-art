ea8a0f7eabcd67eb977f23a67c55933bfe8a0160
Savio Moraes <smmsavio@hotmail.com>
2021-04-10 00:46:50 -0300

    Inicia resolucao do requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const colorPalete = document.querySelector('#color-palette');
 const colors = document.querySelectorAll('.color');
 function changesToBlack() {
   if (colors[0].style.backgroundColor !== 'black') {
@@ -6,7 +7,17 @@ function changesToBlack() {
 }
 changesToBlack();
 
-function defineSelectedColor() {
+function defineInitialSelectedColor() {
   colors[0].className = 'color selected';
 }
-defineSelectedColor();
+defineInitialSelectedColor();
+
+colorPalete.addEventListener('click', colorChange);
+
+function colorChange(event) {
+  let selecionedColor = document.querySelector('#color-palette').children;
+  for (let index = 0; index < selecionedColor.length; index += 1) {
+    selecionedColor[index].setAttribute('class', 'color');
+  }
+  event.target.className += ' selected';
+}
