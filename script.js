1206d1c1356e2e86c7aea802a26a2afaa883ab40
Ruben Lopes <rubenssouss@hotmail.com>
2021-04-10 11:07:31 -0300

    Add pixel board

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function () {
   const colorPalette = document.getElementById('color-palette');
   const arrColors = ['black', 'blue', 'magenta', 'yellow'];
-  const pixelSquad = document.getElementById('pixel-board');
-  let pixelSquadSize = 25;
+  const pixelSquare = document.getElementById('pixel-board');
+  let pixelSquareSize = 25;
 
   for (let index = 0; index < arrColors.length; index += 1) {
     const colorPixel = document.createElement('div');
@@ -14,25 +14,21 @@ window.onload = function () {
     colorPalette.appendChild(colorPixel);
   }
 
-  for (let index = 0; index < Math.sqrt(pixelSquadSize); index += 1) {
-    // const pixel = document.createElement('div');
-    // pixel.className = 'pixel';
-    // // colorPixel.classList.add(arrColors[index]);
-    // // colorPixel.style.backgroundColor = arrColors[index];
-    // // colorPixel.style.margin = '5px';
-    // pixel.style.border = 'solid 1px black';
-    // pixelSquad.appendChild(pixel);
-    let arrPixels = [];
-
-    for (let index2 = 1; index2 < Math.sqrt(pixelSquadSize); index2 += 1) {
+  for (let index = 0; index < Math.sqrt(pixelSquareSize); index += 1) {   
+    pixelSquare.style.display = 'table';
+    const pixelRow = document.createElement('div');
+    
+    for (let index1 = 0; index1 < Math.sqrt(pixelSquareSize); index1 += 1) {      
       const pixel = document.createElement('div');
+      pixelRow.style.display = 'table-row';
       pixel.className = 'pixel';
-      // colorPixel.classList.add(arrColors[index]);
-      // colorPixel.style.backgroundColor = arrColors[index];
-      // colorPixel.style.margin = '5px';
+      pixel.style.display = 'table-cell';
+
       pixel.style.border = 'solid 1px black';
-      arrPixels.push(pixel);
-      pixelSquad.appendChild(arrPixels);
+
+      pixelRow.appendChild(pixel);        
     }
+
+    pixelSquare.appendChild(pixelRow);
   }
 };
