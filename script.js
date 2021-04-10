ba55f2f9d392e88f849395c00ad65de2696cd406
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-09 22:09:21 -0300

    requisito 7 - cria o evento que ao clicar nas cores da paleta ela seja selecionada

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 window.onload = function initial() {
   let initialColor = document.querySelector('#black');
   initialColor.classList.add('selected');
+ 
+  let colorPalette = document.querySelectorAll('.color');
+  for (let index = 0; index < colorPalette.length; index += 1) {
+    colorPalette[index].addEventListener('click', function (event) {
+      let colorSelected = document.querySelector('.selected');
+      colorSelected.classList.remove('selected');
+      event.target.classList.add('selected');
+    });
+  }
 };
