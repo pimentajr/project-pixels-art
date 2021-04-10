30e1be0ad99b16e00db0447114eb47b71b6dba48
anaclarabck <ackyotoku@gmail.com>
2021-04-10 01:20:26 -0300

    4- [UPDATE][ADD] Ajuste requisito 1 e adicionado requisito 5

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const listOfColors = ['black', 'lightsalmon', 'lightskyblue', 'khaki'];
 const colorsPalette = document.querySelector('#color-palette');
-const pixelBoard = document.querySelector('#pixel-board')
-const linePixelBoard = document.querySelectorAll('.line')
+const linePixelBoard = document.querySelectorAll('.line');
 const basePixelBoard = 5;
+const pixelBoard = document.getElementById('pixel-board');
+const color = document.getElementsByClassName('color')
+const pixel = document.getElementsByClassName('pixel')
 
-document.getElementById('title').innerText = 'Paleta de cores';
+document.getElementById('title').innerText = 'Paleta de Cores';
 
 // Cria paleta de cores
 function createPalette(arrayColors) {
@@ -21,14 +23,18 @@ function fillLinePixelBoard(divline) {
   for (let index = 0; index < basePixelBoard; index += 1) {
     const box = document.createElement('div');
     box.className = 'pixel';
-    box.style.background = 'white'
-    divline.appendChild(box)
+    box.style.width = '40px';
+    box.style.height = '40px';
+    box.style.background = 'white';
+    divline.style.width = '210px';
+    divline.style.height = box.style.height;
+    pixelBoard.style.width = divline.style.width;
+    divline.appendChild(box);
   }
 }
 
-// Passa por todas as linhas 
+// Passa por todas as linhas
 function createPixelBoard(lines) {
-  let columns = lines;
   for (let index = 0; index < lines.length; index += 1) {
     fillLinePixelBoard(lines[index]);
   }
@@ -36,3 +42,21 @@ function createPixelBoard(lines) {
 
 createPalette(listOfColors);
 createPixelBoard(linePixelBoard);
+
+let clickedColor = 'black'
+
+color[0].addEventListener('click', function() {
+  clickedColor = color[0].style.background;
+})
+
+color[1].addEventListener('click', function() {
+  clickedColor = color[1].style.background;
+})
+
+pixel[0].addEventListener('click', function() {
+  pixel[0].style.background = clickedColor;
+})
+
+pixel[1].addEventListener('click', function() {
+  pixel[1].style.background = clickedColor;
+})
