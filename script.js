ef5d4be4501d3f23cb2a6d2823845159690916fd
fernando lasmar <f3rnandolasmar@gmail.com>
2021-04-10 15:12:17 -0300

    Ao clicar faz que a paleta seja selecionada

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const colors = document.querySelectorAll('.color'); // atribui um array com as classes color
 
+// seleciona cor
+function selectColor(event) {
+  document.querySelector('.selected').classList.remove('selected');
+  event.target.classList.add('selected');
+}
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', selectColor);
+}
