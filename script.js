0f767af8f2d0c06c306234e600f64b668f59a671
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-10 17:03:44 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -48,20 +48,20 @@ function colorSelect() {
 colorSelect();
 
 function palletSelectColor() {
-  const selectedPixel = document.querySelectorAll('.pixel');
-  for (let i = 0; i < selectedPixel.length; i += 1) {
-    selectedPixel[i].addEventListener('click', (event) => {
-      const selectColor = document.querySelector('.selected');
-      const selectedColor = selectColor.style.backgroundColor;
-      const element = event.target;
+  const selectedPixel = document.getElementById('pixel-board');
+  selectedPixel.addEventListener('click', (event) => {
+    const selectColor = document.querySelector('.selected');
+    const selectedColor = selectColor.style.backgroundColor;
+    const element = event.target;
+    if (element.className === 'pixel') {
       element.style.backgroundColor = selectedColor;
-    });
-  }
+    }
+  });
 }
 palletSelectColor();
 
 function createReset() {
-  const pixel = document.querySelectorAll('.pixel');
+  const pixel = document.getElementsByClassName('pixel');
   const btnReset = document.querySelector('#clear-board');
   btnReset.addEventListener('click', () => {
     for (let cell = 0; cell < pixel.length; cell += 1) {
@@ -73,23 +73,20 @@ function createReset() {
 }
 createReset();
 
-/* 10 - Faça o quadro de pixels ter seu tamanho definido pela pessoa usuária.
-Crie um input e um botão que permitam definir um quadro de pixels com tamanho entre 5 e 50. Ao clicar no botão, deve ser gerado um quadro de N pixels de largura e N pixels de altura, onde N é o número inserido no input;
-Ou seja, se o valor passado para o input for 7, ao clicar no botão, vai ser gerado um quadro de 49 pixels (7 pixels de largura x 7 pixels de altura);
-Se nenhum valor for colocado no input ao clicar no botão, mostre um alert com o texto: "Board inválido!";
-O novo quadro deve ter todos os pixels preenchidos com a cor branca. */
-
 function setBoard() {
   const inputBox = document.querySelector('#board-size');
   const btnVqv = document.querySelector('#generate-board');
   const pxBoard = document.querySelector('#pixel-board');
   btnVqv.addEventListener('click', () => {
-    if (inputBox.value < 1) {
+    if (inputBox.value === '') {
       alert('Board inválido!');
-    } else {
-      pxBoard.innerHTML = '';
-      createBoard(inputBox.value);
+    } else if (inputBox.value <= 5) {
+      inputBox.value = 5;
+    } else if (inputBox.value > 50) {
+      inputBox.value = 50;
     }
+    pxBoard.innerHTML = '';
+    createBoard(inputBox.value);
   });
 }
 setBoard();
