0d0343c95dfaf035097a16e951982668997fd45f
Alexandre <naitwa.alexandre@gmail.com>
2021-04-10 11:39:21 -0300

    Revert "no padrao 05"
    This reverts commit fcf0f3acab21f70f6c68c8cdf18383112ff1b4d5.

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-// global varible
+//global varible
 const colorPalette = document.querySelector('#color-palette');
 const pixelBoard = document.querySelector('#pixel-board');
 
 function createColorPalette() {
+ 
   for (let i = 0; i < 4; i += 1) {
     const div = document.createElement('div');
-    div.className = 'color';
-    div.style.marginRight = '5px';
+    div.className = "color";
+    div.style.marginRight = "5px"
     colorPalette.appendChild(div);
   }
 }
 createColorPalette();
 
 function createPixels() {
-  const pixels = 25;
-
+  const pixels = 25
+  
   for (let i = 0; i < pixels; i += 1) {
     const divPixels = document.createElement('div');
-    divPixels.className = 'pixel';
-    divPixels.style.marginRight = '3px';
+    divPixels.className = "pixel";
+    divPixels.style.marginRight = "3px"
     pixelBoard.appendChild(divPixels);
   }
 }
 createPixels();
+
+
