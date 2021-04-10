eacd2c8f89da5fbe3e9f3d16675612104e93e99b
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-04-10 15:17:19 -0300

    only colors in css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const palette = ['black', 'red', 'blue', 'yellow'];
-const color = document.querySelectorAll('.color');
+// const palette = ['black', 'red', 'blue', 'yellow'];
+// const color = document.querySelectorAll('.color');
 
-for (let i = 0; i < color.length; i += 1) {
-  color[i].style.backgroundColor = palette[i];
-}
+// for (let i = 0; i < color.length; i += 1) {
+//   color[i].style.backgroundColor = palette[i];
+// }
