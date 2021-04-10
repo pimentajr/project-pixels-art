9e37010d129c33b0f2d0ac5a0e2e50ae7df93e75
Leandro Lopes <leandro_carlos@uol.com.br>
2021-04-10 17:58:15 -0300

    exercicio 9

--- script.js
+++ script.js
@@ -18,10 +18,23 @@ let pixel = event.target
 pixel.style.backgroundColor = colorPixel
 }
 
+function ClearTable() {
+  let allPixels =  document.getElementsByClassName('pixel')
+  for (let index = 0; index < allPixels.length; index += 1) {
+    allPixels[index].style.backgroundColor = 'white'
+ 
+  }
+ }
+ 
+
 let selectElements = document.getElementsByClassName('color');
 for (let index = 0; index < selectElements.length; index += 1) {
   selectElements[index].addEventListener('click', changeClassDiv)
 } 
 
-let tableElements = document.getElementById('pixel-board')
+let tableElements = document.getElementById('pixel-board');
 tableElements.addEventListener('click', paintTable)
+
+
+let clearButton = documet.getElementById('clear-board');
+clearButton.addEventListener('click', ClearTable)
