56f9bceffcd925e2704607493b5ad1435f5df0c8
AlyssonSene <alysson_silva@outlook.com>
2021-04-10 11:50:45 -0300

    funçao cria grid

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let pixelBoard = document.querySelector('#pixel-board'); 
-for (let index = 0; index <= 24; index +=1) {
+let pixelBoard = document.querySelector('#pixel-board');
+let gridSize = 5;
+function gridCreate (gridSize){
+for (let index = 0; index < gridSize; index +=1) {
   let pixelDivs = document.createElement('div');
-  pixelDivs.className = "Pixel";
   pixelBoard.appendChild(pixelDivs);
+  pixelDivs.className = "line";
+  for (let secondIndex = 0; secondIndex < gridSize; secondIndex +=1) {
+    let pixelDivs = document.createElement('div');
+  pixelBoard.appendChild(pixelDivs);
+  pixelDivs.className = "Pixel";
+  }
 }
+}
+gridCreate(gridSize);
