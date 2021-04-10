537ef2f39a180e6baf48efb42772684e4665351a
Luís Seixas <luiseixas2@gmail.com>
2021-04-09 21:33:26 -0300

    ajustando o codigo e deixando ele mais clean

--- script.js
+++ script.js
@@ -1,36 +1,72 @@
+let selectedColor = 'black';
+
+function setSelectedColor(color){
+  selectedColor = color;
+  console.log(selectedColor);
+}
 // quesito 1
 let elementH1 = document.getElementById('title');
 elementH1.innerText = 'Paleta de Cores';
 
-// quesito 2
-function createPalettes() {
-  for (let index = 0; index < 4; index += 1) {
-    let palettes = document.createElement('div');
-    let allPalettes = document.getElementById('color-palette');
-    allPalettes.appendChild(palettes);
-    palettes.className = 'color';
+function resetSelected() {
+  let palletes = document.getElementsByClassName('color');
+  for (let index = 0; index < palletes.length; index += 1) {
+    palletes[index].className = 'color';
   }
 }
-createPalettes();
+
+// quesito 2
+function createSquare(colorIndex) {
+  let colors = ['black', 'red', 'yellow', 'green'];
+  let palette = document.createElement('div');
+  palette.style.backgroundColor = colors[colorIndex];
+  palette.className = 'color';
+  palette.addEventListener('click', function () {
+    setSelectedColor(colors[colorIndex]);
+    resetSelected();
+    palette.className = 'color selected';
+  });
+  if (colorIndex === 0) {
+    palette.className = 'color selected';
+  }
+  return palette;
+}
+
+function createPalettes(size) {
+  let allPalettes = document.getElementById('color-palette');
+  for (let index = 0; index < size; index += 1) {
+    allPalettes.appendChild(createSquare(index));
+  }
+}
 
 // quesito 3
-let squareColors = document.getElementsByClassName('color');
-squareColors[0].style.backgroundColor = 'black';
-squareColors[1].style.backgroundColor = 'red';
-squareColors[2].style.backgroundColor = 'yellow';
-squareColors[3].style.backgroundColor = 'green';
+function createSquareboard() {
+  let squareBoard = document.createElement('div');
+  squareBoard.className = 'pixel';
+  squareBoard.addEventListener('click', function () {
+    squareBoard.style.backgroundColor = selectedColor;
+  });
+  return squareBoard;
+}
 
+function createRowBoard(size) {
+  let row = document.createElement('div');
+  for (let index = 0; index < size; index += 1) {
+    row.appendChild(createSquareboard());
+  }
+  return row;
+}
 // quesito 4
-function createMainBoard() {
+function createBoard() {
   let mainBoard = document.getElementById('pixel-board');
   for (let index = 0; index < 5; index += 1) {
-    let columns = document.createElement('div');
-    mainBoard.appendChild(columns);
-    for (let jindex = 0; jindex < 5; jindex += 1) {
-      let lines = document.createElement('div');
-      columns.appendChild(lines);
-      lines.className = 'pixel';
-    }
+    mainBoard.appendChild(createRowBoard(5));
   }
 }
-createMainBoard();
+
+function buildAll() {
+  createPalettes(4);
+  createBoard();
+}
+
+buildAll();
