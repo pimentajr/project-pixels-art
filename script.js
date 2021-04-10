61abf3e72953ca0cfcdf6fb5d682da35e2564b15
Danilo Pereira <danilodps22@hotmail.com>
2021-04-09 22:54:56 -0300

    Req. 8: colorir pixel com cor escolhida

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-window.onload = function () {
+window.onload = function initial() {
   const initialColor = document.querySelector('#black');
-  initialColor.className += ' selected';
+  initialColor.className = 'color selected';
 };
 
 const colors = ['black', 'red', 'blue', 'green'];
 const pixel = 5;
 const pixelBoard = document.querySelector('#pixel-board');
 const colorPalette = document.querySelector('#color-palette');
+let colorSeleted = 'black';
+
+console.log(colorSeleted); // --------------------------------------------------------------seleção cor
 
 function createPalette(colorTitle) {
   for (let index = 0; index < colorTitle.length; index += 1) {
@@ -26,7 +29,7 @@ function createPixelLine(numberOfPixelsXY) {
     pixelBox.className = 'pixel';
     lineBox.appendChild(pixelBox);
     pixelBoard.appendChild(lineBox);
-    console.log(pixelBox);
+    //  console.log(pixelBox);
   }
 }
 
@@ -37,18 +40,28 @@ function numberOfLines(lines) {
 }
 
 function colorSelection(originEvent) {
-  const colorSeleted = originEvent.target.id;
+  const event = originEvent;
+  const boxColor = event.target.id;
   const classColorList = document.querySelectorAll('.color');
 
-  if (colorSeleted !== 'color-palette') {
+  if (boxColor !== 'color-palette') {
     for (let index = 0; index < classColorList.length; index += 1) {
       classColorList[index].className = 'color';
     }
-    originEvent.target.className = 'color selected';
+    event.target.className = 'color selected';
   }
+  colorSeleted = boxColor;
+  console.log(colorSeleted); // -------------------------------------------seleção cor
 }
 
-colorPalette.addEventListener('click', colorSelection);
+function coloringPixel(originEvent) {
+  const event = originEvent;
+  const chosenPixel = event.target;
+  chosenPixel.style.backgroundColor = colorSeleted;
+  console.log(chosenPixel);
+}
 
 createPalette(colors);
 numberOfLines(pixel);
+colorPalette.addEventListener('click', colorSelection);
+pixelBoard.addEventListener('click', coloringPixel);
