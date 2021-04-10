7ef7f6fa772f314d8db54f0c3942192dbe7de608
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-10 15:07:49 -0300

    Adiciona exercícios 1 e id do requisito 2 no arquivo HTML

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const addTitle = document.createElement('h1');
-addTitle.id = 'title';
-addTitle.innerHTML = 'Paleta de Cores';
-document.body.appendChild(addTitle);
-
-function createPaletteDiv() {
-  const addColorPalette = document.createElement('div');
-  addColorPalette.id = 'color-palette';
-  document.body.appendChild(addColorPalette);
-  addColorPalette.style.backgroundColor = 'rgb(30, 116, 119)';
-}
-createPaletteDiv();
-
 function createBoxesDiv() {
   const selectPalette = document.querySelector('#color-palette');
   for (let index = 0; index < 4; index += 1) {
@@ -18,13 +5,11 @@ function createBoxesDiv() {
     selectPalette.appendChild(createDivs);
     createDivs.classList.add('color');
   }
-  
-  const getColors = document.querySelectorAll('.color')
-  getColors[0].style.backgroundColor = 'black';
-  getColors[1].style.backgroundColor = 'rgb(189 172 216)';
-  getColors[2].style.backgroundColor = 'violet';
-  getColors[3].style.backgroundColor = 'purple';
- 
+  const arrayOfColors = ['black', 'blue', 'purple', 'violet'];
+  const selectDivs = document.querySelectorAll('.color');
+  selectDivs.forEach((box, index) => {
+    const div = box; div.style.backgroundColor = arrayOfColors[index];
+  });
   const firstColor = document.querySelector('.color');
   firstColor.classList.add('selected');
 }
