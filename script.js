77ad9bcd94c60d6a5023f6f6db0921d4dcdcf8ba
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-09 22:12:36 -0300

    Realizacao do exercicio 7

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function addInitColor() {
+function setInitialColor() {
   const element = document.querySelector('.black');
   element.classList.add('selected');
 }
 
-addInitColor();
+setInitialColor();
+
+const element = document.querySelector('#color-palette');
+element.addEventListener('click', function(event){
+let targetElement = event.target;
+  if (targetElement === 'selected')
+    targetElement.classList.remove('selected');
+  else {
+    targetElement.classList.toggle('selected');
+  }
+});
