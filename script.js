abf887ca49593cfe978f526ab1c5321e848c4124
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-10 01:43:40 -0300

    Reorder script.js' functions

--- script.js
+++ script.js
@@ -9,87 +9,20 @@ const COLORS = [
 const NUM_OF_COLORS = COLORS.length;
 
 // Pixel board parameters:
-const BOARD_SIDE_SIZE = 5;
+let boardSideSize = 5;
 
-function setFixedColor() {
-  const fixedColorBox = document.querySelector('.color.fixed');
+function clearBoard() {
+  const pixels = document.getElementsByClassName('pixel');
 
-  fixedColorBox.style.backgroundColor = FIXED_COLOR;
-}
-
-function setColors() {
-  const colorBoxes = document.querySelectorAll('.color:not(.fixed)');
-
-  for (let index = 0; index < NUM_OF_COLORS; index += 1) {
-    colorBoxes[index].style.backgroundColor = COLORS[index];
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
   }
 }
 
-function initializePalette() {
-  const colorPalette = document.getElementById('color-palette');
-  const fixedColorBox = document.createElement('div');
-  let colorBox;
+function initializeClearButtonListener() {
+  const clearButton = document.getElementById('clear-board');
 
-  fixedColorBox.style.backgroundColor = FIXED_COLOR;
-  fixedColorBox.classList.add('color', 'fixed', 'selected');
-  colorPalette.appendChild(fixedColorBox);
-
-  for (let index = 0; index < NUM_OF_COLORS; index += 1) {
-    colorBox = document.createElement('div');
-    colorBox.classList.add('color');
-    colorBox.style.backgroundColor = COLORS[index];
-    colorPalette.appendChild(colorBox);
-  }
-
-  setFixedColor();
-  setColors();
-}
-
-function initializePixels() {
-  const pixelRows = document.getElementsByClassName('pixel-row');
-  let pixel;
-
-  for (let rowIndex = 0; rowIndex < BOARD_SIDE_SIZE; rowIndex += 1) {
-    for (let colIndex = 0; colIndex < BOARD_SIDE_SIZE; colIndex += 1) {
-      pixel = document.createElement('div');
-      pixel.classList.add('pixel');
-      pixelRows[rowIndex].appendChild(pixel);
-    }
-  }
-}
-
-function initializePixelBoard() {
-  const pixelBoard = document.createElement('section');
-  const contentContainer = document.querySelector('.content-container');
-  let pixelRow;
-
-  pixelBoard.id = 'pixel-board';
-  for (let index = 0; index < BOARD_SIDE_SIZE; index += 1) {
-    pixelRow = document.createElement('div');
-    pixelRow.classList.add('pixel-row');
-    pixelBoard.appendChild(pixelRow);
-  }
-  contentContainer.appendChild(pixelBoard);
-
-  initializePixels();
-}
-
-function selectColor(e) {
-  const targetColorBox = e.target;
-  const currentlySelectedColor = document.querySelector('.color.selected');
-
-  if (targetColorBox !== currentlySelectedColor) {
-    currentlySelectedColor.classList.remove('selected');
-    targetColorBox.classList.add('selected');
-  }
-}
-
-function initializePaletteListeners() {
-  const colorBoxes = document.getElementsByClassName('color');
-
-  for (let index = 0; index < NUM_OF_COLORS + 1; index += 1) {
-    colorBoxes[index].addEventListener('click', selectColor);
-  }
+  clearButton.addEventListener('click', clearBoard);
 }
 
 function fillPixel(e) {
@@ -111,24 +44,97 @@ function initializeBoardListeners() {
   }
 }
 
-function clearBoard() {
-  const pixels = document.getElementsByClassName('pixel');
+function fillRowsWithPixels() {
+  const pixelRows = document.getElementsByClassName('pixel-row');
+  let pixel;
 
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = 'white';
+  for (let rowIndex = 0; rowIndex < boardSideSize; rowIndex += 1) {
+    for (let colIndex = 0; colIndex < boardSideSize; colIndex += 1) {
+      pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixelRows[rowIndex].appendChild(pixel);
+    }
   }
 }
 
-function initializeClearButtonListener() {
-  const clearButton = document.getElementById('clear-board');
+function fillBoardWithRows() {
+  const pixelBoard = document.getElementById('pixel-board');
+  let pixelRow;
 
-  clearButton.addEventListener('click', clearBoard);
+  for (let index = 0; index < boardSideSize; index += 1) {
+    pixelRow = document.createElement('div');
+    pixelRow.classList.add('pixel-row');
+    pixelBoard.appendChild(pixelRow);
+  }
+}
+
+function initializePixelBoard() {
+  const pixelBoard = document.createElement('section');
+  const contentContainer = document.querySelector('.content-container');
+
+  pixelBoard.id = 'pixel-board';
+  contentContainer.appendChild(pixelBoard);
+
+  fillBoardWithRows();
+  fillRowsWithPixels();
+  initializeBoardListeners();
+}
+
+function selectColor(e) {
+  const targetColorBox = e.target;
+  const currentlySelectedColor = document.querySelector('.color.selected');
+
+  if (targetColorBox !== currentlySelectedColor) {
+    currentlySelectedColor.classList.remove('selected');
+    targetColorBox.classList.add('selected');
+  }
+}
+
+function initializePaletteListeners() {
+  const colorBoxes = document.getElementsByClassName('color');
+
+  for (let index = 0; index < NUM_OF_COLORS + 1; index += 1) {
+    colorBoxes[index].addEventListener('click', selectColor);
+  }
+}
+
+function setColors() {
+  const colorBoxes = document.querySelectorAll('.color:not(.fixed)');
+
+  for (let index = 0; index < NUM_OF_COLORS; index += 1) {
+    colorBoxes[index].style.backgroundColor = COLORS[index];
+  }
+}
+
+function setFixedColor() {
+  const fixedColorBox = document.querySelector('.color.fixed');
+
+  fixedColorBox.style.backgroundColor = FIXED_COLOR;
+}
+
+function initializePalette() {
+  const colorPalette = document.getElementById('color-palette');
+  const fixedColorBox = document.createElement('div');
+  let colorBox;
+
+  fixedColorBox.style.backgroundColor = FIXED_COLOR;
+  fixedColorBox.classList.add('color', 'fixed', 'selected');
+  colorPalette.appendChild(fixedColorBox);
+
+  for (let index = 0; index < NUM_OF_COLORS; index += 1) {
+    colorBox = document.createElement('div');
+    colorBox.classList.add('color');
+    colorBox.style.backgroundColor = COLORS[index];
+    colorPalette.appendChild(colorBox);
+  }
+
+  setFixedColor();
+  setColors();
+  initializePaletteListeners();
 }
 
 window.onload = () => {
   initializePalette();
   initializePixelBoard();
-  initializePaletteListeners();
-  initializeBoardListeners();
   initializeClearButtonListener();
 };
