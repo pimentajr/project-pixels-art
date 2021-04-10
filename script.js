320efad70870e87a65ccf58b7aec5cfeb9215232
guinhoanjos <iago.anjos@hotmail.com>
2021-04-10 01:09:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/* window.onload = function() {
-    document.querySelector('.black').className += ' selectd';
-}
-*/
-
 function createDiv(number) {
   for (let index = 0; index < number; index += 1) {
     const createdDiv = document.createElement('div');
@@ -15,16 +10,16 @@ createDiv(25);
 function changeSelectedClass(event) {
   document.querySelectorAll('.selected')[0].classList.remove('selected');
   event.target.className += ' selected';
-  }
+}
 document.getElementById('color-palette').addEventListener('click', changeSelectedClass);
 
 function changeColorPixel(event) {
   let classColor = document.querySelectorAll('.selected')[0].classList[1];
   if (event.target.classList.length !== 1) {
     event.target.classList.remove(event.target.classList[1]);
-    event.target.className +=' ' + classColor;
+    event.target.className += ' ' + classColor;
   } else {
-    event.target.className +=' ' + classColor;
+    event.target.className += ' ' + classColor;
   }
 }
 document.getElementById('pixel-board').addEventListener('click', changeColorPixel);
