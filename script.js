f099d42da56942c8a020257269b84c79ef23ffd0
Felipe Buso <felipe_buso@hotmail.com>
2021-04-09 22:34:18 -0300

    Item 6

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
 window.onload = function() {
-  let numberOfLines = 5;
-  let numberOfBox = numberOfLines;
-
-  const colorMenu = document.getElementById('color-palette');
-  const colors = ['black', 'red', 'green', 'blue'];
-  for (let index = 0; index < colors.length; index += 1) {
-    const colorSelector = document.createElement('div');
-    colorSelector.className = 'color';
-    colorSelector.id = colors[index];
-    colorSelector.style.backgroundColor = colors[index];
-    colorMenu.appendChild(colorSelector);
-  }
-
-  const pixelBoard = document.getElementById('pixel-board');
-  const boxsLine = document.getElementsByClassName('line');
-  
-  function fillBoard (value) {
-    for (let index = 0; index < value; index += 1) {
-      let lineBox = document.createElement('div');
-      lineBox.className = "line";
-      pixelBoard.appendChild(lineBox);
-    }
-    fillLine(numberOfBox);
-  }
-
-  function fillLine(value) {
-    for (let index = 0; index < value; index += 1) {
-      for (let boxIndex = 0; boxIndex < numberOfBox; boxIndex += 1) {
-        let createdBox = document.createElement('div');
-        createdBox.className = 'pixel';
-        boxsLine[index].appendChild(createdBox);
-      }
-    }
-  }
-  // function boxCreator() {
-  //   let createdBox = document.createElement('div');
-  //   createdBox.className = 'pixel';
-  // }
   fillBoard(numberOfLines);
-
 };
+
+let numberOfLines = 5;
+let numberOfBox = numberOfLines;
+const colorSelected = document.getElementById('black');
+const colorMenu = document.getElementById('color-palette');
+const colors = ['black', 'red', 'green', 'blue'];
+for (let index = 0; index < colors.length; index += 1) {
+  const colorSelector = document.createElement('div');
+  colorSelector.className = 'color';
+  colorSelector.id = colors[index];
+  colorSelector.style.backgroundColor = colors[index];
+  colorMenu.appendChild(colorSelector);
+  if  (colors[index] === 'black') {
+    colorSelector.classList.add('selected');
+  }
+}
+
+const pixelBoard = document.getElementById('pixel-board');
+const boxsLine = document.getElementsByClassName('line');
+
+function fillLine(value) {
+  for (let index = 0; index < value; index += 1) {
+    for (let boxIndex = 0; boxIndex < numberOfBox; boxIndex += 1) {
+      let createdBox = document.createElement('div');
+      createdBox.className = 'pixel';
+      boxsLine[index].appendChild(createdBox);
+    }
+  }
+}
+
+function fillBoard (value) {
+  for (let index = 0; index < value; index += 1) {
+    let lineBox = document.createElement('div');
+    lineBox.className = 'line';
+    pixelBoard.appendChild(lineBox);
+  }
+  fillLine(numberOfBox);
+}
+
+
+colorSelected.addEventListener('load', function() {
+  colorSelected.classList.add('selected');
+})
