ba920c790b9e9f8c4de2a1cb9906c8fc5f8c7eac
marcoswarmling <marcoswarmling15@gmail.com>
2021-04-10 01:27:57 -0300

    a paleta ta funcionando

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-let palselected = document.getElementById('color-palette');
 
+
+let palselected = document.getElementById('color-palette');
+let pixelboard = document.getElementById('pixel-board');
+let color = 'black'
 
 palselected.addEventListener('click',function(event){
     let selectedcolor = event.target.id
-    document.getElementById('cube1').classList.remove('selected');
-    document.getElementById('cube2').classList.remove('selected');
-    document.getElementById('cube3').classList.remove('selected');
-    document.getElementById('cube4').classList.remove('selected');
-    document.getElementById(selectedcolor).classList.add('selected'); 
 
+if (selectedcolor == 'cube2') {
+    color = 'green'
 }
-
+if (selectedcolor =='cube3') {
+    color = 'blue'
+}
+if (selectedcolor =='cube4') {
+    color = 'red'
+}
+return color
+}
 )
+
+let colorselected = document.querySelector('.selected')
+console.log(colorselected)
+
+pixelboard.addEventListener('click',function(event) {
+    let selectedpixel = event.target
+selectedpixel.style.backgroundColor = color
+
+})
+
 
 
 
