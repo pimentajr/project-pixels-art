d808cbd6d1e8a6c4adae963873af52a0a6d2e87d
Thiago Gomes <thiagog.macedom@gmail.com>
2021-04-10 20:15:27 -0300

    adicao de cores randomicas

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// gerando cores aleatórias
+function randomColor() {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+const pallet = document.querySelectorAll('.genColor');
+for (let index = 0; index < pallet.length; index += 1) {
+  const tempColor = randomColor();
+  pallet[index].style.backgroundColor = tempColor;
+}
+
 const board = document.querySelector('#pixel-board');
 const x = 5;
 const y = 5;
