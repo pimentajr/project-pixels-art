5859a1b83bb7c3fff773d5a26aa2f5522fa379b1
Dario Junior <dario.junior.rs@gmail.com>
2021-04-10 19:52:34 -0300

    Eu acho que estou virando um genio

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
 const pixelsBoard = document.querySelector('#pixel-board');
 const colorPalette = document.querySelector('#color-palette');
-
 const DEFAULT_TABLE_VALUE = 5;
-// const DEFAULT_COLOR = 'white';
-
-const createrElements = (elem, cl) => {
-  const element = document.createElement(elem);
-  element.className = cl;
-  return element;
-};
-
-
-window.onload = () => {
-  for (let line = 0; line < DEFAULT_TABLE_VALUE; line += 1) {
-    pixelsBoard.appendChild(createrElements('ul', `line ${line}`));
-    for (let column = 0; column < DEFAULT_TABLE_VALUE; column += 1) {
-      pixelsBoard.lastChild.appendChild(createrElements('li', `${line} ${column}`));
-    }
-  }
-};
+const DEFAULT_COLOR = 'white';
 
 colorPalette.addEventListener('click', (event) => {
   for (let index = 0; index < colorPalette.children.length; index += 1) {
@@ -26,4 +9,35 @@ colorPalette.addEventListener('click', (event) => {
   }
   return event.target.classList.toggle('selected');
 });
+
+
+const changePixelColor = (event) => {
+  let selectedColor = document.querySelector('#color-palette .selected');
+  selectedColor = selectedColor.classList[1];
+  let pixel = event.target;
+  pixel.style.backgroundColor = selectedColor;
+}
+
+const createrElements = (elem, cl, id) => {
+  const element = document.createElement(elem);
+  cl ? element.className = cl : '';
+  id ? element.id = id : '';
+  if (elem == 'li') {
+    element.addEventListener('click', changePixelColor);
+  }
+  return element;
+};
+
+const createTable = () => {
+  for (let line = 0; line < DEFAULT_TABLE_VALUE; line += 1) {
+    pixelsBoard.appendChild(createrElements('ul', '', `line ${line}`));
+    for (let column = 0; column < DEFAULT_TABLE_VALUE; column += 1) {
+      let pixelLine = pixelsBoard.lastChild.appendChild(createrElements('li', 'pixel', `${line} ${column}`));
+      pixelLine.style.backgroundColor = DEFAULT_COLOR;
+    }
+  }
+}
+
+createTable();
+
 
