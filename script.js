bde417bc876677bbf789ab57654895f7a6a4c685
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-04-10 11:09:49 -0300

    refaturaçao do html para o JS

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-window.onload
-let boardHeight = 5;
-let boardWidth = 5;
-let board = document.getElementById('pixel-board');
-for (let index = 1; index <= (boardHeight * boardWidth); index += 1) {
-  let square = document.createElement('div');
-  square.classList.add('pixel');
-  board.appendChild(square);
+window.onload = createBoard;
+
+function createBoard() {
+  let boardHeight = 5;
+  let boardWidth = 5;
+  let board = document.getElementById('pixel-board');
+  for (let index = 1; index <= (boardHeight * boardWidth); index += 1) {
+    let square = document.createElement('div');
+    square.classList.add('pixel');
+    board.appendChild(square);
+  }
+}
+const colorBox = document.querySelectorAll('.color');
+for (let elem of colorBox) {
+elem.addEventListener('click', selectColor);
 }
 
-function selectColor() {
-  if (class = '')
-  alert('Fui clicado!');
+function selectColor(origin) {
+  alert(origin.target);
 }
