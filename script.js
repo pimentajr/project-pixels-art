48c9799a1c323b75daf207c039d7eeef6d89e4bc
Gisele Costa <jikinha@gmail.com>
2021-04-09 21:13:19 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 function createColor(boxColor) {
   const palette = document.querySelector('#color-palette');
   const box = document.createElement('div');
-
   box.style.backgroundColor = boxColor;
   box.className = ('color');
   if (boxColor === 'black') box.classList.add('selected');
   palette.appendChild(box);
 }
-
 createColor('black');
 createColor('red');
 createColor('blue');
@@ -17,11 +15,9 @@ createColor('yellow');
 // Preenche uma grid
 function populateGrid(size) {
   const pixelBox = document.querySelector('#pixel-board');
-
   for (let index = 0; index < size; index += 1) {
     const pixelColumn = document.createElement('div');
     pixelBox.appendChild(pixelColumn);
-
     for (let index2 = 0; index2 < size; index2 += 1) {
       const pixelLine = document.createElement('div');
       pixelColumn.appendChild(pixelLine);
@@ -29,9 +25,20 @@ function populateGrid(size) {
     }
   }
 }
-
 populateGrid(5);
 
-/*
-
-*/
+// Muda a classe selected
+function clickPalette() {
+  const eventPalette = document.getElementById('color-palette');
+  eventPalette.addEventListener('click', (selectedColor) => {
+    const eventTarget = selectedColor.target;
+    const color = document.getElementsByClassName('color');
+    for (let index = 0; index < color.length; index += 1) {
+      color[index].classList.remove('selected');
+      if (eventTarget.className === 'color') {
+        eventTarget.classList.add('selected');
+      }
+    }
+  });
+}
+clickPalette();
