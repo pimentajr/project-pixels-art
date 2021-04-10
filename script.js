fb18b6a1600a116d6b0a77c264ef0f52ce1f27de
Thiago Gomes <thiagog.macedom@gmail.com>
2021-04-10 12:44:29 -0300

    corrigindo problemas do lint

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let board = document.querySelector('#pixel-board')
-let x = 5
-let y = 5
+const board = document.querySelector('#pixel-board');
+const x = 5;
+const y = 5;
 
-for (let index = 0; index < x * y; index++) {
-	let square = document.createElement('div')
-	square.style.backgroundColor = 'white'
-	square.className = 'pixel box'
+for (let index = 0; index < x * y; index += 1) {
+  const square = document.createElement('div');
+  square.style.backgroundColor = 'white';
+  square.className = 'pixel box';
 
-	board.appendChild(square)
+  board.appendChild(square);
 }
