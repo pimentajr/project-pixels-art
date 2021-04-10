d5961a5d93365f470226b56c8cb99fc9b03393d4
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-04-10 17:18:47 -0300

    only javascript on create

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-// const palette = ['black', 'red', 'blue', 'yellow'];
-// const color = document.querySelectorAll('.color');
+const palette = ['black', 'red', 'blue', 'yellow'];
+const colorPalette = document.querySelector('#color-palette');
+const clearBoard = document.querySelector('#clear-board');
+sessionStorage.color = 'black';
 
-// for (let i = 0; i < color.length; i += 1) {
-//   color[i].style.backgroundColor = palette[i];
-// }
+function colorSelectEvent(e) {
+  for (let i = 0; i < colorPalette.children.length; i += 1) {
+    const element = colorPalette.children[i];
+    if (element.className === 'color selected') {
+      element.className = 'color';
+    }
+  }
+  sessionStorage.color = e.target.style.backgroundColor;
+  e.target.className = 'color selected';
+}
+
+function colorSetEvent(e) {
+  e.target.style.backgroundColor = sessionStorage.color;
+}
+
+function clearEvent() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    const element = pixels[i];
+    element.style.backgroundColor = 'white';
+  }
+}
+
+function createPalette() {
+  for (let i = 0; i < palette.length; i += 1) {
+    const color = document.createElement('div');
+    color.style.backgroundColor = palette[i];
+    color.onclick = colorSelectEvent;
+    color.className = 'color';
+    if (i === 0) {
+      color.className += ' selected';
+    }
+    colorPalette.appendChild(color);
+  }
+}
+
+function createPixels(x, y) {
+  const table = document.createElement('table');
+  table.id = 'pixel-board';
+
+  for (let xi = 0; xi <= x; xi += 1) {
+    const tr = document.createElement('tr');
+    table.appendChild(tr);
+    for (let yi = 0; yi <= y; yi += 1) {
+      const td = document.createElement('td');
+      td.onclick = colorSetEvent;
+      td.className = 'pixel';
+      tr.appendChild(td);
+    }
+  }
+  document.body.appendChild(table);
+}
+
+createPalette();
+createPixels(5, 5);
+clearBoard.addEventListener('click', clearEvent);
