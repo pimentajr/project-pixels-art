bb1b4eab80d3fc1a23eda5bc0ef0e80eb6882870
DADA <davimarque2000@gmail.com>
2021-04-10 03:07:46 -0300

    pinta os pixels com a cor selecionada

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ window.onload = function() {
     let blackColor = document.querySelector('#preto')
     blackColor.classList.add("selected");
 }
-colorTable = document.querySelector('#color-palette')
+ 
+let colorTable = document.querySelector('#color-palette')
 colorTable.addEventListener('click', function (event){
     let colorSelected = document.querySelector('.selected')
     colorSelected.classList.remove('selected');
     event.target.classList.add('selected')
 })
+let pixelBoard = document.querySelector('#pixel-board')
+pixelBoard.addEventListener('click', function (event){
+    let colorSelected = document.querySelector('.selected')
+    let sectedColor = window.getComputedStyle(colorSelected, null).getPropertyValue("background-color");
+    let pixelPaint = event.target
+    pixelPaint.style.backgroundColor = sectedColor 
+    })
