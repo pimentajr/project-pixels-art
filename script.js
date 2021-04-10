4b7cc653fab8ecf0a033d492120ce64b912d8baa
josanjohnata <josanjohnata@gmail.com>
2021-04-10 11:44:37 -0300

    add the consts in the script

---
+++ script.js
@@ -0,0 +1,9 @@
+const boardSize = 5;
+const colors = document.getElementsByClassName('color');
+const pixelBoard = document.getElementById('pixel-board');
+const clearButton = document.getElementById('clear-board');
+const makeBoardButton = document.getElementById('make-board');
+const classSelected = 'selected';
+const paletteColor = document.getElementById('color-palette');
+const boardSize = document.getElementById('board-size');
+// I got some tips on stackoverflow.com
