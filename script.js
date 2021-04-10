fb7cdb1320f0ca38d9b01b8490258cd0f541ae13
Dario Junior <dario.junior.rs@gmail.com>
2021-04-10 18:55:05 -0300

    Comitao bomba pq esqueci e do nada resolvi varios

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+const pixelsBoard = document.querySelector('#pixel-board');
+const colorPalette = document.querySelector('#color-palette');
+
 const DEFAULT_TABLE_VALUE = 5;
-const DEFAULT_COLOR = 'white';
-const createrElements = (elem) => {
-  document.createElement(elem);
-}
+// const DEFAULT_COLOR = 'white';
 
-const pixelsTable = document.querySelector('#lines-columns');
-console.log(pixelsTable)
+const createrElements = (elem, cl) => {
+  const element = document.createElement(elem);
+  element.className = cl;
+  return element;
+};
+
+
 window.onload = () => {
-  pixelsTable.appendChild(createrElements('tr'));
+  for (let line = 0; line < DEFAULT_TABLE_VALUE; line += 1) {
+    pixelsBoard.appendChild(createrElements('ul', `line ${line}`));
+    for (let column = 0; column < DEFAULT_TABLE_VALUE; column += 1) {
+      pixelsBoard.lastChild.appendChild(createrElements('li', `${line} ${column}`));
+    }
+  }
+};
 
-  console.log(pixelsTable)
-}
+colorPalette.addEventListener('click', (event) => {
+  for (let index = 0; index < colorPalette.children.length; index += 1) {
+    colorPalette.children[index].classList.remove('selected');
+  }
+  return event.target.classList.toggle('selected');
+});
+
