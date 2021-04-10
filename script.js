31b6a258186678925b6986f354224acb39c0b3fc
Hugo Mouto <hmoutol@gmail.com>
2021-04-10 14:29:54 -0300

    Bonus10 - Correção7 - Input e Validação

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Create table on load
 window.onload = function() {
-createTable()
+  tableSize = document.getElementById('board-size').value = 5;
+  valueCheck(5)
+  tableSize = document.getElementById('board-size').value = '';
 }
 
 // get Color Palette
@@ -14,30 +16,54 @@ let pixelBoard = document.getElementById('pixel-board')
 // get Pixels
 let pixels = document.getElementsByClassName('pixel')
 
-// Create table
-// tableSize > 50 || tableSize < 5 || 
-function createTable(tableSize) {
+// Value Check
+// if (tableSize > 50) {
+//   tableSize = 50;
+// }
+// if (tableSize < 5) {
+//   tableSize = 5;
+// }
+function valueCheck(tableSize) {
   tableSize = document.getElementById('board-size').value;
-  if (tableSize === '') {
-    alert('Board inválido!')
- } else {
+//   if (!tableSize) {
+//     alert('Board inválido!')
+//  } else {
+//    createTable(tableSize)
+//   }
+  switch (true) {
+    case !tableSize:
+      alert('Board inválido!');
+      break;
+    case tableSize < 5:
+      tableSize = 5;
+      createTable(tableSize);
+      break;
+    case tableSize > 50:
+      tableSize = 50;
+      createTable(tableSize);
+      break;
+    default:
+      createTable(tableSize);
+      break;
+  }
+}
+// Create Table
+function createTable(tableSize) {
   let tableBody = document.querySelector('tbody')
-    pixelBoard.removeChild(tableBody);
-    let newTBody = document.createElement('tbody');
-    pixelBoard.appendChild(newTBody);
-    for (let i = 0; i < tableSize; i +=1) {
-      let newRow = document.createElement('tr')
-      newTBody.appendChild(newRow);
-      let tableRow = document.getElementsByTagName('tr');
-      for (let j = 0; j < tableSize; j +=1) {
-        let newPixel = document.createElement('td')
-        tableRow[i].appendChild(newPixel);
-        newPixel.className = 'pixel';
-      }
+  pixelBoard.removeChild(tableBody);
+  let newTBody = document.createElement('tbody');
+  pixelBoard.appendChild(newTBody);
+  for (let i = 0; i < tableSize; i +=1) {
+    let newRow = document.createElement('tr')
+    newTBody.appendChild(newRow);
+    let tableRow = document.getElementsByTagName('tr');
+    for (let j = 0; j < tableSize; j +=1) {
+      let newPixel = document.createElement('td')
+      tableRow[i].appendChild(newPixel);
+      newPixel.className = 'pixel';
     }
   }
 }
-
 // Color Select
 colorPalette.addEventListener('click', function(event){
   for (key of colors) {
