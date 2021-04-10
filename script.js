87091642f3aa293b5998d3c38ebfc56aa2f74da2
Marcelo Leite <marsleite@gmail.com>
2021-04-10 01:12:58 -0300

    retornando ao estagio anterior.

--- script.js
+++ script.js
@@ -4,18 +4,6 @@ function colorPalette() {
     startColor.classList.add('selected');
   }
   startColorPage();
-
-  function setSelectClass(event) {
-    let setElement = document.querySelectorAll('.selected');
-    for(let index = 0; setElement.length; index += 1) {
-      setElement[index].classList.remove('selected')
-    }
-    event.target.classList.add('selected');
-  }
-
-  let colorFather = document.querySelector('color-palette');
-
-  colorFather.addEventListener('click', setSelectClass);
 }
 
-onload = colorPalette;
+window.onload = colorPalette;
