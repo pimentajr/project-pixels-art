f7ae4de4c2be70fd24ec9d2334e4b30f061c0607
PH Assis <pieit@live.com>
2021-04-09 21:23:53 -0300

    friday push

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const pixelBoard = document.getElementById('pixel-board');
 const pixelLine = document.querySelector('#pixel-board').children;
+const colorPalette = document.querySelectorAll('#color-palette div');
 
 function CreateLineBox() {
   for (let i = 0; i < 5; i += 1) {
@@ -20,3 +21,25 @@ function createAllBoxes() {
   }
 }
 createAllBoxes();
+
+function selectPalette() {
+  for (let i = 0; i < colorPalette.length; i += 1) {
+    colorPalette[i].addEventListener('click', (event) => {
+      for (let j = 0; j < colorPalette.length; j += 1) {
+        colorPalette[j].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    });
+  }
+}
+selectPalette();
+
+function paintBlocks() {}
+const currentColor = document.querySelector('.pixel-line');
+
+pixelBoard.addEventListener('click', (event) => {
+  event.target.style.backgroundColor = currentColor;
+});
+paintBlocks();
+
+console.log(currentColor);
