889ec5c7559ec58f67f39ddb1fbfbfdab877f031
Savio Moraes <smmsavio@hotmail.com>
2021-04-10 19:58:40 -0300

    Inicia a resolucao do requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const colorPalete = document.querySelector('#color-palette');
 const colors = document.querySelectorAll('.color');
+
 function changesToBlack() {
   if (colors[0].style.backgroundColor !== 'black') {
     colors[0].style.backgroundColor = 'black';
@@ -12,7 +13,7 @@ function defineInitialSelectedColor() {
 }
 defineInitialSelectedColor();
 
-function colorChange(event) {
+function colorChangeForBlack(event) {
   const selecionedColor = document.querySelector('#color-palette').children;
   const selectedClassEvent = event;
   for (let index = 0; index < selecionedColor.length; index += 1) {
@@ -20,4 +21,13 @@ function colorChange(event) {
   }
   selectedClassEvent.target.className += ' selected';
 }
-colorPalete.addEventListener('click', colorChange);
+colorPalete.addEventListener('click', colorChangeForBlack);
+
+const coloringTable = document.querySelector('#pixel-board');
+function addColorPixel(event) {
+  const color = document.querySelector('.selected');
+  const changeColor = getComputedStyle(color).backgroundColor;
+  const coloringTableEvent = event.target;
+  coloringTableEvent.style.backgroundColor = changeColor;
+}  
+coloringTable.addEventListener('click', addColorPixel);
