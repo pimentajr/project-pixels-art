35ff3479ec632beb748241f406fceaabfbe15afc
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-10 13:52:31 -0300

    completa requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-function pageStarter() {
+function selectInitialColor() {
   document.getElementById('color-1').className = 'color selected';
+}
+
+// function getTargetElementById(targetElement) {
+//   const targetId = targetElement.id;
+//   const element = document.getElementById(targetId);
+//   return element;
+// }
+
+function classSwaper(targetElement) {
+  const palette = document.getElementsByClassName('color');
+
+  for (let index = 0; index < palette.length; index += 1) {
+    const object = palette[index];
+    if (object === targetElement) {
+      object.classList.add('selected');
+    } else {
+      object.classList.remove('selected');
+    }
+  }
+}
+
+function colorSelector(targetElement) {
+  const selectedElementColor = getComputedStyle(targetElement).backgroundColor;
+  return selectedElementColor;
+}
+
+function setSelectedColor(event) {
+  const targetElement = event.target;
+  classSwaper(targetElement);
+  colorSelector(targetElement);
+}
+
+function pageStarter() {
+  selectInitialColor();
+  document
+    .getElementById('color-palette')
+    .addEventListener('click', setSelectedColor);
 }
 
 window.onload = pageStarter;
