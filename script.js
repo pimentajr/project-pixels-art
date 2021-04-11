93a8161e1997ad215fa7141df5fc89bbf9ab2931
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-04-10 23:05:38 -0300

    pixel arts

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const palette = ['black', 'red', 'blue', 'yellow'];
+const colorPalette = document.querySelector('#color-palette');
 const clearBoard = document.querySelector('#clear-board');
-sessionStorage.color = 'black';
+const buttonBoard = document.querySelector('#generate-board');
+const pixelBoard = document.querySelector('#pixel-board');
+
+let colorStorage = 'black';
+
+function int(str) {
+  return parseInt(str, 0);
+}
 
 function colorSelectEvent(e) {
-  const colorPalette = document.querySelectorAll('.color');
-  for (let i = 0; i < colorPalette.length; i += 1) {
-    const element = colorPalette[i];
-    if (element.className === 'color selected') {
-      element.className = 'color';
-    }
+  const element = e.target;
+  if (element.className === 'color') {
+    const color = document.querySelector('.color.selected');
+    color.className = 'color';
+
+    colorStorage = element.style.backgroundColor;
+    element.className = 'color selected';
   }
-  sessionStorage.color = e.target.style.backgroundColor;
-  e.target.className = 'color selected';
 }
 
 function colorSetEvent(e) {
-  e.target.style.backgroundColor = sessionStorage.color;
+  const element = e.target;
+  if (element.className === 'pixel') {
+    element.style.backgroundColor = colorStorage;
+  }
 }
 
 function clearEvent() {
@@ -26,28 +35,69 @@ function clearEvent() {
   }
 }
 
-function createPalette() {
-  const color = document.querySelectorAll('.color');
-  for (let i = 0; i < palette.length; i += 1) {
-    color[i].style.backgroundColor = palette[i];
-    color[i].onclick = colorSelectEvent;
+function updatePixels(x, y) {
+  const board = document.querySelector('table');
+  if (x === y) {
+    const input = document.querySelector('#board-size');
+    input.value = x;
   }
-}
-
-function createPixels(x, y) {
+  const table = document.createElement('table');
+  table.id = 'pixel-board';
   for (let xi = 0; xi <= x; xi += 1) {
-    const line = document.createElement('div');
-    line.id = 'pixel-board';
+    const tr = document.createElement('tr');
+    table.appendChild(tr);
     for (let yi = 0; yi <= y; yi += 1) {
-      const pixel = document.createElement('span');
-      pixel.onclick = colorSetEvent;
-      pixel.className = 'pixel';
-      line.appendChild(pixel);
+      const td = document.createElement('td');
+      td.onclick = colorSetEvent;
+      td.className = 'pixel';
+      tr.appendChild(td);
     }
-    document.body.appendChild(line);
+  }
+  board.replaceWith(table);
+}
+
+function valueChecker(value, minimum, maximum) {
+  console.log(value, minimum, maximum);
+  if (minimum <= value && value <= maximum) {
+    updatePixels(value, value);
+  } else if (value <= minimum) {
+    updatePixels(minimum, minimum);
+  } else if (value > maximum) {
+    updatePixels(maximum, maximum);
   }
 }
 
-createPalette();
-createPixels(5, 5);
+function inputEvent() {
+  const input = document.querySelector('#board-size');
+
+  if (input.value !== '') {
+    valueChecker(int(input.value), int(input.min), int(input.max));
+  } else {
+    alert('Board inválido!');
+  }
+}
+
+function ran255() {
+  return Math.floor(Math.random() * 255);
+}
+
+function randomColors() {
+  const colors = document.querySelectorAll('.color');
+
+  for (let c = 0; c < colors.length; c += 1) {
+    const color = colors[c];
+    if (c === 0) {
+      color.style.backgroundColor = 'black';
+    } else {
+      const rgb = `rgb(${ran255()}, ${ran255()}, ${ran255()})`;
+      color.style.backgroundColor = rgb;
+    }
+  }
+}
+
+window.onload = randomColors;
+
+colorPalette.addEventListener('click', colorSelectEvent);
 clearBoard.addEventListener('click', clearEvent);
+buttonBoard.addEventListener('click', inputEvent);
+pixelBoard.addEventListener('click', colorSetEvent);
