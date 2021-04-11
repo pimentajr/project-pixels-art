2ab5618d64e7ec59f5c07a057566954f00834b1e
heyynat <natalifplima@gmail.com>
2021-04-11 19:37:21 -0400

    Requisito 6

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 window.onload = createPixels
 
-let frame = document.querySelector('#pixel-board');
+const frame = document.querySelector('#pixel-board');
 function createPixels() {
+  for (let index = 0; index < 5; index += 1) {
+    const box = document.createElement('div');
+    frame.appendChild(box);
     for (let index = 0; index < 5; index += 1) {
-        let box = document.createElement('div');
-        frame.appendChild(box);
-        for (let index = 0; index < 5; index += 1) {
-            let box = document.createElement('div');
-            box.classList.add('pixel');
-            frame.appendChild(box);
-            box.style.backgroundColor = 'white';
-            box.style.display = 'inline-block';
-            box.style.height = '38px';
-            box.style.width = '38px';
-            box.style.border = 'solid 1px black';
-        }
+      const box = document.createElement('div');
+      box.classList.add('pixel');
+      frame.appendChild(box);
+      box.style.backgroundColor = 'white';
+      box.style.display = 'inline-block';
+      box.style.height = '40px';
+      box.style.width = '40px';
+      box.style.border = 'solid 1px black';
     }
+  }
 }
+
+const elementColorBlack = document.querySelector('#color-palette').firstElementChild
+elementColorBlack.classList.add('selected');
