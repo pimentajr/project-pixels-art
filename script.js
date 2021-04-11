e9acd10ed80d602cc91cb080174359f9ac035ab9
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-11 18:50:30 -0300

    Finaliza requisitos 10 e 11

--- script.js
+++ script.js
@@ -30,6 +30,53 @@ function clearBoard() {
   }
 }
 
+function getBoardSize() {
+  const boardSize = document.getElementById('board-size').value;
+  console.log(boardSize);
+  if (boardSize === '') {
+    return null;
+  }
+  if (boardSize < 5) {
+    return 5;
+  }
+  if (boardSize > 50) {
+    return 50;
+  }
+  return boardSize;
+}
+
+function generateLines(size) {
+  const baseBoard = document.getElementById('pixel-board');
+  for (let index = 0; index < size; index += 1) {
+    const line = document.createElement('div');
+    line.className = 'pixel-line';
+    baseBoard.appendChild(line);
+  }
+}
+
+function generateCells(size) {
+  const lines = document.getElementsByClassName('pixel-line');
+  for (let index = 0; index < lines.length; index += 1) {
+    const currentLine = lines[index];
+    for (let index2 = 0; index2 < size; index2 += 1) {
+      const cell = document.createElement('div');
+      cell.className = 'pixel';
+      currentLine.appendChild(cell);
+    }
+  }
+}
+
+function generateBoard() {
+  const size = getBoardSize();
+  if (size === null) {
+    window.alert('Board inválido!');
+    return;
+  }
+  document.getElementById('pixel-board').innerHTML = '';
+  generateLines(size);
+  generateCells(size);
+}
+
 function pageStarter() {
   selectInitialColor();
   document
@@ -37,6 +84,9 @@ function pageStarter() {
     .addEventListener('click', setSelectedColor);
   document.getElementById('pixel-board').addEventListener('click', pencil);
   document.getElementById('clear-board').addEventListener('click', clearBoard);
+  document
+    .getElementById('generate-board')
+    .addEventListener('click', generateBoard);
 }
 
 window.onload = pageStarter;
