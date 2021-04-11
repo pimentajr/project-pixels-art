a883a95b318c4653a0ea3c9029aebb04951463f3
Raphael <raphaellfpg@gmail.com>
2021-04-11 05:39:51 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -16,9 +16,28 @@ let black = document.querySelector('.black');
 black.classList.add('selected');
 let blackClick = document.querySelector('.selected');
 blackClick.addEventListener('click', function() {
-    blackClick.backgroundColor = 'black'
+    blackClick.backgroundColor = 'black';
 })
 return blackClick;
 }
 
 firstColor();
+
+function removeSelected () {
+  let color = document.getElementsByClassName('color');
+  for (let index = 0; index < color.length; index += 1){
+      color[index].classList.remove('selected');
+  }
+    return color;
+}
+
+let color = document.getElementsByClassName('color');
+for (let index = 0; index < color.length; index += 1) {
+    color[index].addEventListener('click', function() {
+        removeSelected()
+        color[index].classList.add('selected')
+        
+    })
+}
+
+
