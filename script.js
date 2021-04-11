0186166430a7e771fb69ed1ea388dc07cb0d42c4
renan <renan_menezes07@hotmail.com>
2021-04-11 17:46:26 -0300

    Alterações aleatórias.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let black = document.getElementById('black');
-black.classList.add('selected');
+
+window.onload = function() {
+  let black = document.getElementById('black');
+  let blackColor = black.style.backgroundColor;
+  black.classList.add('selected');
+  sessionStorage.setItem('color', blackColor);
+}
 
 let pixelBoard = document.getElementById('pixel-board');
 
+// Cria um quadro de pixels.
 for (let index = 0; index < 25; index += 1) {
   let pixel = document.createElement('div');
   pixelBoard.appendChild(pixel);
@@ -14,38 +20,38 @@ function SelectRemotion() {
   let colorPalette = document.getElementsByClassName('color');
   for (let colorIndex = 0; colorIndex < colorPalette.length; colorIndex += 1) {
     colorPalette[colorIndex].classList.remove('selected');
-    return colorPalette;
   }
 }
 
 // Seleciona uma cor no palette.
 let colorPalette = document.getElementsByClassName('color');
 for (let colorIndex = 0; colorIndex < colorPalette.length; colorIndex += 1) {
-  let colorSelected = colorPalette[colorIndex];
-  colorSelected.addEventListener('click', function () {
+  let colorClicked = colorPalette[colorIndex];
+  colorClicked.addEventListener('click', function () {
     SelectRemotion();
-    colorSelected.classList.add('selected');
+    colorClicked.classList.add('selected');
+    sessionStorage.setItem('color', colorSelected.style.backgroundColor);
   });
 }
 
 // Pinta um pixel ao ser clicado.
 function PixelColor() {
-  let pixelBoard = document.getElementById('pixel-board');
   for (let index = 0; index < pixelBoard.length; index += 1) {
     let pixel = pixelBoard[index];
     pixel.addEventListener('click', function() {
-      pixel.style.backgroundColor = document.getElementsByClassName('selected').style.backgroundColor;
+      pixel.style.backgroundColor = sessionStorage.getItem('color');
     });
   }
 }
 
+// Pinta de branco todos os pixels.
 function clearPixels(pixelBoard) {
-  let pixelBoard = document.getElementById('pixel-board');
   for (let index2 = 0; index2 < pixelBoard.length; index2 += 1) {
     pixelBoard[index2].style.backgroundColor = 'white';
   }
 }
 
+// Clique no botão
 function clearButton () {
   let button = document.getElementsByTagName('button');
   button.addEventListener('click', clearPixels);
