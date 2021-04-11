5bae39b7403dab909df5e02fe3d6ca85dcaf1127
Gustavo Mendes <gusm217@gmail.com>
2021-04-11 15:38:35 -0300

    Correçoes lint

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // 2 -
 function coloringPalette() {	
-	let palette = document.getElementsByClassName('color');	
+	const palette = document.getElementsByClassName('color');	
 	for (let color = 0; color < palette.length; color += 1) {				
 		palette[color].style.backgroundColor = palette[color].innerText;
 		palette[color].style.color = palette[color].innerText;
 	}
-};	
+}
 
 // 3 -
 // document.querySelector('.color').style.backgroundColor = 'black';
@@ -13,60 +13,60 @@ function coloringPalette() {
 // 4 - 
 function creatingBoard() {		
 	for (let index = 1; index <= 5; index += 1) {
-		let newDiv = document.createElement('div');
+		const newDiv = document.createElement('div');
 		for (j = 1; j <= 5; j += 1) {
-			let newPixel = document.createElement('div');			
+			const newPixel = document.createElement('div');			
 			newPixel.classList.add('pixel');
 			document.getElementById('pixel-board').appendChild(newDiv);
 			newDiv.appendChild(newPixel);			
 		}
 	}
-};
+}
 
 
 // 5 -
-let newBorder = document.getElementById('color-palette').nextElementSibling;
+const newBorder = document.getElementById('color-palette').nextElementSibling;
 newBorder.classList.add('border-pixel');
 
 // 6 - 
-let pixelBlack = document.querySelector('.color');
-pixelBlack.classList.add('selected')
+const pixelBlack = document.querySelector('.color');
+pixelBlack.classList.add('selected');
 
 // 7
 document.getElementById('color-palette').addEventListener('click', selection);
 
-function selection () {
-	let whosSelected = document.querySelector('.selected');
+function selection() {
+	const whosSelected = document.querySelector('.selected');
 	if (event.target !== whosSelected) {				
 		whosSelected.classList.remove('selected');
 		event.target.classList.add('selected');		
 	}		
-};
+}
 
 
 // 8 - 
 document.getElementById('pixel-board').addEventListener('click', fillingPixels);
 
 function fillingPixels() {
-	let color = document.querySelector('.selected').innerText;	
+	const color = document.querySelector('.selected').innerText;	
 	if (event.target.backgroundColor !== color){	
 		event.target.style.backgroundColor = color;
 	}	
-};
+}
 
 
 // 9 -
-let button = document.getElementById('clear-board');
+const button = document.getElementById('clear-board');
 button.addEventListener('click', clearButton);
 
-function clearButton(){
-	let allPixels = document.querySelectorAll('.pixel');
+function clearButton() {
+	const allPixels = document.querySelectorAll('.pixel');
 	if (button = event) {
 		for (let index = 0; index < allPixels.length; index += 1) {
 			allPixels[index].style.backgroundColor = 'white';
 		}
 	}		
-};
+}
 
 
 
