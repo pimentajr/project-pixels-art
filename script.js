08bb19db64ff9b867dd0e13343670fa27940bf2f
Anajulia Brito <juliehindius@gmail.com>
2021-04-11 19:10:06 -0300

    step 6, trying to add black as first color

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/*
-for (let index = 0; index < colors.length; index += 1) {
-  let selectedColor = colors[index];
-  const colors = document.createElement('div');
-  colors.innerText = selectedColor;
-  colorPalletElement.appendChild(colors);
-}
-*/
+window.onload = () => {
+  document.getElementById('black').className = 'selected';
+};
