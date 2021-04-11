d7d537c0bc619be9e23142d25228dd7fdd62bf10
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-04-11 08:15:56 -0300

    6- Adicionado requisitos cobrados 6-2

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-/*let selectedColor = document.querySelector('.selected');
-  selectedColor.document.addEventListener('click', setColors);
- */ 
-  function setColors(pixel) {
-    pixel.style.backgroundColor = 'black';
+window.onload = function() {
+  let black = document.getElementById('black');
+  black.classList.add('selected');
+}
+
+function setColor(event) {
+  let test = event.target;
+  test.style.backgroundColor = 'black';
+}
+
+function setColorAll(event) {
+  let aux = document.querySelectorAll('.selected');
+  for (let index = 0; index < aux.length; index += 1) {
+    aux[index].remove('selected');
   }
+  event.target.classList.add('selected');
+}
 
-  
+const colorPalettes = document.querySelector('#color-palette');
+colorPalettes.addEventListener('click', setColorAll);
 
-
+const pixelBoard = document.querySelector('#pixel-board');
+function pxBoard(event) {
+  let colorPixel = document.querySelector('.selected');
+}
