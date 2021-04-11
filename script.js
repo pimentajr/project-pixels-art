97345d62c3e0bee09839202d41116b2a3d67aabe
Genivaldo <cgenivaldo46@gmail.com>
2021-04-11 03:02:22 -0300

    Exercicio 04.2 - Passando no linter

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-window.onload = function() {
-    linha()
+function linha() {
+  for (let index = 0; index < 25; index += 1) {
+    const addPixel = document.querySelector('#pixel-board');
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    addPixel.appendChild(pixel);
+  }
 }
 
-
-
-function linha(){
-    for (index = 0; index < 25; index += 1){
-        let addPixel = document.querySelector('#pixel-board')
-        let pixel = document.createElement('div')
-        pixel.classList.add('pixel')
-        addPixel.appendChild(pixel)
-    }   
-}
-
+window.onload = function carregar() {
+  linha();
+};
