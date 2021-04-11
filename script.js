4a519bd557f7f4980e20b7ba15629afa0ddd18e2
Halysson2 <halyssonsena83@gmail.com>
2021-04-11 18:24:44 -0300

    adicionando a sexta

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 window.onload = function carregar(){
     document.getElementById('black').classList.add('selected');
+
 }
+
+colorTable = document.querySelector('#color-palette')
+colorTable.addEventListener('click', function (event){
+let colorSelected = document.querySelector('.selected')
+colorSelected.classList.remove('.selected');
+event.target.classList.add('selected')
+})
+
+let pixelBoard = document.querySelector('#pixel-board')
+pixelBoard.addEventListener('click', function (event){
+    let colorSelected = document.querySelector('.selected')
+    let sectedColor = window.getComputedStyle(colorSelected, null).getPropertyValue("background-color");
+    let paintPinxel = event.target
+    paintPinxel.style.backgroundColor = sectedColor 
+    }) 
+
