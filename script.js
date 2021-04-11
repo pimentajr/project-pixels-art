e29131e9d433c87c26f811e34169b6f81f11402e
Vinicius <viniciusfod@gmail.com>
2021-04-11 16:49:18 -0300

    conserta bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const colorPalette = document.getElementById('color-palette');
-let colorToPaint = document.getElementsByClassName('selected')[0];
-
+// let colorToPaint = document.getElementsByClassName('selected')[0];
+let pixelBoard = document.getElementById('pixel-board');
 
 
 function createPixelsSize (size) {
@@ -19,13 +19,49 @@ function createPixelsSize (size) {
 }
 createPixelsSize (5)
 
-function selectedColor (event) {
-  colorToPaint.classList.remove('selected');
-  colorToPaint = event.target;
-  colorToPaint.classList.add('selected');
+window.onload = () => {
+  const firstSelectedColor = document.getElementById ('black-selector');
+  firstSelectedColor.classList = ('color selected')
 }
-colorPalette.addEventListener('click', selectedColor);
 
-function changeColor (event) {
-  
+function colorSelection() {
+  const tableColor = document.querySelector('#color-palette');
+  tableColor.addEventListener('click', (event) => {
+    const actualColor = document.querySelector('.selected');
+    actualColor.className = 'color';
+    const elementSelected = event.target;
+    elementSelected.classList = 'color selected';
+  });
 }
+colorSelection()
+
+function printingPixels(printBox) {
+  const pixelSelected = printBox;
+  switch (document.querySelector('.selected').id) {
+  case 'black-selector':
+    pixelSelected.style.backgroundColor = 'black';
+    break;
+  case 'red-selector':
+    pixelSelected.style.backgroundColor = 'red';
+    break;
+  case 'green-selector':
+    pixelSelected.style.backgroundColor = 'green';
+    break;
+  case 'blue-selector':
+    pixelSelected.style.backgroundColor = 'rgb(0, 76, 175)';
+    break;
+  default:
+  }
+}
+
+
+function selectPixel () {
+  pixelBoard.addEventListener ('click', (event) => {
+    const boxTarget = event.target;
+    if (boxTarget.id !== 'pixel-board') {
+      printingPixels(boxTarget);
+    }
+  });
+}
+
+selectPixel();
