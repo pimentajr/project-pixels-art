3d47d43242012a13b8d24d84873eb489afd93202
Marcelo Leite <marsleite@gmail.com>
2021-04-11 20:21:41 -0300

    requisito 10 bonus.

--- script.js
+++ script.js
@@ -25,17 +25,49 @@ function clearPixels() {
   }
 }
 
+function generateBoard(param) {
+  const tableFather = document.getElementById('pixel-board');
+  for (let index = 0; index < param; index += 1) {
+    const column = document.createElement('tr');
+    for (let idx = 0; idx < param; idx += 1) {
+      const row = document.createElement('td');
+      row.classList.add('pixel');
+      column.appendChild(row);
+    }
+    tableFather.appendChild(column);
+  }
+}
+
+function isVerified() {
+  const valueInput = document.getElementById('board-size').value;
+  let sizeTable = valueInput;
+  if (valueInput.length === 0 || valueInput < 0) {
+    return true;
+  } else if (sizeTable < 5) {
+    sizeTable = 5;
+  } else if (sizeTable > 50) {
+    sizeTable = 50;
+  }
+  return sizeTable;
+}
+
 function colorPalette() {
   document.getElementById('black').classList.add('selected');
-
   const clickPalette = document.getElementById('color-palette');
   clickPalette.addEventListener('click', setClassSelect);
-
   const clickPrint = document.getElementById('pixel-board');
   clickPrint.addEventListener('click', getPixel);
-
   const resetBtn = document.querySelector('.btn-style');
   resetBtn.addEventListener('click', clearPixels);
+  const btnBoard = document.getElementById('generate-board');
+  generateBoard('5');
+  btnBoard.addEventListener('click', () => {
+    if (isVerified() === true) {
+      return alert ('Board inválido');
+    } else {
+      generateBoard(isVerified());
+    }
+  });
 }
 
 window.onload = colorPalette;
