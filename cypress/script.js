9d9e40eff01fde182bac858392ac59800084681f
Anajulia Brito <juliehindius@gmail.com>
2021-04-11 16:55:48 -0300

    step 2, add a color pallet

---
+++ cypress/script.js
@@ -0,0 +1,10 @@
+let colors = ['#000000', '#F1C3D0', '#A8C870', '#97EAF4'];
+
+/*
+for (let index = 0; index < colors.length; index += 1) {
+  let selectedColor = colors[index];
+  const colors = document.createElement('div');
+  colors.innerText = selectedColor;
+  colorPalletElement.appendChild(colors);
+}
+*/
