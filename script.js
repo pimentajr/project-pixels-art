e06919db4ab8fed1a60d2820b98ba5bb9ab5aaaf
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-04-11 10:12:22 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ window.onload = function() {
   addClassPixels();
 
   function blackSelector() {
-    blackColor = document.getElementById('black');
+    let blackColor = document.getElementById('black');
     blackColor.classList.add('selected');
-}
+  }
+  blackSelector();
 
-blackSelector();
+  function classSelector() {
+    const colorSelected = document.getElementsByClassName('selected')[0];
+    const colorPalette = document.getElementById('color-palette');
+    colorPalette.addEventListener('click', function(event) {
+      colorSelected.classList.remove('selected');
+      event.target.classList.add('selected');
+    })
+  }
+  classSelector();
 
 };
