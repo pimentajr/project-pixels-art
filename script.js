4e61b26959754a3077e5326c3633c2eedf196b37
fernando lasmar <f3rnandolasmar@gmail.com>
2021-04-11 20:10:54 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const colors = document.querySelectorAll('.color'); // atribuindo array com as classes color
 const eventPixels = document.querySelector('#pixel-board');
+const clearButton = document.querySelector('#clear-board');
 
 // seleciona cor
 function selectColor(event) {
@@ -16,5 +17,13 @@ eventPixels.addEventListener('click', (event) => {
   if (event.target.className === 'pixel') {
     const evTarget = event.target;
     evTarget.style.backgroundColor = selectedColor;
+  }
+});
+
+// resetar a tabela
+clearButton.addEventListener('click', () => {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
   }
 });
