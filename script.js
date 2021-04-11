20e3b1c22b1cbb084438aa169f4c662452f30445
Dario Junior <dario.junior.rs@gmail.com>
2021-04-10 21:35:09 -0300

    apenas otimizando e estilizando um pouco.

--- script.js
+++ script.js
@@ -11,20 +11,22 @@ colorPalette.addEventListener('click', (event) => {
   return event.target.classList.toggle('selected');
 });
 
-
-const changePixelColor = (event) => {
-  let selectedColor = document.querySelector('#color-palette .selected');
-  selectedColor = selectedColor.classList[1];
-  let pixel = event.target;
-  pixel.style.backgroundColor = selectedColor;
-}
-
 const createrElements = (elem, cl, id) => {
   const element = document.createElement(elem);
-  cl ? element.className = cl : '';
-  id ? element.id = id : '';
-  if (elem == 'li') {
-    element.addEventListener('click', changePixelColor);
+  // cl ? element.id = id : '';
+  if (cl) {
+    element.className = cl;
+  }
+  // id ? element.id = id : '';
+  if (id) {
+    element.id = id;
+  }
+  if (elem === 'li') {
+    element.addEventListener('click', (event) => {
+      let selectedColor = document.querySelector('#color-palette .selected');
+      selectedColor = selectedColor.classList[1];
+      event.target.style.backgroundColor = selectedColor;
+    });
   }
   return element;
 };
@@ -33,21 +35,18 @@ const createTable = () => {
   for (let line = 0; line < DEFAULT_TABLE_VALUE; line += 1) {
     pixelsBoard.appendChild(createrElements('ul', '', `line ${line}`));
     for (let column = 0; column < DEFAULT_TABLE_VALUE; column += 1) {
-      let pixelLine = pixelsBoard.lastChild.appendChild(createrElements('li', 'pixel', `${line} ${column}`));
+      const pixelLine = pixelsBoard.lastChild.appendChild(createrElements('li', 'pixel', `${line} ${column}`));
       pixelLine.style.backgroundColor = DEFAULT_COLOR;
     }
   }
-}
+};
 
 createTable();
 
 resetButton.addEventListener('click', () => {
   for (let lines = 0; lines < DEFAULT_TABLE_VALUE; lines += 1) {
     for (let column = 0; column < DEFAULT_TABLE_VALUE; column += 1) {
-      pixelsBoard.children[lines].children[column].style.backgroundColor = DEFAULT_COLOR
+      pixelsBoard.children[lines].children[column].style.backgroundColor = DEFAULT_COLOR;
     }
-
-    // console.log(pixelsBoard.children[lines])
   }
-  // console.log(pixelsBoard)
-})
+});
