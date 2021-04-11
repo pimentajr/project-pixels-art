dc70df1f1c10ef974b1aaa7391d774af650c1316
Ghislaine <glenox79@gmail.com>
2021-04-11 16:21:51 -0300

    Correcoes eventos

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const colorSelected = document.querySelectorAll('#color-palette');
+const color = document.querySelectorAll('.color');
+const pixelBoard = document.querySelector('#pixel-board')
+const selectColor = document.querySelector('.selected');
 
 function selectColor() {
   colorSelected.addEventListener('click', (event) => {
     const etarget = event.target;
-    const color = document.querySelectorAll('.color')[0];
     color.forEach((element) => element.classList.remove('selected'));
     etarget.classList.add('selected');
   });
 }
 selectColor();
+
+function paintPixel() {
+  pixelBoard.addEventListener('click', (event) => {
+    const etarget = event.target;
+    etarget.style.backgroundColor = getComputedStyle(selectColor).backgroundColor;
+  });
+}
+paintPixel();
