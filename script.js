57b7164d147983a09470b409441baec0536a254f
renan <renan_menezes07@hotmail.com>
2021-04-11 20:17:01 -0300

    Várias alterações.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-window.onload = function () {
+function getBlack() {
   let black = document.getElementById('black');
-  let blackColor = black.style.backgroundColor;
+  let blackColor = window.getComputedStyle(black).backgroundColor;
   black.classList.add('selected');
   sessionStorage.setItem('color', blackColor);
-};
+}
+getBlack();
 
 let pixelBoard = document.getElementById('pixel-board');
 
@@ -16,36 +17,40 @@ for (let index = 0; index < 25; index += 1) {
 
 // Remove a classe 'seleção' de uma cor.
 function SelectRemotion() {
-  let Palette = document.getElementsByClassName('color');
-  for (let colorIndex = 0; colorIndex < Palette.length; colorIndex += 1) {
-    Palette[colorIndex].classList.remove('selected');
+  let palette = document.getElementsByClassName('color');
+  for (let colorIndex = 0; colorIndex < palette.length; colorIndex += 1) {
+    palette[colorIndex].classList.remove('selected');
   }
+}
+function selectColor (event) {
+  SelectRemotion();
+  let selectedColor = event.target;
+  selectedColor.classList.add('selected');
+  sessionStorage.setItem('color', window.getComputedStyle(selectedColor).backgroundColor);
 }
 
 // Ao clicar em uma das cores da paleta, faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
-palette.addEventListener('click', function(event) {
-  SelectRemotion;
-  let selectedColor = event.target;
-  selectedColor.classList.add('selected');
-  sessionStorage.setItem('color', selectedColor.style.background);
-});
+const palette = document.getElementsByClassName('color');
+const colorPalette = document.querySelector('#color-palette');
+colorPalette.addEventListener('click', selectColor);
 
 // Pinta um pixel ao ser clicado.
-pixelBoard.addEventListener('click', function(event) {
+pixelBoard.addEventListener('click', function (event) {
   let pixelColored = event.target;
   pixelColored.style.background = sessionStorage.getItem('color');
 });
 
 // Pinta de branco todos os pixels.
 function clearPixels() {
-  let pixels = document.getElementById('pixel');
-  for (let index2 = 0; index2 < pixel.length; index2 += 1) {
+  let pixels = document.getElementsByClassName('pixel');
+  for (let index2 = 0; index2 < pixels.length; index2 += 1) {
     pixels[index2].style.backgroundColor = 'white';
   }
 }
 
 // Clique no botão
 function clearButton() {
-  let button = document.getElementsByTagName('button');
+  let button = document.getElementById('clear-board');
   button.addEventListener('click', clearPixels);
 }
+clearButton()
