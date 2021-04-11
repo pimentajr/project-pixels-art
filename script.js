c1b2f15047226da6166a81ae4329afdd3fbe21f0
israeltonholo <israeltonholo@hotmail.com>
2021-04-11 14:32:34 -0300

    adiciona exer 8

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-window.onload = function onLoadWindow() {
-  let divSelected = document.getElementById('color-palette');
-  let divSelectedUpdate = document.querySelector('.selected');
+window.onload = onLoadWindow();
+
+function onLoadWindow() {
+  /*function getClassNameOnClick(event) {
+    return event.target.className;
+  }
+  console.log(addEventListener('click', getClassNameOnClick));*/
+  let divColorSelected = document.getElementById('color-palette');
+  let divColorSelectedUpdate = document.querySelector('.selected');
   let colorSelected = document.querySelector('.selected').classList[1];
-  console.log(divSelectedUpdate);
-  console.log(document.getElementById('color-palette'));
-  divSelected.addEventListener('click', function eventFunction(event) {
-    divSelectedUpdate.classList.remove('selected');
-    divSelectedUpdate = event.target;
-    divSelectedUpdate.classList.add('selected');
-    console.log(divSelectedUpdate);
-    console.log(document.getElementById('color-palette'));
-    colorSelected = divSelectedUpdate.classList[1];
+  //sessionStorage.setItem('colorSelected', colorSelected);
+  //console.log(sessionStorage.colorSelected);
+  divColorSelected.addEventListener('click', function(event) {
+    divColorSelectedUpdate.classList.remove('selected');
+    divColorSelectedUpdate = event.target;
+    divColorSelectedUpdate.classList.add('selected');
+    colorSelected = divColorSelectedUpdate.classList[1];
+    //sessionStorage.setItem('colorSelected', colorSelected);
   });
+  
+  let pixelSelected = document.getElementById('pixel-board');
+  pixelSelected.addEventListener('click', function(event) {
+    let printPixel = event.target;
+    if (printPixel.classList.length > 1) {
+      printPixel.classList.remove(printPixel.classList[1]);
+    }
+    printPixel.classList.add(colorSelected);
+  })
+  //let varMain = document.getElementsByTagName('main');
+  //varBody.addEventListener('click', getClassNameOnClick('click'));
+  //addEventListener('click', getClassNameOnClick);
+  
+  //eventClick(divSelected);
 };
