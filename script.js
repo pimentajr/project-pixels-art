7dc7c8931337609ad8d8ae7639ac37f5676ab6af
fernando lasmar <f3rnandolasmar@gmail.com>
2021-04-11 19:47:36 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colors = document.querySelectorAll('.color'); // atribuindo array com as classes color
+const eventPixels = document.querySelector('#pixel-board');
 
 // seleciona cor
 function selectColor(event) {
@@ -10,3 +11,10 @@ for (let index = 0; index < colors.length; index += 1) {
 }
 
 // pinta a tabela
+eventPixels.addEventListener('click', (event) => {
+  const selectedColor = document.querySelector('.selected').id;
+  if (event.target.className === 'pixel') {
+    const evTarget = event.target;
+    evTarget.style.backgroundColor = selectedColor;
+  }
+});
