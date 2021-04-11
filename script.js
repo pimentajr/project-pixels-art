8fd512fd2a45d44c1ebef1e9bc5cade6ac98404f
Vinicius <viniciusfod@gmail.com>
2021-04-11 17:31:14 -0300

    erros lint

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const colorPalette = document.getElementById('color-palette');
+// const colorPalette = document.getElementById('color-palette');
 // let colorToPaint = document.getElementsByClassName('selected')[0];
-let pixelBoard = document.getElementById('pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 
-
-function createPixelsSize (size) {
-    let pixelBox = document.getElementById('pixel-board')
-    for (let index = 0; index < size; index += 1) {
-      let pixelColumn = document.createElement('div');
-      pixelBox.appendChild(pixelColumn);
+function createPixelsSize(size) {
+  let pixelBox = document.getElementById('pixel-board')
+  for (let index = 0; index < size; index += 1) {
+    let pixelColumn = document.createElement('div');
+    pixelBox.appendChild(pixelColumn);
       
-
-      for (let index2 = 0; index2 < size; index2 += 1) {
-        let pixelLine = document.createElement('div');
-        pixelBox.appendChild(pixelLine);
-        pixelLine.className = 'pixel';
+    for (let index2 = 0; index2 < size; index2 += 1) {
+      let pixelLine = document.createElement('div');
+      pixelBox.appendChild(pixelLine);
+      pixelLine.className = 'pixel';
       }
     }
 }
