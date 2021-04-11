a710805d427ba1dee9370dc076b3fbb80d897cd7
Gabriela Azevedo <gabiazevedoms@gmail.com>
2021-04-10 22:58:04 -0300

    Adiciona correçao linter

--- script.js
+++ script.js
@@ -29,23 +29,23 @@ for (let index = 1; index <= 5; index += 1) {
   for (let indice = 1; indice <= 5; indice += 1) {
     const columnLines = document.createElement('td');
     columnLines.classList = 'pixel';
-    columnLines.addEventListener('click', function createEvents(eventos) {
+    columnLines.addEventListener('click', (buttom) => {
       if (document.querySelector('.selected')) {
         const color = document.querySelector('.selected').style.backgroundColor;
-        eventos.target.style.backgroundColor = color;
+        buttom.target.style.backgroundColor = color;
       }
     });
-  tableLines.appendChild(columnLines);
+    tableLines.appendChild(columnLines);
   }
 
-// Criando botão
+  // Criando botão
 
-const firstEventButton = document.getElementById('color-palette');
+  const firstEventButton = document.getElementById('color-palette');
 
-firstEventButton.addEventListener('click', function createButton1(event1) {
-  if (document.querySelector('.selected') !== null) {
-    (document.querySelector('.selected').classList.remove('selected'));
-  }
-  event1.target.classList.add('selected');
-});
+  firstEventButton.addEventListener('click', (event2) => {
+    if (document.querySelector('.selected') !== null) {
+      (document.querySelector('.selected').classList.remove('selected'));
+    }
+    event2.target.classList.add('selected');
+  });
 }
