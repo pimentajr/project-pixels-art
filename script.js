f701bfcf8f2210838abdb83f3bcee00b5f781527
Marcelo Leite <marsleite@gmail.com>
2021-04-11 10:30:28 -0300

    requisito 7.

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function colorPalette() {
 }
 
 window.onload = colorPalette;
+
+function setClassSelect(event) {
+  const setAtributte = document.querySelectorAll('.color');
+  for (let index = 0; index < setAtributte.length; index += 1) {
+    setAtributte[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+addEventListener('click', setClassSelect);
