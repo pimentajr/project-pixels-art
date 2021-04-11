8c2e599e1dcd1e86b5f2d12e9e824fad562c11d6
Marcelo Leite <marsleite@gmail.com>
2021-04-11 14:06:57 -0300

    refatorando o codigo e atendendo o requisito 6 e 7.

---
+++ script.js
@@ -0,0 +1,15 @@
+function setClassSelect(event) {
+  const setAtributte = document.querySelectorAll('.color');
+  for (let index = 0; index < setAtributte.length; index += 1) {
+    setAtributte[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+function colorPalette() {
+  document.getElementById('black').classList.add('selected');
+  const clickPalette = document.getElementById('color-palette');
+  clickPalette.addEventListener('click', setClassSelect);
+}
+
+window.onload = colorPalette;
