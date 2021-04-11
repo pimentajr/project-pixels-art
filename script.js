3ebcbe6d05c2753bcf2c7421b3a9f2b2a511bd7f
Nalu <aluziapantoja@gmail.com>
2021-04-11 17:27:17 -0300

    Req 1

---
+++ script.js
@@ -0,0 +1,12 @@
+//1
+let body = document.body;
+
+let title = document.createElement('h1');
+
+body.appendChild(title);
+
+title.id = 'title';
+
+let titleText = document.createTextNode('Paleta de Cores');
+
+title.appendChild(titleText);
