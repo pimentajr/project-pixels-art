29099550ad4cdbb0b6e4b45cfbc22b29b97361aa
Genivaldo <cgenivaldo46@gmail.com>
2021-04-11 04:33:45 -0300

    Exercicio 06-Defina a cor preta como cor inicial ao carrefar a pagina

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function linha() {
-  for (let index = 0; index < 25; index += 1) {
+  const arrayLinha = 25;
+  for (let index = 0; index < arrayLinha; index += 1) {
     const addPixel = document.querySelector('#pixel-board');
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     addPixel.appendChild(pixel);
   }
 }
+function classSelect() {
+  const select = document.getElementById('black');
+  select.classList.add('select');
+}
 
 window.onload = function carregar() {
   linha();
+  classSelect();
 };
