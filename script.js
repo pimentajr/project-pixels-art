f9dcd204aa834eff099ada973b18af6a54e4a525
Vitor <vitorguima@gmail.com>
2021-04-11 02:46:26 -0300

    alguns ajustes lint - ainda nao finalizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // adiciona fundo branco para pixel da paintBoard
 function addWhiteToBoard() {
-  let paintBoardPixel = document.querySelectorAll('.pixel');
+  const paintBoardPixel = document.querySelectorAll('.pixel');
   for (let index = 0; index < paintBoardPixel.length; index += 1) {
     paintBoardPixel[index].style.backgroundColor = 'rgb(255, 255, 255)';
   }
@@ -10,8 +10,8 @@ addWhiteToBoard();
 
 // adiciona tag selected p/ cor preta ao iniciair a página
 function addSelectedToBlack() {
-  let countSelectClass = document.getElementsByClassName('selected');
-  let getBlackColor = document.querySelector('.black');
+  const countSelectClass = document.getElementsByClassName('selected');
+  const getBlackColor = document.querySelector('.black');
   if (countSelectClass.length === 0) {
     getBlackColor.classList.add('selected');
   }
@@ -21,9 +21,9 @@ addSelectedToBlack();
 
 // evento click para aplicar cor - ocorrer dentro de #pixel-board
 function applySelectedColor() {
-  let getPaintTable = document.querySelector('#pixel-board');
-  let getSelectedColor = document.querySelector('.selected');
-  let compStyles = window.getComputedStyle(getSelectedColor);
+  const getPaintTable = document.querySelector('#pixel-board');
+  const getSelectedColor = document.querySelector('.selected');
+  const compStyles = window.getComputedStyle(getSelectedColor);
 
   getPaintTable.addEventListener('click', function (event) {
     event.target.style.backgroundColor = compStyles.getPropertyValue('background-color');
@@ -34,15 +34,15 @@ applySelectedColor();
 
 // remove a classe selected de um determinado elemento
 function removeSelectedClass() {
-  let countSelectClass = document.getElementsByClassName('selected');
+  const countSelectClass = document.getElementsByClassName('selected');
   for (let index = 0; index < countSelectClass.length; index += 1) {
     countSelectClass[index].classList.remove('selected');
   }
 }
 
-// Altera a clor que será utilizada no preenchimento do pixel
+// Altera a cor que será utilizada no preenchimento do pixel
 function changeColorSelected() {
-  let getColorsPallete = document.querySelector('#color-palette');
+  const getColorsPallete = document.querySelector('#color-palette');
   getColorsPallete.addEventListener('click', function (event) {
     removeSelectedClass();
     event.target.classList.add('selected');
@@ -54,7 +54,7 @@ changeColorSelected();
 
 // limpa toda a boardpaint
 function clearButton() {
-  let getClearButton = document.getElementById('clear-board');
+  const getClearButton = document.getElementById('clear-board');
   getClearButton.addEventListener('click', addWhiteToBoard);
 }
 
@@ -62,10 +62,10 @@ clearButton();
 
 // gerar quantidade N de divs.tr (altura)
 function generateHeightDivs(number) {
-  let getPaintBoard = document.querySelector('#pixel-board');
+  const getPaintBoard = document.querySelector('#pixel-board');
 
   for (let index = 0; index < number; index += 1) {
-    let heightDiv = document.createElement('div');
+    const heightDiv = document.createElement('div');
     heightDiv.className = 'tr';
     getPaintBoard.appendChild(heightDiv);
   }
@@ -73,11 +73,11 @@ function generateHeightDivs(number) {
 
 // quantidade de divs.pixel existentes dentro de um div.tr
 function generateWidthDivs(number) {
-  let getHeightDiv = document.querySelectorAll('.tr');
+  const getHeightDiv = document.querySelectorAll('.tr');
 
   for (let index = 0; index < getHeightDiv.length; index += 1) {
     for (let index2 = 0; index2 < number; index2 += 1) {
-      let widthDiv = document.createElement('div');
+      const widthDiv = document.createElement('div');
       widthDiv.className = 'pixel';
       getHeightDiv[index].appendChild(widthDiv);
     }
@@ -86,30 +86,16 @@ function generateWidthDivs(number) {
 
 // puxa informação do input e armazena em uma variável
 function getInputData() {
-  let inputData = document.getElementById('board-size');
-  let pixelAmount = inputData.value;
+  const inputData = document.getElementById('board-size');
+  const pixelAmount = inputData.value;
 
   return pixelAmount;
 }
 
-// gera paintboard conforme número do input
-function generatePaintBoard() {
-  let boardSizeButton = document.getElementById('generate-board');
-
-  boardSizeButton.addEventListener('click', function() {
-    generateMax();
-    generateMin();
-    generateStandard();
-    exclaimer();
-  })
-}
-
-generatePaintBoard();
-
 // remove elementos div paintboard
 function removeBoardPaint() {
   let getHeightDivs = document.querySelectorAll('.tr');
-  for (index = 0; index < getHeightDivs.length; index += 1) {
+  for (let index = 0; index < getHeightDivs.length; index += 1) {
     getHeightDivs[index].remove();
   }
 }
@@ -137,7 +123,7 @@ function generateMax() {
 
 // gera paintBoard para valores > 5 && <= 50
 function generateStandard() {
-  if (getInputData() >= 5 && getInputData() <= 50 ) {
+  if (getInputData() >= 5 && getInputData() <= 50) {
     removeBoardPaint();
     generateHeightDivs(getInputData());
     generateWidthDivs(getInputData());
@@ -151,3 +137,17 @@ function exclaimer() {
     window.alert('Board inválido!');
   }
 }
+
+// gera paintboard conforme número do input
+function generatePaintBoard() {
+  const boardSizeButton = document.getElementById('generate-board');
+
+  boardSizeButton.addEventListener('click', function () {
+    generateMax();
+    generateMin();
+    generateStandard();
+    exclaimer();
+  })
+}
+
+generatePaintBoard();
