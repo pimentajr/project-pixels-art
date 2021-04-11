0ca3bfe1d603994baa4b31d41a76e5782b533f78
BeatrizR <biarl.ramalhorl@gmail.com>
2021-04-11 13:14:24 -0300

    correçao requsito 02

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let paletList = document.querySelector('#color-palette');
+const paletList = document.querySelector('#color-palette');
 
 function addItemPalette() {
   for (let index = 0; index < 4; index += 1) {
-    let colorPalet = document.createElement('li');
+    const colorPalet = document.createElement('li');
     colorPalet.className = 'color';
     paletList.appendChild(colorPalet);
- }
+  }
 }
 addItemPalette();
 
-let paletteItem = document.querySelectorAll('.color');
+const paletteItem = document.querySelectorAll('.color');
 
 function addcolorPalette(colors) {
   for (let index = 0; index < paletteItem.length; index += 1) {
-    let color = paletteItem[index];
+    const color = paletteItem[index];
     color.style.backgroundColor = colors[index];
- }
+  }
 }
 addcolorPalette(['black', 'red', 'blue', 'yellow']);
