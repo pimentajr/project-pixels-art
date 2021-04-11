08fdfd88d59e0e323f6b70b9b2ab30f369a4f70d
Ghislaine <glenox79@gmail.com>
2021-04-11 16:09:44 -0300

    Corrigindo novamente os eventos

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-let black = document.querySelector('#black').style.backgroundColor = 'black';
-let orange = document.querySelector('#orange').style.backgroundColor = 'orangered';
-let green = document.querySelector('#green').style.backgroundColor = 'green';
-let purple = document.querySelector('#purple').style.backgroundColor = 'purple';
+const colorSelected = document.querySelectorAll('#color-palette');
 
-const listColorSelected = document.querySelectorAll('.color');
-const colorSelect = document.querySelector('.selected');
-
-function selectColor(){
-  for(let i = 0; i < listColorSelected.length; i += 1){
-    const clickColor = listColorSelected[i];
-
-    clickColor.addEventListener('click', function(event){
-      if(clickColor === event.target){
-        clickColor.classList.add('selected');
-      }
-      else{
-        clickColor.classList.remove('selected');
-      }
-    });
-  }
+function selectColor() {
+  colorSelected.addEventListener('click', (event) => {
+    const etarget = event.target;
+    const color = document.querySelectorAll('.color')[0];
+    color.forEach((element) => element.classList.remove('selected'));
+    etarget.classList.add('selected');
+  });
 }
 selectColor();
