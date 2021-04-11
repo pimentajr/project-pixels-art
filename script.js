fd857f3737ad7876bd966743481b75e8ad1e2326
Marcelo Leite <marsleite@gmail.com>
2021-04-11 10:54:07 -0300

    consertando o codigo para a funcionalidade ok.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-window.onload = function colorPalette() {
+function colorPalette() {
   const startColor = document.getElementById('black');
   startColor.classList.add('selected');
-};
-
-function setClassSelect(event) {
-  const setAtributte = document.querySelectorAll('.color');
-  for (let index = 0; index < setAtributte.length; index += 1) {
-    setAtributte[index].classList.remove('selected');
+  function setClassSelect(event) {
+    const setAtributte = document.querySelectorAll('.color');
+    for (let index = 0; index < setAtributte.length; index += 1) {
+      setAtributte[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
   }
-  event.target.classList.add('selected');
+  const clickPalette = document.getElementById('color-palette');
+  clickPalette.addEventListener('click', setClassSelect);
 }
 
-const clickPalette = document.querySelector('#color-palette');
-clickPalette.addEventListener('click', setClassSelect);
+window.onload = colorPalette;
