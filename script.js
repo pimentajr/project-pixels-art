36aed0cf1a4e0bb26847e9d77545b83ded2c77b9
Maran <maranmorimoto@gmail.com>
2021-04-11 17:08:06 -0300

    item 9, done

--- script.js
+++ script.js
@@ -20,8 +20,24 @@ const pixelBoard = document.getElementById('pixel-board');
 
 // https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 function printColor(event) {
-  var selectedColor = document.querySelector('.selected');
-  var theCSSprop = window.getComputedStyle(selectedColor).getPropertyValue('background-color');event.target.style.backgroundColor = theCSSprop;
+  let selectedColor = document.querySelector('.selected');
+  let theCSSprop = window.getComputedStyle(selectedColor).getPropertyValue('background-color'); 
+  event.target.style.backgroundColor = theCSSprop;
 }
 
 pixelBoard.addEventListener('click', printColor);
+
+// BUTTON -CLEAR
+
+let button = document.getElementById('clear-board');
+
+function clearAllPixels () {
+ var clearPixel = document.getElementsByClassName('pixel');
+ for(index = 0; index < clearPixel.length; index += 1) { 
+  clearPixel[index].style.backgroundColor = 'white';
+ }
+ var board = document.querySelector('#pixel-board');
+ board.style.backgroundColor = 'white';
+}
+
+button.addEventListener('click', clearAllPixels);
