b458716d39b07c345ce8496433ce3ef7e42c1f27
Wilquer82 <wilquer.fig@gmail.com>
2021-04-12 13:14:47 -0300

    Requisito 6 02

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-window.onload = function() {
-  corPincel = document.getElementsByClassName('color').select(getElementById("black").style);
-}
-document.onclick = oQueFazer(); 
+//window.onload = function(inicio) {
+  const corPincel = document.getElementsByClassName('color');
+  corPincel[0].classList.add('selected');
+//}
 
-function oQueFazer(){
-  if 
-}
+// document.onclick = oQueFazer(); 
 
-addCor();
-mudaCor();
-function altCor(e); {
-  switch addEventListener("click") {
-    case document.getElementById("red").addEventListener("click")
-    corPincel= getComputedStyle("red");
-    break;
-    case document.getElementById("orange").addEventListener("click")
-    corPincel= getComputedStyle("orange");
-    break;
-    case document.getElementById("yellow").addEventListener("click")
-    corPincel= getComputedStyle("yellow");
-    break;
-    case document.getElementById("black").addEventListener("click")
-    corPincel= getComputedStyle("black");
-    break;
-  }
-}
+// function oQueFazer(){
+//   if 
+// }
 
-function mudaCor(e);{
-  document.getElementById("pixel").addEventListener("click", mudaCor);
+// addCor();
+// mudaCor();
+// function altCor(e); {
+//   switch addEventListener("click") {
+//     case document.getElementById("red").addEventListener("click")
+//     corPincel= getComputedStyle("red");
+//     break;
+//     case document.getElementById("orange").addEventListener("click")
+//     corPincel= getComputedStyle("orange");
+//     break;
+//     case document.getElementById("yellow").addEventListener("click")
+//     corPincel= getComputedStyle("yellow");
+//     break;
+//     case document.getElementById("black").addEventListener("click")
+//     corPincel= getComputedStyle("black");
+//     break;
+//   }
+// }
+
+// function mudaCor(e);{
+//   document.getElementById("pixel").addEventListener("click", mudaCor);
