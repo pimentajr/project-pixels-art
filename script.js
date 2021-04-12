b40b1caa7fdf6d57d4168a08625c76ef6d5b3513
Antonio Malato <malatoneto@gmail.com>
2021-04-12 13:32:39 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
+
+let colorsArray = ['rgb(000, 000, 000)', 'rgb(61, 128, 235)', 'rgb(204, 152, 31)', 'rgb(204, 31, 106)']
+
 let sectionBoxColors = document.querySelector('#color-palette');
 
-for (let index = 0; index < 4; index += 1) {
+for (let index = 0; index < colorsArray.length; index += 1) {
     let createBoxColors = document.createElement('div');
     sectionBoxColors.appendChild(createBoxColors);
 
-    let boxColorDiv = createBoxColors;
-    boxColorDiv.className = 'color'
-    boxColorDiv.id = "box-color" + index
+    createBoxColors.className = 'color';
+    createBoxColors.style.backgroundColor = colorsArray[index];
+    document.getElementsByClassName('color')[0].className = 'color selected'
 
 }
-console.log(sectionBoxColors);
-
-let box0 = document.querySelector('#box-color0').style.backgroundColor = 'rgb(000, 000, 000)';
-let box1 = document.querySelector('#box-color1').style.backgroundColor = 'rgb(61, 128, 235)';
-let box2 = document.querySelector('#box-color2').style.backgroundColor = 'rgb(204, 152, 31)';
-let box3 = document.querySelector('#box-color3').style.backgroundColor = 'rgb(204, 31, 106)';
 
 let table = document.querySelector('#pixel-board');
 
-for (let index = 0; index < 5; index++) {
-    let createPixel = document.createElement('tr');
-    createPixel.className = 'base';
-    table.appendChild(createPixel);
-    
-    for (let index = 0; index < 5; index += 1) {
-        let createLine = document.createElement('th');
-        createLine.className = 'pixel';
-        createLine.style.backgroundColor = 'rgb(255, 255, 255)'
-        createPixel.appendChild(createLine);
-        
+function createTable(numberRows, unityNumbers) {
+    for (let index = 0; index < numberRows; index += 1) {
+        let createLine = document.createElement('tr');
+        createLine.className = 'row';
+        table.appendChild(createLine);
+
+        for (let index = 0; index < unityNumbers; index += 1) {
+            let createUnityPixel = document.createElement('th');
+            createUnityPixel.className = 'pixel';
+            createUnityPixel.style.backgroundColor = 'rgb(255, 255, 255)';
+            createLine.appendChild(createUnityPixel);
+        }
+
     }
 }
+createTable(5, 5);
+
+/* function startBlackColor() {
+    let setTarget = document.querySelector('.pixel');
+    setTarget.style.backgroundColor = 'rgb(000, 000, 000)';
+    
+    let selected = setTarget.style.backgroundColor;
+  
+    setTarget.addEventListener('click', function() {
+        if (selected === 'click');
+        selected = setTarget
+    });
+}; */
