19487d77a573ee0154d2a724683507ce0fffabda
Gabriela Azevedo <gabiazevedoms@gmail.com>
2021-04-12 16:45:59 -0300

    Adiciona requisito 9

--- script.js
+++ script.js
@@ -16,6 +16,25 @@ orangeSquare.style = 'background-color: orange';
 const yellowSquare = document.getElementById('yellow');
 yellowSquare.style = 'background-color: yellow';
 
+// Criando seção para armazenar o botão "limpar";
+
+const sectionPixelBoard = document.createElement('section');
+document.body.appendChild(sectionPixelBoard);
+
+const button = document.createElement('button');
+button.innerText = 'Limpar';
+button.id = 'clear-board';
+sectionPixelBoard.appendChild(button);
+
+function changeColor() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    const removeColor = pixels[index];
+    removeColor.style.backgroundColor = 'white';
+  }
+}
+button.addEventListener('click', changeColor);
+
 // Criando tabela
 
 const divPixels = document.createElement('div');
