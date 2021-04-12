e7f456810c06d49f1ba182704fd62a000a87a628
Antonio Malato <malatoneto@gmail.com>
2021-04-12 16:20:54 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
+const colorsArray = ['black', 'red', 'blue', 'yellow'];
 
-let colorsArray = ['rgb(000, 000, 000)', 'rgb(61, 128, 235)', 'rgb(204, 152, 31)', 'rgb(204, 31, 106)']
-
-let sectionBoxColors = document.querySelector('#color-palette');
-
-for (let index = 0; index < colorsArray.length; index += 1) {
+function createColors(colors) {
+  const sectionBoxColors = document.querySelector('#color-palette');
+  for (let index = 0; index < colors.length; index += 1) {
     let createBoxColors = document.createElement('div');
     sectionBoxColors.appendChild(createBoxColors);
-
     createBoxColors.className = 'color';
     createBoxColors.style.backgroundColor = colorsArray[index];
-    document.getElementsByClassName('color')[0].className = 'color selected'
-
+  }
 }
+createColors(colorsArray);
 
-let table = document.querySelector('#pixel-board');
-
+const table = document.querySelector('#pixel-board');
 function createTable(numberRows, unityNumbers) {
-    for (let index = 0; index < numberRows; index += 1) {
-        let createLine = document.createElement('tr');
-        createLine.className = 'row';
-        table.appendChild(createLine);
+  for (let index = 0; index < numberRows; index += 1) {
+    let createLine = document.createElement('tr');
+    createLine.className = 'row';
+    table.appendChild(createLine);
 
-        for (let index = 0; index < unityNumbers; index += 1) {
-            let createUnityPixel = document.createElement('th');
-            createUnityPixel.className = 'pixel';
-            createUnityPixel.style.backgroundColor = 'rgb(255, 255, 255)';
-            createLine.appendChild(createUnityPixel);
-        }
-
+    for (let index = 0; index < unityNumbers; index += 1) {
+      let createUnityPixel = document.createElement('th');
+      createUnityPixel.className = 'pixel';
+      createUnityPixel.style.backgroundColor = 'white';
+      createLine.appendChild(createUnityPixel);
     }
+  }
 }
 createTable(5, 5);
 
-/* function startBlackColor() {
-    let setTarget = document.querySelector('.pixel');
-    setTarget.style.backgroundColor = 'rgb(000, 000, 000)';
-    
-    let selected = setTarget.style.backgroundColor;
-  
-    setTarget.addEventListener('click', function() {
-        if (selected === 'click');
-        selected = setTarget
+function inicitalColor() {
+  document.querySelector('#color-palette').firstElementChild.className = 'color selected';
+}
+inicitalColor();
+
+function addOrRemoveClass() {
+  const addOrRemove = document.querySelectorAll('.color');
+
+  for (let index = 0; index < addOrRemove.length; index += 1) {
+    let item = addOrRemove[index];
+
+    item.addEventListener('click', function(event) {
+      let selected = document.querySelector('.selected');
+      if (item.className === 'color') {
+        event.target.classList.add('selected');
+        selected.classList.remove('selected');
+      }
     });
-}; */
+  };
+};
+addOrRemoveClass();
