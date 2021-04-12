eaf8bd7d21a72924e220f523932d9dab834118d6
Anajulia Brito <juliehindius@gmail.com>
2021-04-12 12:03:41 -0300

    step 7, creating some functions

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 window.onload = () => {
   document.getElementById('black').classList.add('selected');
 };
+
+function getSelectedColorElement() {
+  const selectedElements = document.getElementsByClassName('selected');
+  return selectedElements[0];
+}
+
+function getActualColor() {
+  const selectedColor = getSelectedColorElement();
+  return selectedColor.id;
+}
+
+function clearSelectedColor() {
+  const selectedColor = getSelectedColorElement();
+  selectedColor.classList.remove('selected');
+}
+
+function selectColor(color) {
+  clearSelectedColor();
+  const colorToSelect = document.getElementById(color);
+  colorToSelect.classList.add('selected');
+}
