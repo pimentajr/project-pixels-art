b2e33ccf40a736f10ba3b96b670db394b9909f47
Halysson2 <halyssonsena83@gmail.com>
2021-04-12 14:46:56 -0300

    adicionando alguns requisitos

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-    window.onload = function(){
+ 
+    window.onload = function carregar() {
         document.getElementById('black').classList.add('selected');
-
-    }
-
+      };
+      function selecionaClasse(event) {
+        const atribuiClasse = document.querySelectorAll('.selected');
+        for (let index = 0; index < atribuiClasse.length; index += 1) {
+          atribuiClasse[index].classList.remove('selected');
+        }
+        event.target.classList.add('selected');
+      }
+      const paleta = document.querySelector('#color-palette');
+      paleta.addEventListener('click', selecionaClasse);
 
 
     let pixelBoard = document.querySelector('#pixel-board')
