00500a65d6eb896da9ce363600dd9a602644db30
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-04-12 09:46:23 -0300

    Modifica requisito 06

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function setColorToPalette() {
   for (let index = 0; index < divColors.length; index += 1) {
     const idColorDivs = divColors[index].id;
     if (divColors[index].getElementsByClassName.backgroundColor !== 'black') {
-      divColors[0].getElementsByClassName.backgroundColor = 'black';  
+      divColors[0].getElementsByClassName.backgroundColor = 'black';
     }
-    divColors[index].getElementsByClassName.backgroundColor = idColorDivs;        
+    divColors[index].getElementsByClassName.backgroundColor = idColorDivs;
   }
 }
 setColorToPalette();
 
-window.onload = setBlack
+window.onload = setBlack;
 
 function setBlack() {
   const elementBlack = document.getElementById('black');
-  elementBlack.addEventListener("onload", setBlack);
+  elementBlack.className.add('selected');
 }
 
 function colorSelection() {
