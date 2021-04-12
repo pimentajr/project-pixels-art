b9f9e830d3172d61ab624b47f0396265019a4924
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-04-12 18:34:11 -0300

    Adiciona requisito 08

--- script.js
+++ script.js
@@ -19,14 +19,27 @@ function setBlack() {
 }
 
 function getColor(event) {
-  if(event.target.classList[0] === 'color'){
-    let colorSelected = document.querySelectorAll('.selected');
-  for (let index = 0; index < colorSelected.length; index += 1) {
-    colorSelected[index].classList.remove('selected');    
-   }
-  event.target.classList.add('selected');
+  if (event.target.classList[0] === 'color') {
+    const colorSelected = document.querySelectorAll('.selected');
+    for (let index = 0; index < colorSelected.length; index += 1) {
+      colorSelected[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
   }
 }
 
 const colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', getColor);
+
+function setColorToBoard() {
+  let pixelBoard = document.getElementById('pixel-board')
+  pixelBoard.addEventListener('click', function (e) {
+    const colorSelected = document.querySelector('.selected');
+    if(e.target.className === 'pixel') {
+      const cor = window.getComputedStyle(colorSelected).getPropertyValue('background-color');
+      const clickedPixel = e.target;
+      clickedPixel.style.background = cor;
+    }
+  })  
+}
+setColorToBoard();
