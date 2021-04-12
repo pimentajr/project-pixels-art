49a90b260aca5eff23d478ffbb418bf60cf4cdb8
Wilquer82 <wilquer.fig@gmail.com>
2021-04-12 11:40:24 -0300

    Requisito 7 01

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-let corPincel = String;
-window.onload = function(){
-  corPincel = document.getElementsByClassName('color').select("background-color=black");
+window.onload = function() {
+  corPincel = document.getElementsByClassName('color').select(getElementById("black").style);
 }
+document.onclick = oQueFazer(); 
+
+function oQueFazer(){
+  if 
+}
+
+addCor();
+mudaCor();
+function altCor(e); {
+  switch addEventListener("click") {
+    case document.getElementById("red").addEventListener("click")
+    corPincel= getComputedStyle("red");
+    break;
+    case document.getElementById("orange").addEventListener("click")
+    corPincel= getComputedStyle("orange");
+    break;
+    case document.getElementById("yellow").addEventListener("click")
+    corPincel= getComputedStyle("yellow");
+    break;
+    case document.getElementById("black").addEventListener("click")
+    corPincel= getComputedStyle("black");
+    break;
+  }
+}
+
+function mudaCor(e);{
+  document.getElementById("pixel").addEventListener("click", mudaCor);
