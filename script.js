72e6552e91742c8751675dd79fcb2227a11ceeea
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-04-12 18:39:13 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('red').style.backgroundColor = 'red';
+document.getElementById('green').style.backgroundColor = 'green';
+document.getElementById('blue').style.backgroundColor = 'blue';
+document.getElementById('yellow').style.backgroundColor = 'yellow';
