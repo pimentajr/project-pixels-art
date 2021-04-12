66cad5870320bc2e7588952515db7c06c766471e
Taís Carvalho <taispcarvalho@gmail.com>
2021-04-12 13:52:28 -0300

    fundo branco

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ function createColumnLines() {
     const pixelLine = document.createElement('div');
     pixelLine.className = 'pixelLine';
     pixelBoard.appendChild(pixelLine);
-    for (let i = 0; i < 5; i += 1) {
-      const pixelColumn = document.createElement('div');
-      pixelColumn.className = 'pixel';
-      pixelLine.appendChild(pixelColumn);
+    for (let index = 0; index < 5; index += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixelLine.appendChild(pixel);
+      pixel.style.backgroundColor = 'white';
     }
   }
 }
