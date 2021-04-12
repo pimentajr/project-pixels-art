67d2efa064073c032e74caec29f7df8b1a4410b3
Rafael Schmitz <schimitaum@gmail.com>
2021-04-12 17:20:05 -0300

    nova tentativa

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function selectThis(e) {
-	document.getElementsByClassName('selected')[0].classList.remove('selected');
-	e.target.classList.add('selected');
-	let selectedColor = e.target.classList[1];
-	setPixelColor(selectedColor);
-}
-
-let palette = document.getElementById('color-palette');
-palette.addEventListener('click', selectThis);
-
 function setPixelColor(color) {
-  let pixelBoard = document.getElementById('pixel-board');
-  pixelBoard.addEventListener('click', function(event){
-	  event.target.style.backgroundColor = color;
+  const pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.addEventListener('click', function(event) {
+    event.target.style.backgroundColor = color;
   });
 }
+
+function selectThis(e) {
+  if (e.target.classList[0] === "color"){
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+    e.target.classList.add('selected');
+    const selectedColor = e.target.classList[1];
+    setPixelColor(selectedColor);
+  }
+}
+
+const palette = document.getElementById('color-palette');
+palette.addEventListener('click', selectThis);
