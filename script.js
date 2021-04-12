6041bbf2fdf3568191ead15aa7e9a3461b88aa1f
Rafael Schmitz <schimitaum@gmail.com>
2021-04-12 17:40:11 -0300

    yeah

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const pixelBoard = document.getElementById('pixel-board');
+const palette = document.getElementById('color-palette');
+
 function setPixelColor(color) {
-  const pixelBoard = document.getElementById('pixel-board');
-  pixelBoard.addEventListener('click', function(event) {
-    event.target.style.backgroundColor = color;
+  pixelBoard.addEventListener('click', function (e) {
+    e.target.style.backgroundColor = color;
   });
 }
 
 function selectThis(e) {
-  if (e.target.classList[0] === "color"){
+  if (e.target.classList[0] === 'color') {
     document.getElementsByClassName('selected')[0].classList.remove('selected');
     e.target.classList.add('selected');
     const selectedColor = e.target.classList[1];
@@ -14,5 +16,4 @@ function selectThis(e) {
   }
 }
 
-const palette = document.getElementById('color-palette');
 palette.addEventListener('click', selectThis);
