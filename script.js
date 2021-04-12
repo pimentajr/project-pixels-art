12e2b5706cc7981974c5916e620dd030242df43c
Matheus Costa <mathws.costag@gmail.com>
2021-04-12 19:36:14 -0300

    Adiciona bloco de pixels

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+window.onload = function () {
+  randomColors();
+  createColons(5);
+  createLines(5);
+};
+
+//
 const randomC1 = document.getElementById('randomColor1');
 const randomC2 = document.getElementById('randomColor2');
 const randomC3 = document.getElementById('randomColor3');
+const buttonColors = document.getElementById('paletaButton');
 
 function randomRGB() {
   const random = parseInt((Math.random() * 255), 10);
@@ -20,7 +28,28 @@ function randomColors() {
     randomC3.style.backgroundColor = rB3;
   }
 }
+buttonColors.addEventListener('click', randomColors);
+//
+const pBlock = document.querySelector('#pixel-board');
 
-window.onload = function () {
-  randomColors();
-};
+function createColons(colonsNumber) {
+  for (let index = 0; index < colonsNumber; index += 1) {
+  const colon = document.createElement('div');
+  colon.className = 'colon';
+  pBlock.appendChild(colon);
+  }
+}
+const pColons = document.getElementsByClassName('colon');
+function createLines(linesNumber) {
+  for (let index = 0; index < pColons.length; index += 1) {
+    const element = pColons[index];
+    for (let index = 0; index < linesNumber; index += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      element.appendChild(pixel);
+    }
+    let pixel = document.createElement('div');
+
+  }
+}
+
