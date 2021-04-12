b4c3dab59cc2b77f978f3241589fb4705a7843d8
Taís Carvalho <taispcarvalho@gmail.com>
2021-04-12 13:31:25 -0300

    5 ja tava pronto - eslint arrumada

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const pixelBoard = document.getElementById('pixel-board');
 
 function createColumnLines() {
   for (let index = 0; index < 5; index += 1) {
-    let pixelLine = document.createElement('div');
+    const pixelLine = document.createElement('div');
     pixelLine.className = 'pixelLine';
     pixelBoard.appendChild(pixelLine);
     for (let i = 0; i < 5; i += 1) {
-      let pixelColumn = document.createElement('div');
+      const pixelColumn = document.createElement('div');
       pixelColumn.className = 'pixel';
       pixelLine.appendChild(pixelColumn);
     }
   }
 }
-  
 createColumnLines();
