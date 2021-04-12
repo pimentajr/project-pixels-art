8bfec8a74ccd03c0c4910e56b84a79f68725c031
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-04-12 17:10:53 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-window.onload = function() {
-	//Seleciona a cor preta como inicial:
-	
-	function createBoard(boardTable) {
-		let board = boardTable;
-		for (let boardRow = 0; boardRow < 5; boardRow += 1){
-			let row = document.createElement('tr');
-			board.appendChild(row);
-			for (let boardColumn = 0; boardColumn < 5; boardColumn += 1){
-				let cell = document.createElement('td');
-				let content = document.createTextNode('');
-				cell.className = ('pixel');
-				row.appendChild(cell);
-			}
-		}
-	}
-	document.getElementById('black').classList.add('selected');
-
-	createBoard(document.getElementById('pixel-board'));
-
-	
+function createBoard(boardTable) {
+  // let board;
+  let row;
+  let cell;
+  for (let boardRow = 0; boardRow < 5; boardRow += 1) {
+    row = document.createElement('tr');
+    boardTable.appendChild(row);
+    for (let boardColumn = 0; boardColumn < 5; boardColumn += 1) {
+      cell = document.createElement('td');
+      // content = document.createTextNode('');
+      cell.className = ('pixel');
+      row.appendChild(cell);
+    }
+  }
 }
+
+function selectColor(newColor) {
+  const previousColor = document.querySelector('.selected');
+  previousColor.classList.remove('selected');
+  console.log(newColor.type);
+  this.classList.add('selected');
+}
+
+window.onload = function () {
+  // Seleciona a cor preta como inicial:
+  document.getElementById('black').classList.add('selected');
+  // Chama a função que cria o quadro:
+  createBoard(document.getElementById('pixel-board'));
+  // Chamar evento de seleção de cor:
+  const colors = document.getElementsByClassName('color');
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', selectColor);
+  }
+};
