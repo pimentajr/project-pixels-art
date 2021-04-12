689ab2ca2c3e4811ef5fcb417842759e0b42f1d6
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-04-12 15:40:10 -0300

    add bonus 11 - create functions check size board

--- script.js
+++ script.js
@@ -15,31 +15,42 @@ function creatPallete() {
 }
 creatPallete();
 
-function pixelBoard() {
+function createPixelBoard() {
   const pixelBoard = document.querySelector('#pixel-board');
-  const inputLines = document.querySelector('#board-size').value;
+  let size = document.querySelector('#board-size').value;
 
   while (pixelBoard.firstChild) {
     pixelBoard.removeChild(pixelBoard.firstChild);
   }
-  
-  for (let i = 0; i < inputLines; i += 1) {
+
+  if (size === '' || size <= 0) {
+    window.alert('Board inválido!');
+  } 
+  if (size < 5) {
+    size = 5;
+  }
+  if (size > 50) {
+    size = 50;
+  }
+
+  for (let i = 0; i < size; i += 1) {
     const columns = document.createElement('div');
-    pixelBoard.appendChild(columns);
-    for (let j = 0; j < inputLines; j += 1) {
+     pixelBoard.appendChild(columns);
+    for (let j = 0; j < size; j += 1) {
       const lines = document.createElement('div');
       lines.classList.add('pixel');
       columns.appendChild(lines);
     }
   }
 }
-pixelBoard();
 
 const generateBoard = document.querySelector('#generate-board');
 generateBoard.addEventListener('click', function(event) {
   event.preventDefault();
-  pixelBoard();
+  createPixelBoard();
 });
+
+createPixelBoard(5);
 
 function addClassSelect() {
   const selectColor = document.querySelector('#color-palette');
@@ -74,25 +85,3 @@ function clearPixel() {
   });
 }
 clearPixel();
-
-// function createGrid() {
-//   const boardSize = document.querySelector('#board-size').value;
-//   const pixelCanvas = document.querySelector('#pixel-board');
-//   while (pixelCanvas.firstChild) {
-//     pixelCanvas.removeChild(pixelCanvas.firstChild);
-//   }
-//   for (let i = 0; i < boardSize; i += 1) {
-//     let createLines = document.createElement('div');
-//     pixelCanvas.appendChild(createLines);
-//     for (let j = 0; j < boardSize; j += 1) {
-//       let createColumns = document.createElement('div');
-//       createLines.appendChild(createColumns);
-//     }
-//   }
-// }
-// createGrid()
-// const sizePicker = document.querySelector('#size-picker');
-// sizePicker.addEventListener('submit', function(event) {
-//   event.preventDefault(); //Cancela o evento se for cancelável, sem parar a propagação do mesmo.
-//   createGrid();
-// });
