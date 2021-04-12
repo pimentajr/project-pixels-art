34c36db9a292489c42a8218111fb30fb1c191fb4
Anajulia Brito <juliehindius@gmail.com>
2021-04-12 18:02:50 -0300

    fixed files after the corruption of the .git folder

---
+++ script.js
@@ -0,0 +1,50 @@
+window.onload = () => {
+  document.getElementById('black').classList.add('selected');
+};
+
+function getSelectedColorElement() {
+  const selectedElements = document.getElementsByClassName('selected');
+  return selectedElements[0];
+}
+
+function getActualColor() {
+  const selectedColor = getSelectedColorElement();
+  return selectedColor.id;
+}
+
+function clearSelectedColor() {
+  const selectedColor = getSelectedColorElement();
+  selectedColor.classList.remove('selected');
+}
+
+function defineNewSelectedColor(targetID) {
+  clearSelectedColor();
+  const htmlElementToUpdate = document.getElementById(targetID);
+  htmlElementToUpdate.classList.add('selected');
+}
+
+// N sei resolver os erros do ESLint!
+function paintElement(elementToBePainted, colorToPaint) {
+  elementToBePainted.className = 'pixel';
+  elementToBePainted.classList.add(colorToPaint);
+}
+
+const pixels = document.getElementsByClassName('pixel');
+
+const allColors = document.getElementsByClassName('color');
+
+for (let index = 0; index < allColors.length; index += 1) {
+  const colorPickerElement = allColors[index];
+  colorPickerElement.addEventListener('click', () => {
+    const newColor = colorPickerElement.id;
+    defineNewSelectedColor(newColor);
+  });
+}
+
+for (let index = 0; index < pixels.length; index += 1) {
+  const pixel = pixels[index];
+  pixel.addEventListener('click', () => {
+    const newColor = getActualColor();
+    paintElement(pixel, newColor);
+  });
+}
