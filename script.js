5b4db38b50fde7f8210b87832464fde2f2f0b95d
Antonio Malato <malatoneto@gmail.com>
2021-04-12 16:25:07 -0300

    Correçao lint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const colorsArray = ['black', 'red', 'blue', 'yellow'];
 function createColors(colors) {
   const sectionBoxColors = document.querySelector('#color-palette');
   for (let index = 0; index < colors.length; index += 1) {
-    let createBoxColors = document.createElement('div');
+    const createBoxColors = document.createElement('div');
     sectionBoxColors.appendChild(createBoxColors);
     createBoxColors.className = 'color';
     createBoxColors.style.backgroundColor = colorsArray[index];
@@ -14,12 +14,12 @@ createColors(colorsArray);
 const table = document.querySelector('#pixel-board');
 function createTable(numberRows, unityNumbers) {
   for (let index = 0; index < numberRows; index += 1) {
-    let createLine = document.createElement('tr');
+    const createLine = document.createElement('tr');
     createLine.className = 'row';
     table.appendChild(createLine);
 
     for (let index = 0; index < unityNumbers; index += 1) {
-      let createUnityPixel = document.createElement('th');
+      const createUnityPixel = document.createElement('th');
       createUnityPixel.className = 'pixel';
       createUnityPixel.style.backgroundColor = 'white';
       createLine.appendChild(createUnityPixel);
@@ -37,15 +37,15 @@ function addOrRemoveClass() {
   const addOrRemove = document.querySelectorAll('.color');
 
   for (let index = 0; index < addOrRemove.length; index += 1) {
-    let item = addOrRemove[index];
+    const item = addOrRemove[index];
 
-    item.addEventListener('click', function(event) {
-      let selected = document.querySelector('.selected');
+    item.addEventListener('click', function (event) {
+      const selected = document.querySelector('.selected');
       if (item.className === 'color') {
         event.target.classList.add('selected');
         selected.classList.remove('selected');
       }
     });
-  };
+  }
 };
 addOrRemoveClass();
