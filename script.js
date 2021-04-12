a3e3620d8792e6731091516c9794de1c040a9d1e
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-04-12 12:04:44 -0300

    adicionando etapas 8 e 9

--- script.js
+++ script.js
@@ -16,5 +16,25 @@ function selectedColor(param) {
   param.target.className = 'color selected';
 }
 
+// https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
+// etapa 8 - Pintar os quadradinhos
+const allBoard = document.querySelector('#pixel-board');
+allBoard.addEventListener('click', colorPixel);
 
+function colorPixel (event) {
+ const selected = document.querySelector('.selected');
+ const selectedColor = window.getComputedStyle(selected).getPropertyValue('background-color');
+ event.target.style.backgroundColor = selectedColor;
+}
 
+//etapa 9 - criar botão
+
+const clearButton = document.getElementById('clear-board');
+
+function clearPixels () {
+const pixels = document.querySelectorAll('.pixel');
+for(let index = 0; index <= pixels.length; index += 1) {
+  pixels[index].style.backgroundColor = 'white';
+}
+}
+clearButton.addEventListener('click', clearPixels);
