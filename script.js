1cdd52fc873d17df723e1d97657e9b8a76f9ab52
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-04-12 18:28:58 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ function selectInitialColor() {
   document.getElementById('Black').className = 'color selected';
 }
 selectInitialColor();
+
+function setSelectColor(tagetColor) {
+  const palette = document.getElementsByClassName('color');
+
+  for (let index = 0; index < palette.length; index += 1) {
+    const object = palette[index];
+    if (object === tagetColor) {
+      object.classList.add('selected');
+    } else {
+      object.classList.remove('selected');
+    }
+  }
+}
+
+function colorSelector(targetColor) {
+  const selectedColor = getComputedStyle(targetColor).backgroundColor;
+  return selectedColor;
+}
+
+function setColor(event) {
+  const element = event.target;
+  setSelectColor(targetColor);
+  colorSelector(targetColor);
+}
+
+function starterPixelArt() {
+  document.getElementById('color-palette')
+  document.addEventListener('click', setColor);
+}
