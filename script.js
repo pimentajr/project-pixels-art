e6f50c594c706a54ae54742c28000c66b04554bc
Jessica Arruda <sicarruda@gmail.com>
2021-04-12 19:19:13 -0300

    exercicos 10 e 11

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+const MAX_BOARD_SIZE = 50;
+const MIN_BOARD_SIZE = 5;
+
 const sectionPixelBoard = document.getElementById('pixel-board');
-
-let colorBlackSelected = true;
-let colorRedSelected = false;
-let colorBlueSelected = false;
-let colorGreenSelected = false;
-
 const colorBlackButton = document.getElementsByClassName('black')[0];
 const colorRedButton = document.getElementsByClassName('red')[0];
 const colorBlueButton = document.getElementsByClassName('blue')[0];
 const colorGreenButton = document.getElementsByClassName('green')[0];
 const buttonClear = document.querySelector('#clear-board');
+
+let colorBlackSelected = true;
+let colorRedSelected = false;
+let colorBlueSelected = false;
+let colorGreenSelected = false;
 
 function colorSelectedBlack() {
   if (!colorBlackButton.classList.contains('selected')) {
@@ -36,6 +38,7 @@ function colorSelectedRed() {
     colorRedButton.classList.add('selected');
   }
 }
+
 function colorSelectedBlue() {
   if (!colorBlueButton.classList.contains('selected')) {
     colorBlueSelected = true;
@@ -48,6 +51,7 @@ function colorSelectedBlue() {
     colorRedButton.classList.remove('selected');
   }
 }
+
 function colorSelectedGreen() {
   if (!colorGreenButton.classList.contains('selected')) {
     colorGreenSelected = true;
@@ -75,7 +79,6 @@ function backgroundColorDefine(boolean1, boolean2, boolean3, boolean4) {
     return 'green';
   }
 }
-onclick = 'clear()';
 
 function paintPixelWithSelectedColor(event) {
   const pixelEvent = event;
@@ -87,11 +90,25 @@ function paintPixelWithSelectedColor(event) {
   );
 }
 
-function pixelBoard(linha, coluna) {
-  for (let index = 0; index < linha; index += 1) {
+function removePixelBoard() {
+  const sectionPixelBoardColumn = document.querySelectorAll('.column');
+  if (
+    sectionPixelBoardColumn[0] === 0 || sectionPixelBoardColumn[0] === undefined
+  ) {
+    return;
+  }
+  for (let index = 0; index < sectionPixelBoardColumn.length; index += 1) {
+    sectionPixelBoard.removeChild(sectionPixelBoardColumn[index]);
+  }
+}
+
+function pixelBoard(boardSize) {
+  removePixelBoard();
+  for (let index = 0; index < boardSize; index += 1) {
     const sectionPixelBoardChild = document.createElement('section');
     sectionPixelBoard.appendChild(sectionPixelBoardChild);
-    for (let indexJ = 0; indexJ < coluna; indexJ += 1) {
+    sectionPixelBoardChild.className = 'column';
+    for (let indexJ = 0; indexJ < boardSize; indexJ += 1) {
       const divPixelBoard = document.createElement('div');
       sectionPixelBoardChild.appendChild(divPixelBoard);
       divPixelBoard.className = 'pixel';
@@ -100,7 +117,28 @@ function pixelBoard(linha, coluna) {
   }
 }
 
-pixelBoard(5, 5);
+function createPixelBoard(boardSize) {
+  const errorMessage = 'Board inválido!';
+  let newPixel = boardSize;
+  if (boardSize === undefined) {
+    alert(errorMessage);
+    return;
+  }
+  if (boardSize < MIN_BOARD_SIZE) {
+    alert(errorMessage);
+    newPixel = MIN_BOARD_SIZE;
+  } else if (boardSize > MAX_BOARD_SIZE) {
+    alert(errorMessage);
+    newPixel = MAX_BOARD_SIZE;
+  }
+
+  pixelBoard(newPixel);
+}
+function startBoard() {
+  const boardSize = document.querySelector('#board-size').value;
+  createPixelBoard(boardSize);
+}
+window.onload = startBoard;
 
 function clear() {
   colorBlackSelected = true;
