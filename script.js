f43003c28bf11053f0e921be0814109115891266
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-04-12 19:41:35 -0300

    Voltando ao 7 Requisito

--- script.js
+++ script.js
@@ -3,30 +3,32 @@ function selectInitialColor() {
 }
 selectInitialColor();
 
-function setSelectedColor(event) {
+function setSelectColor(targetColor) {
   const palette = document.getElementsByClassName('color');
 
   for (let index = 0; index < palette.length; index += 1) {
     const object = palette[index];
-    if (object === event.target) {
+    if (object === targetColor) {
       object.classList.add('selected');
     } else {
       object.classList.remove('selected');
     }
   }
 }
+function colorSelector(targetColor) {
+  const selectedColor = getComputedStyle(targetColor).backgroundColor;
+  return selectedColor;
+}
 
-function pencil(event) {
-  const colorElement = document.querySelector('.selected');
-  const color = getComputedStyle(colorElement).backgroundColor;
-  const pixel = event.target;
-  pixel.style.backgroundColor = color;
+function setColor(event) {
+  const targetColor = event.target;
+  setSelectColor(targetColor);
+  colorSelector(targetColor);
 }
 
 function starterPixelArt() {
   document.getElementById('color-palette');
-  document.addEventListener('click', setSelectedColor);
-  document.getElementById('pixel-board').addEventListener('click', pencil);
+  document.addEventListener('click', setColor);
 }
 
 window.onload = starterPixelArt;
