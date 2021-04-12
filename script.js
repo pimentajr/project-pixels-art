79c916d80e7e31c1c7ede8bfe0f8f07c79e9b382
tulio sploradori <tuliospl@gmail.com>
2021-04-12 12:51:18 -0300

    teste item 7

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 
 function criadorDePixels() {
-let quadro = document.getElementById('pixel-board');
- for (let coluna = 0; coluna < 5; coluna += 1) {
- const createDadPixel = document.createElement('div');
- quadro.appendChild(createDadPixel);
- for (let linha = 0; linha < 5; linha += 1) {
- const createPixel = document.createElement('div');
- createPixel.className = 'pixel';
- createDadPixel.appendChild(createPixel);
- }
-} 
+  let quadro = document.getElementById('pixel-board');
+  for (let coluna = 0; coluna < 5; coluna += 1) {
+    const createDadPixel = document.createElement('div');
+    quadro.appendChild(createDadPixel);
+    for (let linha = 0; linha < 5; linha += 1) {
+      const createPixel = document.createElement('div');
+      createPixel.className = 'pixel';
+      createDadPixel.appendChild(createPixel);
+    }
+  }
 }
 criadorDePixels()
+
+let selectPalette = document.querySelector('#color-palette');
+
+function selectColor() {
+  const selectedColor = document.querySelector('.selected');
+  selectPalette.addEventListener('click', function(e) {
+  if (e.target.id !== 'color-palette') {
+    selectedColor.classList.remove('selected');
+    e.target.classList.add('selected');
+  }
+})
+}
+
+selectColor()
+
