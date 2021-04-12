c1e85cdff3995b5908c0b7f9533197cc8d4cb1c6
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-04-12 09:34:55 -0300

    Corrige requisito 06 e testa requisito 07

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function setBlack() {
   const elementBlack = document.getElementById('black');
   elementBlack.addEventListener("onload", setBlack);
 }
+
+function colorSelection() {
+const tableColor = document.querySelector('#color-palette');
+  tableColor.addEventListener('click', (event) => {
+    const elementTableColor = document.querySelector('#black' || '#red' || '#green' || '#blue');
+    elementTableColor.className = 'color';
+    elementTableColor.className = 'color selected';
+
+    switch (true) {
+      case elementTableColor === '#black':
+        document.querySelector('#black')
+        break;
+      case elementTableColor === '#red':
+        document.querySelector('#red')
+        break;
+      case elementTableColor === '#green':
+        document.querySelector('#green')
+        break;
+      case elementTableColor === '#blue':
+        document.querySelector('#blue')
+        break;
+      default:
+        elementTableColor === '#black'
+        document.querySelector('#black')    
+      }
+  });
+}
+colorSelection();
