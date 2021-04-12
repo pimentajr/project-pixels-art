8c98639b74a9039ea78f07ce7181e1f186c28f93
Cris Lustosa <cmlustosa@gmail.com>
2021-04-12 18:43:29 -0300

    Adiciona funções ao JS

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function setColorAll (event) {
   }
   event.target.classList.add('selected');
 }
+
+const colorPalettes = document.querySelector('#colorPalettte');
+colorPalettes.addEventListener('click', setColorAll);
+
+const pixelBoard = document.querySelector('#pixel-board');
+pixelBoard.addEventListener('click',pxBoard);
+
+function pxBoard(event) {
+  let colorPixel = document.querySelector('.selected');
+  const color = window.getComputedStyle(colorPixel).backgroundColor;
+  event.target.style.backgroundColor = color;
+}
+
+
+
+function clearBoard () {
+  const clPixel = document.querySelector('.pixel');
+  for (let index = 0; index < clPixel.length; index =+1) {
+    clPixel[index].style.backgroundColor = 'white';
+  }
+}
+const button = document.getElementById('clear-board');
+let pixels = document.querySelectorAll('.pixel');
+button.addEventListener('click', clearBoard);
