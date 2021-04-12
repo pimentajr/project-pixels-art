af2fcfa74de57aea85bcd9e411ab18578c05c6c9
nandovbr <nandovbr15@hotmail.com>
2021-04-12 01:44:04 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const colorsPalette = ['black', 'red', 'yellow', 'orange'];
+let selectedColor = colorsPalette[0];
+
 function color() {
   const colors = document.getElementsByClassName('color');
   colors[0].style.background = 'black';
@@ -7,3 +10,13 @@ function color() {
 }
 color();
 
+function changeSelectedColor(event) {
+  let ball = event.target;
+  selectedColor = ball.id;
+  document.querySelector('.selected').classList.remove('selected');
+  ball.classList.add('selected');
+}
+
+document.querySelectorAll('.color').forEach(function (item) {
+  item.addEventListener('click', changeSelectedColor);
+});
