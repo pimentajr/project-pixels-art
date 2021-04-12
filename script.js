50bbb1afbf9308999267850886335f04f3acc910
Taís Carvalho <taispcarvalho@gmail.com>
2021-04-12 14:18:51 -0300

    ate o 5 - bordas, fundo branco e utilizaçao certa do table

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function createColumnLines() {
     const pixelLine = document.createElement('div');
     pixelLine.className = 'pixelLine';
     pixelBoard.appendChild(pixelLine);
-    for (let index = 0; index < 5; index += 1) {
+    for (let i = 0; i < 5; i += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
       pixelLine.appendChild(pixel);
