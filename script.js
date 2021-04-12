f319fdd231fa31aa4e2523e752ddd983e4dc3352
Jessica Arruda <sicarruda@gmail.com>
2021-04-12 10:47:44 -0300

    exercicos 6 e 7

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const sectionPixelBoard = document.getElementById('pixel-board');
 
+let colorBlackSelected = true;
+let colorRedSelected = false;
+let colorBlueSelected = false;
+let colorGreenSelected = false;
+
+const colorBlackButton = document.getElementsByClassName('black')[0];
+const colorRedButton = document.getElementsByClassName('red')[0];
+const colorBlueButton = document.getElementsByClassName('blue')[0];
+const colorGreenButton = document.getElementsByClassName('green')[0];
+
 function pixelBoard(linha, coluna) {
   for (let index = 0; index < linha; index += 1) {
     const sectionPixelBoardChild = document.createElement('section');
@@ -9,6 +19,44 @@ function pixelBoard(linha, coluna) {
       sectionPixelBoardChild.appendChild(divPixelBoard);
       divPixelBoard.className = 'pixel';
     }
+  }
+}
+
+function colorSelectedBlack() {
+  if (!colorBlackButton.classList.contains('selected')) {
+    colorBlackSelected = true;
+    colorBlackButton.classList.add('selected');
+    colorBlueButton.classList.remove('selected');
+    colorGreenButton.classList.remove('selected');
+    colorRedButton.classList.remove('selected');
+  }
+}
+
+function colorSelectedRed() {
+  if (!colorRedButton.classList.contains('selected')) {
+    colorRedSelected = true;
+    colorBlackButton.classList.remove('selected');
+    colorBlueButton.classList.remove('selected');
+    colorGreenButton.classList.remove('selected');
+    colorRedButton.classList.add('selected');
+  }
+}
+function colorSelectedBlue() {
+  if (!colorBlueButton.classList.contains('selected')) {
+    colorBlueSelected = true;
+    colorBlackButton.classList.remove('selected');
+    colorBlueButton.classList.add('selected');
+    colorGreenButton.classList.remove('selected');
+    colorRedButton.classList.remove('selected');
+  }
+}
+function colorSelectedGreen() {
+  if (!colorGreenButton.classList.contains('selected')) {
+    colorGreenSelected = true;
+    colorBlackButton.classList.remove('selected');
+    colorBlueButton.classList.remove('selected');
+    colorGreenButton.classList.add('selected');
+    colorRedButton.classList.remove('selected');
   }
 }
 
