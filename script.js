02dd67a37c5be12fcb746a674fd00a7724667d2d
renan <renan_menezes07@hotmail.com>
2021-04-12 10:33:53 -0300

    Lint: Trocar let por const.

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function getBlack() {
-  let black = document.getElementById('black');
-  let blackColor = window.getComputedStyle(black).backgroundColor;
+  const black = document.getElementById('black');
+  const blackColor = window.getComputedStyle(black).backgroundColor;
   black.classList.add('selected');
   sessionStorage.setItem('color', blackColor);
 }
 getBlack();
 
-let pixelBoard = document.getElementById('pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 
 // Cria um quadro de pixels.
 for (let index = 0; index < 25; index += 1) {
-  let pixel = document.createElement('div');
+  const pixel = document.createElement('div');
   pixelBoard.appendChild(pixel);
   pixel.className = 'pixel';
 }
 
 // Remove a classe 'seleção' de uma cor.
 function SelectRemotion() {
-  let palette = document.getElementsByClassName('color');
+  const palette = document.getElementsByClassName('color');
   for (let colorIndex = 0; colorIndex < palette.length; colorIndex += 1) {
     palette[colorIndex].classList.remove('selected');
   }
 }
-function selectColor (event) {
+function selectColor(event) {
   SelectRemotion();
-  let selectedColor = event.target;
+  const selectedColor = event.target;
   selectedColor.classList.add('selected');
   sessionStorage.setItem('color', window.getComputedStyle(selectedColor).backgroundColor);
 }
@@ -36,13 +36,13 @@ colorPalette.addEventListener('click', selectColor);
 
 // Pinta um pixel ao ser clicado.
 pixelBoard.addEventListener('click', function (event) {
-  let pixelColored = event.target;
+  const pixelColored = event.target;
   pixelColored.style.background = sessionStorage.getItem('color');
 });
 
 // Pinta de branco todos os pixels.
 function clearPixels() {
-  let pixels = document.getElementsByClassName('pixel');
+  const pixels = document.getElementsByClassName('pixel');
   for (let index2 = 0; index2 < pixels.length; index2 += 1) {
     pixels[index2].style.backgroundColor = 'white';
   }
@@ -50,7 +50,7 @@ function clearPixels() {
 
 // Clique no botão
 function clearButton() {
-  let button = document.getElementById('clear-board');
+  const button = document.getElementById('clear-board');
   button.addEventListener('click', clearPixels);
 }
-clearButton()
+clearButton();
