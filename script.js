8bc3d7b3b4d6777b667b032a1cf2f2fa9abdfd0d
Halysson2 <halyssonsena83@gmail.com>
2021-04-12 13:29:11 -0300

    adicionando alguns requisitos

--- script.js
+++ script.js
@@ -1,53 +1,46 @@
-window.onload = function carregar(){
-    document.getElementById('black').classList.add('selected');
+    window.onload = function(){
+        document.getElementById('black').classList.add('selected');
 
-}
+    }
 
 
 
-let pixelBoard = document.querySelector('#pixel-board')
-pixelBoard.addEventListener('click', function (event){
-    let colorSelected = document.querySelector('.selected')
-    let sectedColor = window.getComputedStyle(colorSelected, null).getPropertyValue("background-color");
-    let paintPinxel = event.target
-    paintPinxel.style.backgroundColor = sectedColor 
-    }) 
+    let pixelBoard = document.querySelector('#pixel-board')
+    pixelBoard.addEventListener('click', function (event){
+        let colorSelected = document.querySelector('.selected')
+        let sectedColor = window.getComputedStyle(colorSelected, null).getPropertyValue("background-color");
+        let paintPinxel = event.target
+        paintPinxel.style.backgroundColor = sectedColor 
+        }) 
 
-    function createButton() {
-        let getButton = document.querySelector('#clear-board')
-        getButton.innerHTML = "Reset"
-        getButton.addEventListener('click', function(){
-            let getPixel = document.querySelectorAll('#pixel-board>div')
-            let genBoardButton = document.querySelector('#generate-board')
-            genBoardButton.addEventListener('click', function (){
-            let boardTable = document.querySelector('#board-size').value
-            if(boardTable > 0){
-            while (pixelBoard.firstChild) {
-            pixelBoard.removeChild(pixelBoard.lastChild);
+        function createDivs() {
+            let getBody = document.querySelector('body')
+            let session = document.createElement('section')
+            let sessionButton = document.createElement('button')
+            sessionButton.id = "clear-board"
+            getBody.appendChild(sessionButton)
+            session.id = "pixel-board"
+            getBody.appendChild(session)
             }
-            
-            for(let index = 0; index < boardTable; index += 1){
-            let tr = document.createElement('div')
-            tr.classList.add('tr')
-            pixelBoard.appendChild(tr)
-            for(let index = 0; index < boardTable; index += 1){
-            let newPixel = document.createElement('div')
-            newPixel.classList.add('pixel')
-            tr.appendChild(newPixel)  
-            }      
-            }
-            }
-            else{
-                alert('Board inválido!')
-            }
-            })  
-              for(let index =0 ; index < getPixel.length; index += 1){ 
-              getPixel[index].style.backgroundColor= 'white'
-              }
-            })
-        }
-        createButton()
+          createDivs()
 
-        
+        function createButton() {
+            let getButton = document.querySelector('#clear-board')
+            getButton.innerHTML = "Limpar"
+            getButton.addEventListener('click', function(){
+                let getPixel = document.querySelectorAll('#pixel-board>div')
+                let getPixelBoard = document.querySelector('#pixel-board')
+                for(let index =0 ; index < getPixel.length; index += 1){ 
+                getPixel[index].style.backgroundColor= 'white'
+                getPixelBoard.style.backgroundColor= 'white'
+                }
+              })
+            }
+               createButton()
+               
+               function createPixel() {
+                let getSession = document.querySelector('#pixel-board')
+                let pixelSize = 5;
+               }                     
+       
 
-        
