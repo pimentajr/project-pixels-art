fb35f26a29e310af10b56ed19d0eb71dadf8d53e
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-04-13 15:42:15 -0300

    Desafio 9.2

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
 function selectInitialColor() {
-  document.getElementById('Black').className = 'color selected';
+  document.getElementById('color-1').className = 'color selected';
 }
-selectInitialColor();
-
-function setSelectColor(targetColor) {
+function setSelectedColor(event) {
   const palette = document.getElementsByClassName('color');
-
   for (let index = 0; index < palette.length; index += 1) {
     const object = palette[index];
-    if (object === targetColor) {
+    if (object === event.target) {
       object.classList.add('selected');
     } else {
       object.classList.remove('selected');
     }
   }
 }
-
-function colorSelector(targetColor) {
-  const selectedColor = getComputedStyle(targetColor).backgroundColor;
-  return selectedColor;
+function setColor(event) {
+  const colorElement = document.querySelector('.selected');
+  const color = getComputedStyle(colorElement).backgroundColor;
+  const pixel = event.target;
+  pixel.style.backgroundColor = color;
 }
 
 function clearBoard() {
   const boardItems = document.getElementsByClassName('pixel');
   for (let index = 0; index < boardItems.length; index += 1) {
     const element = boardItems[index];
-    element.getElementById('clear-board').style.backgroundColor = 'white';
+    element.style.backgroundColor = 'white';
   }
 }
 
-function setColor(event) {
-  const targetColor = event.target;
-  setSelectColor(targetColor);
-  colorSelector(targetColor);
-}
-
-function starterPixelArt() {
-  document.getElementById('color-palette');
-  document.addEventListener('click', setColor);
+function pageStarter() {
+  selectInitialColor();
+  document
+    .getElementById('color-palette')
+    .addEventListener('click', setSelectedColor);
+  document.getElementById('pixel-board').addEventListener('click', setColor);
   document.getElementById('clear-board').addEventListener('click', clearBoard);
 }
 
-window.onload = starterPixelArt;
+window.onload = pageStarter;
