d3ce8420ce68fb5ca8ceaaec59ade4d5952cd3d4
Wilquer82 <wilquer.fig@gmail.com>
2021-04-13 18:19:23 -0300

    13-04 18:19

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
-function pintando(event) {
+function coloring(event) {
   const currentColor = document.querySelector('.selected');
-  console.log(currentColor.style.backgroundColor);
   event.target.style.backgroundColor = currentColor.style.backgroundColor;
-
-  
-  // if (apontador.style !== corEscolhida.style.backgroundColor) {
-  //   apotador.style.backgroundColor = corEscolhida.style.backgroundColor;
-  // } else {
-  //   apontador.style.backgroundColor = 'white';
-  // }
-
- // ultimaCorUsada = apontador.style.backgroundColor;
 }
-
 function palette(event) {
   const currentColor = document.querySelector('.selected');
   currentColor.classList.remove('selected');
   event.target.classList.add('selected');
 }
-
-function inicio() {
-  //const corAtual = document.querySelector('.color.select');
-  
-  const colorPalette = document.getElementById('color-palette');
-
-  const pixelBoard = document.getElementById('pixel-board');
-
-  colorPalette.addEventListener('click', palette);
-
-  pixelBoard.addEventListener('click', pintando);
-
+function clearBoard(event){
+  const listPixels=document.getElementsByClassName('pixel');
+  console.log(listPixels);
+   for (let index=0; index <= listPixels.length; index += 1){
+     listPixels[index].style.backgroundColor='white';
+   }
 }
-
+function inicio() {
+  const colorPalette = document.getElementById('color-palette');
+  const pixelBoard = document.getElementById('pixel-board');
+  const button = document.querySelector('#clear-board');
+  colorPalette.addEventListener('click', palette);
+  pixelBoard.addEventListener('click', coloring);
+  button.addEventListener('click', clearBoard);
+}
 window.onload = inicio;
-
