ef33432ed756a9a532eb4c2e3c54f977907e3078
Reginara Gomes <refagomes@gmail.com>
2021-04-13 19:10:24 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colorPalette = document.getElementById('color-palette');
+const pixelBoard = document.getElementById('pixel-board')
 
 function selectColor(event) {
   const selectedDiv = document.querySelector('.selected');
@@ -6,4 +7,16 @@ function selectColor(event) {
   event.target.classList.add('selected');
 }
 
-colorPalette.addEventListener('click', selectColor);
+function paintPixel(event) {
+  const selectedColor = document.querySelector('.selected');
+  const colorComputedStyle = window.getComputedStyle(selectedColor);
+  //selectedColorComputedStyle - para pegar informações do CSS
+  event.target.style.backgroundColor = selectedColorComputedStyle.getPropertyValue('background-color');
+}
+
+function selections() {
+  colorPalette.addEventListener('click',selectColor);
+  pixelBoard.addEventListener('click', paintPixel);
+}
+
+selections()
