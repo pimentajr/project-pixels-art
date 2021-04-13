0b822adfe17130170977abb21fadadf601af765c
Luís Seixas <luiseixas2@gmail.com>
2021-04-12 21:59:51 -0300

    quesitos 9, 10, 11 e 12

--- script.js
+++ script.js
@@ -15,9 +15,24 @@ function resetSelected() {
   }
 }
 
+function generateColors () {
+  const colors = ['black'];
+  const palette = ['yellow','green', 'blue', 'magenta', 'grey', 'pink', 'purple', 'brown', 'cyan'];
+  do {
+    const colorIndex = getRandomIntInclusive(0, palette.length - 1);
+    colors.push(palette[colorIndex]);
+  } while (colors.length < 4)
+  return colors;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 // quesito 2
 function createSquare(colorIndex) {
-  let colors = ['black', 'red', 'yellow', 'green'];
+  let colors = generateColors();
   let palette = document.createElement('div');
   palette.style.backgroundColor = colors[colorIndex];
   palette.className = 'color';
@@ -57,16 +72,64 @@ function createRowBoard(size) {
   return row;
 }
 // quesito 4
-function createBoard() {
+function createBoard(boardSize) {
   let mainBoard = document.getElementById('pixel-board');
-  for (let index = 0; index < 5; index += 1) {
-    mainBoard.appendChild(createRowBoard(5));
+  for (let index = 0; index < boardSize; index += 1) {
+    mainBoard.appendChild(createRowBoard(boardSize));
   }
+}
+function resetSquareBoardColors () {
+  let squareBoards = document.getElementsByClassName('pixel');
+  for (let i = 0; i < squareBoards.length; i += 1) {
+    squareBoards[i].style.backgroundColor = 'white';
+  }
+}
+
+function buildButtonEvents () {
+  let buttonClearBoard = document.getElementById('clear-board');
+  buttonClearBoard.addEventListener('click', resetSquareBoardColors);
+  
+  let buttonCreateBoard = document.getElementById('generate-board');
+  buttonCreateBoard.addEventListener('click', createNewBoard);
+}
+
+function createNewBoard () {
+  let inputBoardSize = document.getElementById('board-size');
+  let boardSize = inputBoardSize.value;
+  if (validateBoardValue(boardSize)) {
+    removeBoard();
+    boardSize = generateLimit(boardSize);
+    createBoard(boardSize);
+    return; 
+  }
+  alert('Board inválido!');
+}
+
+function removeBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.innerHTML = '';
+}
+
+function validateBoardValue(value) {
+  if (value.trim().length === 0) {
+    return false;
+  }
+  return true;
+}
+
+function generateLimit(value) {
+  if (value < 5) {
+    return 5;
+  }
+  if (value > 50) {
+    return 50;
+  }
+  return value;
 }
 
 function buildAll() {
   createPalettes(4);
-  createBoard();
+  createBoard(5);
+  buildButtonEvents();
 }
-
 buildAll();
