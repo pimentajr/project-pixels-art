875911ea32f2cf7dfca134c45d4e95ac949c4475
Michel Pereira <michelbecauro@gmail.com>
2021-04-13 01:21:55 -0300

    Recomendações do lint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = start;
-
-function start (){
-    let linhas = document.getElementsByClassName('pixel');
-    for (let i = 0; i <= linhas.length; i += 1)
-    linhas[i].style.backgroundColor = "white" ;
+function start() {
+  const linhas = document.getElementsByClassName('pixel');
+  for (let i = 0; i <= linhas.length; i += 1) {
+  linhas[i].style.backgroundColor = 'white';
+	}
 }
+window.onload = start;
