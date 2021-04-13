232577b6e3e65d28ad8481b9c4a6f9aee315adb6
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-04-13 19:55:23 -0300

    Projeto Pixels Art Mod3

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 // CORES DA PALETA
 
-const black = document.getElementsByClassName('color')[0];
+const black = document.querySelectorAll('.color')[0];
 black.style.backgroundColor = 'black';
 
-const green = document.getElementsByClassName('color')[1];
+const green = document.querySelectorAll('.color')[1];
 green.style.backgroundColor = 'green';
 
-const blue = document.getElementsByClassName('color')[2];
+const blue = document.querySelectorAll('.color')[2];
 blue.style.backgroundColor = 'blue';
 
-const yellow = document.getElementsByClassName('color')[3];
+const yellow = document.querySelectorAll('.color')[3];
 yellow.style.backgroundColor = 'yellow';
 
 // CRIA TABELA DE PIXELS
 
-const pixelBoard = document.querySelector('#pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 
 for (let indexRow = 0; indexRow < 5; indexRow += 1) {
-  let pixelLine = document.createElement('div');
+  const pixelLine = document.createElement('div');
   pixelLine.className = 'pixelLine';
   pixelBoard.appendChild(pixelLine);
+  
   for (let indexColl = 0; indexColl < 5; indexColl += 1) {
-    let pixelCell = document.createElement('div');
+    const pixelCell = document.createElement('div');
     pixelLine.appendChild(pixelCell);
     pixelCell.style.backgroundColor = 'white';
     }
@@ -29,14 +30,14 @@ for (let indexRow = 0; indexRow < 5; indexRow += 1) {
 
 // ADICIONAR CLASSE
 
-const paletteColors = document.getElementById('color-palette');
+const paletteColors = document.querySelector('#color-palette');
 
 function selectColor() {
-    paletteColors.addEventListener('click', (event) => {
+  paletteColors.addEventListener('click', (event) => {
     const color = document.getElementsByClassName('selected');
     if (event.target.id !== 'color-palette') {
-        color.classList.remove('selected');
-        event.target.classList.add('selected');
+      color.classList.remove('selected');
+      event.target.classList.add('selected');
     }
   });
 }
@@ -48,7 +49,7 @@ pixelBoard.addEventListener('click', (event) => {
   if (event.target.className === 'pixel') {
     const pixelSelected = event.target;
     pixelSelected.style.backgroundColor = selectedColor;
-    }
+  }
 });
 
 // BOTAO DE LIMPAR
@@ -62,4 +63,4 @@ cleartableButton.addEventListener('click', () => {
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].backgroundColor = 'white';
   }
-})
+});
