78f08ad1413becb54b6376bff5d12bb890a2fb86
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-04-13 16:31:24 -0300

    requisito 7

---
+++ script.js
@@ -0,0 +1,16 @@
+function selectedColor() {
+  const colorSelected = document.getElementById('color-palette');
+  colorSelected.addEventListener('click', (event) => {
+    const myEvent = event.target;
+
+    const divColor = document.querySelectorAll('.color');
+    for(let index = 0; index < divColor.length; index += 1) {
+      divColor[index].classList.remove('selected');
+      if(myEvent.className === 'color') {
+        myEvent.classList.add('selected');
+      }
+    }
+
+  })
+}
+selectedColor();
