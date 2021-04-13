31cc0f975fdaf0911be5b09d442b1ccf7c06e256
Uriel Bochi <urielbochi@gmail.com>
2021-04-13 16:10:01 -0300

    teste 6 e 7

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-let colorPalette = document.getElementById('color-palette')
-let colors = document.getElementsByClassName('color')
-let selectedColor = document.querySelector('.selected')
-let pixelBoard = document.getElementById('pixel-board')
-let pixels = document.getElementsByClassName('pixel')
-
-
-//Requisito 4
-function createLinesAndRows () {
-  let board = document.getElementById('pixel-board')
-    for (let i =0; i < 5; i+=1){
-        pixelRow = document.createElement('div')
-        pixelRow.className = 'pixel-row'
-        board.appendChild(pixelRow)
-
-        for (let j=0; j < 5; j+= 1){
-            pixelCell = document.createElement('div')
-            pixelCell.className = 'pixel'
-            pixelRow.appendChild(pixelCell)
-            pixelCell.style.backgroundColor = 'white'
-
-        }
-    }
-}
-
 
 //Requisito 2, 3 e 12
 function randomColor() {
@@ -53,33 +28,44 @@ function randomColor() {
       }
     }
 
-    //Requisito 6 e 7
-    function setSelectColor(selectTheColor) {
-        const colorPalette = document.getElementsByClassName('color');
-      
-        for (let index = 0; index < palette.length; index += 1) {
-          const object = palette[index];
-          if (object === selectTheColor) {
-            object.classList.add('selected');
-          } else {
-            object.classList.remove('selected');
-          }
+//Requisito 4
+function createLinesAndRows () {
+  let board = document.getElementById('pixel-board')
+    for (let i =0; i < 5; i+=1){
+        pixelRow = document.createElement('div')
+        pixelRow.className = 'pixel-row'
+        board.appendChild(pixelRow)
+
+        for (let j=0; j < 5; j+= 1){
+            pixelCell = document.createElement('div')
+            pixelCell.className = 'pixel'
+            pixelRow.appendChild(pixelCell)
+            pixelCell.style.backgroundColor = 'white'
+
         }
-      }
-      function colorSelector(selectTheColor) {
-        const selectedColor = getComputedStyle(selectTheColor).backgroundColor;
-        return selectedColor;
+    }
+}
+
+    //Requisito 6 e 7
+    function selectedColor() {
+        paletaCores.addEventListener('click', (event) => {
+          const cor = document.querySelector('.selected');
+          if (event.target.id !== 'color-palette') {
+            cor.classList.remove('selected');
+            event.target.classList.add('selected');
+          }
+        });
       }
 
+
+   
     
-    
+         
+
 
 window.onload = () => {
 createLinesAndRows()
 randomColor()
 chooseColor()
-setSelectColor()
-colorSelector()
-
-
+selectedColor();
 }
