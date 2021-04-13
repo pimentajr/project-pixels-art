8e66fbdfe28e534733e08bc32db3c5939b3ce32d
Michel Pereira <michelbecauro@gmail.com>
2021-04-13 01:28:16 -0300

    Recomendações do lint e troca de lugar da ref ao arquivo js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function start() {
   const linhas = document.getElementsByClassName('pixel');
   for (let i = 0; i <= linhas.length; i += 1) {
-  linhas[i].style.backgroundColor = 'white';
+  	linhas[i].style.backgroundColor = 'white';
 	}
 }
 window.onload = start;
