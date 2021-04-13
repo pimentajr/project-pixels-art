7bbd49e6f9fef3958ee60ed66504131151dfa036
Felipe Carvalho <fecarvalho066@gmail.com>
2021-04-12 21:56:18 -0300

    [Felipe Carvalho] - Requisito 5

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ for (let index = 0; index < 5; index += 1) {
   const linha = document.createElement('tr');
   linha.className = 'pixel';
   pixelBoard.appendChild(linha);
-  for (let felipex = 0; felipex < 5; felipex += 1) {
+  for (let felipex = 0; felipex < 4; felipex += 1) {
     const coluna = document.createElement('td');
     coluna.className = 'pixel';
     pixelBoard.appendChild(coluna);
