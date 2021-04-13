7d64d64892231165c4c561978c95d9a1b708f815
Michel Pereira <michelbecauro@gmail.com>
2021-04-13 17:56:25 -0300

    Requisito 5. Testing

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let linhas = [];
 linhas = document.getElementsByClassName('pixel');
 function start() {
-	
   for (let i = 0; i < linhas.length; i += 1) {
 		linhas[i].style.backgroundColor = 'white';
 	}
