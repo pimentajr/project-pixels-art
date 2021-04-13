4ea719e1e5aaf58ed96456c6f239b85c6a3cab42
Edson <edson.exe@outlook.com>
2021-04-12 21:50:01 -0300

    Resolvendo pendências Lint

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.onload = function () {
-  const pixelBoard = document.querySelector('#pixel-board').children;
-  const pixelToColor = document.querySelector('#pixel-board');
-  const colorPalette = document.querySelector('#color-palette');
-  const clearBoard = document.getElementById('clear-board');
-  let black = document.querySelector('#black');
-  black.classList.add('selected');
-  var color = 'black';
-  const quant = 5;
+let color = 'black';
+const pixelBoard = document.querySelector('#pixel-board').children;
+const pixelToColor = document.querySelector('#pixel-board');
+const colorPalette = document.querySelector('#color-palette');
+const clearBoard = document.getElementById('clear-board');
+const black = document.querySelector('#black');
+black.classList.add('selected');
+const quant = 5;
 
+function createPixelBoard() {
   for (let index = 0; index < quant; index += 1) {
     for (let index2 = 0; index2 < quant; index2 += 1) {
       const pixel = document.createElement('div');
@@ -15,22 +15,26 @@ window.onload = function () {
       pixelBoard[index].appendChild(pixel);
     }
   }
-
-  clearBoard.addEventListener('click', function(){
-    window.location.reload();
-    }
-  )
-
-  colorPalette.addEventListener('click', function(c) {
-    document.querySelector('.selected').classList.remove('selected');
-    c.target.classList.add('selected');
-    color = document.querySelector('.selected').id;
-  })
-
-  pixelToColor.addEventListener('click', function(d) {
-    let pixel = d.target;
-    pixel.classList.add(color);
-  })  
-
 }
 
+function clearBoardColors() {
+  window.location.reload();
+}
+
+function getColor(c) {
+  document.querySelector('.selected').classList.remove('selected');
+  c.target.classList.add('selected');
+  color = document.querySelector('.selected').id;
+}
+
+function setColor(d) {
+  const pixel = d.target;
+  pixel.classList.add(color);
+}
+
+window.onload = () => {
+  createPixelBoard();
+  clearBoard.addEventListener('click', clearBoardColors);
+  colorPalette.addEventListener('click', getColor);
+  pixelToColor.addEventListener('click', setColor);
+};
