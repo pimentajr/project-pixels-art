ae72e67cd494cc9b6c55a8ba63290867f0107b7d
Wilquer82 <wilquer.fig@gmail.com>
2021-04-13 12:26:19 -0300

    Requisito 6 e 7 12:26

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function altCor(e) {
 }
 
 function inicio() {
-  const corAtual = document.querySelector('.color.selected');
+  const corAtual = document.selected.querySelector(".color");
   
   addEventListener("click", altCor);
   addEventListener("click", trocaCor);
