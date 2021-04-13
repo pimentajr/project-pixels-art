d42edee27ad738265fe5bb34cf9332af280c076c
IgorMendez <igor.emp@hotmail.com>
2021-04-12 21:11:09 -0300

    Resolução exercicio 3 e 4

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Variables
-const colorPalette = document.getElementById('color-palette');
+// const colorPalette = document.getElementById('color-palette');
 const colors = document.getElementsByClassName('color');
-console.log(colorPalette);
+const pixelBoard = document.getElementById('pixel-board');
+// const pixelGet = document.getElementsByClassName('pixel');
 // Assignments
 
 // Functions
@@ -9,3 +10,16 @@ console.log(colorPalette);
 for (let index = 0; index < colors.length; index += 1) {
   colors[index].style.backgroundColor = colors[index].id;
 }
+
+(function createPixel() {
+  for (let index = 0; index < 5; index += 1) {
+    const createDivPixelRow = document.createElement('tr');
+    pixelBoard.appendChild(createDivPixelRow);
+    createDivPixelRow.classList.add('pixel-row');
+    for (let j = 0; j < 5; j += 1) {
+      const createDivPixelColum = document.createElement('td');
+      createDivPixelRow.appendChild(createDivPixelColum);
+      createDivPixelColum.classList.add('pixel', 'white');
+    }
+  }
+}());
