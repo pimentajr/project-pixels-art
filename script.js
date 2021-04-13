3c820e3869f02a93895ddc2a40d3797c44b61765
Michel Pereira <michelbecauro@gmail.com>
2021-04-13 15:33:43 -0300

    Problema anterior. Era só um sinal igual(=) no FOR da função start()

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let linhas = [];
+linhas = document.getElementsByClassName('pixel');
 function start() {
-  const linhas = document.getElementsByClassName('pixel');
-  for (let i = 0; i <= linhas.length; i += 1) {
-	linhas[i].style.backgroundColor = 'white';
+	
+  for (let i = 0; i < linhas.length; i += 1) {
+		linhas[i].style.backgroundColor = 'white';
 	}
 }
 window.onload = start;
