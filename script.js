8b633000bc7f03adc993fc9b561474f4faff963e
Kevin Willian <kevinwillian084@gmail.com>
2021-04-13 18:56:57 -0300

    Criando a paleta de cores de forma dinamica com JS

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+let colorPalette = document.querySelector('#color-palette');
 
 let cores = ['black', 'red', 'blue', 'green', 'pink', 'yellow', 'brown'];
 
 for(let index = 0; index < cores.length; index += 1){
-  let cor = cores[index];
-  document.createElement('div') 
+  let nameCor = cores[index];
+  let colorsDiv = document.createElement('div');
+  colorPalette.appendChild(colorsDiv);
+  colorsDiv.className = 'color';
+  colorsDiv.id = nameCor;
+  colorsDiv.innerHTML = nameCor;
+
+  if(colorsDiv.innerText.includes('black')){
+    colorsDiv.className += ' selected'
+  }
+
 }
-
-
 
 for(let index = 0; index < cores.length; index += 1){
   let cor = document.getElementById(cores[index]);
   cor.style.backgroundColor = cores[index];
-  cor.style.width ='60px';
-  cor.style.height = '60px';
-  cor.style.display = 'inline-block';
 }
 
