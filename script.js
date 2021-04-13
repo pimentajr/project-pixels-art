cc01c1b78078f36630619ebbfd6a64519d14ec9b
Cris Lustosa <cmlustosa@gmail.com>
2021-04-13 10:35:01 -0300

    Atualiza HTML com a classe selected para a cor preta

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ window.onload = function () {
   document.getElementById('black').classList.add('selected');
 }
 
+//Requisito 4
+let pixelBoardSize = 5;
+
+for (let lineIndex = 0; lineIndex <= pixelBoardSize; lineIndex += 1 ) {
+  const createColumnDivs = document.createElement('div');
+  createColumnDivs.className = 'column-pixel';
+  for (let columnIndex = 0; columnIndex <= pixelBoardSize; columnIndex +=1) {
+    const createDivs = document.createElement('div');
+    createDivs.className = 'pixel';
+    createColumnDivs.appendChild(createDivs);
+  }
+  pixelBoard.appendChild(createColumnDivs);
+}
+
+
+
 function setColorAll (event) {
   let aux = document.querySelectorAll('.selected');
   for (let index = 0; index < aux.length; index +=1) {
@@ -10,6 +26,9 @@ function setColorAll (event) {
   }
   event.target.classList.add('selected');
 }
+
+
+
 
 const colorPalettes = document.querySelector('#color-palette');
 colorPalettes.addEventListener('click', setColorAll);
