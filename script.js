ad044140596fe8ef1d774f3403450362647cbaae
Cesar Lopez <cglopez874@hotmail.com>
2021-04-13 23:24:32 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function createTable() {
-    let element = document.createElement('div');
-    element.className = 'pixel';
-    return element;
+  const element = document.createElement('div');
+  element.className = 'pixel';
+  return element;
 }
 
-let pixelboard1 = document.getElementById('first-row');
-let pixelboard2 = document.getElementById('second-row');
-let pixelboard3 = document.getElementById('third-row');
-let pixelboard4 = document.getElementById('fourth-row');
-let pixelboard5 = document.getElementById('fifht-row');
+const pixelboard1 = document.getElementById('first-row');
+const pixelboard2 = document.getElementById('second-row');
+const pixelboard3 = document.getElementById('third-row');
+const pixelboard4 = document.getElementById('fourth-row');
+const pixelboard5 = document.getElementById('fifht-row');
 
 pixelboard1.appendChild(createTable());
 pixelboard1.appendChild(createTable());
@@ -35,4 +35,3 @@ pixelboard5.appendChild(createTable());
 pixelboard5.appendChild(createTable());
 pixelboard5.appendChild(createTable());
 pixelboard5.appendChild(createTable());
-
