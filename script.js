3207f2de80000ea27e66468a7a88191dc786165c
Felipe Carvalho <fecarvalho066@gmail.com>
2021-04-14 14:25:16 -0300

    [Felipe Carvalho] - Requisito 5

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-function paletaDeCores(classes) {
-  const cores = document.createElement('li');
-  cores.className = classes;
-  return cores;
+const title = document.querySelector('#title');
+title.innerHTML = 'Paleta de Cores';
+
+function makeColorPalete(classes) {
+  const colors = document.createElement('li');
+  colors.className = classes;
+  return colors;
 }
 const colorPalette = document.querySelector('#color-palette');
-colorPalette.appendChild(paletaDeCores('color selected preto'));
-colorPalette.appendChild(paletaDeCores('color coral'));
-colorPalette.appendChild(paletaDeCores('color violeta'));
-colorPalette.appendChild(paletaDeCores('color aqua'));
+colorPalette.appendChild(makeColorPalete('color selected preto'));
+colorPalette.appendChild(makeColorPalete('color coral'));
+colorPalette.appendChild(makeColorPalete('color violeta'));
+colorPalette.appendChild(makeColorPalete('color aqua'));
 
-const pixelBoard = document.querySelector('#pixel-board');
+const makePixelBoard = document.querySelector('#pixel-board');
 for (let index = 0; index < 5; index += 1) {
   const linha = document.createElement('tr');
-  linha.className = 'pixel';
-  pixelBoard.appendChild(linha);
-  for (let felipex = 0; felipex < 4; felipex += 1) {
+  linha.className = 'pixel branco';
+  makePixelBoard.appendChild(linha);
+  for (let felipex = 0; felipex < 5; felipex += 1) {
     const coluna = document.createElement('td');
-    coluna.className = 'pixel';
-    pixelBoard.appendChild(coluna);
+    coluna.className = 'pixel branco';
+    makePixelBoard.appendChild(coluna);
   }
 }
+
+function paintPixel(event) {
+  const cell = event;
+  const selectedColor = document.querySelector('.selected');
+  if (cell.target.className === 'pixel') {
+    cell.target.style.backgroundColor = selectedColor.style.backgroundColor;
+  }
+}
+makePixelBoard.addEventListener(paintPixel('click', paintPixel));
