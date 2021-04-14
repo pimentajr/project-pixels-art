260068dd697bf150959407f06cc7426a50bdad4a
Cesar Lopez <cglopez874@hotmail.com>
2021-04-13 23:11:26 -0300

    requisito 4 e 5

---
+++ script.js
@@ -0,0 +1,38 @@
+function createTable() {
+    let element = document.createElement('div');
+    element.className = 'pixel';
+    return element;
+}
+
+let pixelboard1 = document.getElementById('first-row');
+let pixelboard2 = document.getElementById('second-row');
+let pixelboard3 = document.getElementById('third-row');
+let pixelboard4 = document.getElementById('fourth-row');
+let pixelboard5 = document.getElementById('fifht-row');
+
+pixelboard1.appendChild(createTable());
+pixelboard1.appendChild(createTable());
+pixelboard1.appendChild(createTable());
+pixelboard1.appendChild(createTable());
+pixelboard1.appendChild(createTable());
+pixelboard2.appendChild(createTable());
+pixelboard2.appendChild(createTable());
+pixelboard2.appendChild(createTable());
+pixelboard2.appendChild(createTable());
+pixelboard2.appendChild(createTable());
+pixelboard3.appendChild(createTable());
+pixelboard3.appendChild(createTable());
+pixelboard3.appendChild(createTable());
+pixelboard3.appendChild(createTable());
+pixelboard3.appendChild(createTable());
+pixelboard4.appendChild(createTable());
+pixelboard4.appendChild(createTable());
+pixelboard4.appendChild(createTable());
+pixelboard4.appendChild(createTable());
+pixelboard4.appendChild(createTable());
+pixelboard5.appendChild(createTable());
+pixelboard5.appendChild(createTable());
+pixelboard5.appendChild(createTable());
+pixelboard5.appendChild(createTable());
+pixelboard5.appendChild(createTable());
+
