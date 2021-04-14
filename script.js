d99cc6b5669914f69605772daebe28187e6da384
Michel Pereira <michelbecauro@gmail.com>
2021-04-13 23:38:42 -0300

    desafio 6

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let linhas = [];
 linhas = document.getElementsByClassName('pixel');
+
 function start() {
   for (let i = 0; i < linhas.length; i += 1) {
 		linhas[i].style.backgroundColor = 'white';
 	}
+
+	let corBlack = document.querySelector('.color');
+	corBlack.style.backgroundColor = 'black';
+	corBlack.classList.add('selected'); // classlist / add() Stackoverflow: https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro
 }
+
 window.onload = start;
+
+
+// tr-palette.addEventListener('click', function(event) {
+	
+// });
