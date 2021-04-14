38914a04b02368abc4acec1009d6fc54a2b0156d
Taís Carvalho <taispcarvalho@gmail.com>
2021-04-14 20:53:49 -0300

    squares feitas no javascript

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+function createColorPalette() {
+  const colorPalette = document.getElementById('color-palette');
+
+  const colorBlack = document.createElement('div');
+  colorBlack.className = 'color';
+  colorBlack.style.backgroundColor = 'black';
+  colorPalette.appendChild(colorBlack); 
+
+  const secondColor = document.createElement('div');
+  secondColor.className = 'color';
+  secondColor.style.backgroundColor = 'rgb(204, 204, 102)';
+  colorPalette.appendChild(secondColor);
+
+  const thirdColor = document.createElement('div');
+  thirdColor.className = 'color';
+  thirdColor.style.backgroundColor = 'rgb(121, 140, 53)';
+  colorPalette.appendChild(thirdColor);
+
+  const forthColor = document.createElement('div');
+  forthColor.className = 'color';
+  forthColor.style.backgroundColor = 'rgb(72, 89, 34)';
+  colorPalette.appendChild(forthColor);
+}
+
+createColorPalette();
+
+
 const pixelBoard = document.getElementById('pixel-board');
 
 function createColumnLines() {
@@ -14,9 +41,3 @@ function createColumnLines() {
   }
 }
 createColumnLines();
-
-
-
-
-
-
