f560fca6db6f64385bd21d94597d53576cf519f7
Michel Pereira <michelbecauro@gmail.com>
2021-04-14 04:00:00 -0300

    Desafio 8: Apenas o primeiro pixel

--- script.js
+++ script.js
@@ -2,17 +2,6 @@ let linhas = [];
 linhas = document.getElementsByClassName('pixel');
 let selected;
 
-function start() {
-  for (let i = 0; i < linhas.length; i += 1) {
-		linhas[i].style.backgroundColor = 'white';
-	}
-	let corBlack = document.querySelector('.color');
-	corBlack.style.backgroundColor = 'black';
-	corBlack.classList.add('selected'); // classlist / add() Stackoverflow: https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro
-	selected = corBlack;
-	selecionar();
-}
-
 function selecionar () {
 	let palette = document.getElementById('tr-palette')
 	palette.addEventListener('click', function(event) {
@@ -22,5 +11,40 @@ function selecionar () {
 		selected.classList.add('selected');
 	});
 }
+
+function pintar () {
+	let quadro = document.getElementsByClassName('tr-board')
+	let pixel;
+	let cor;
+	
+	quadro[0].addEventListener('click', function(event) {
+		// cor = document.querySelector('.selected');
+		cor = selected;
+		pixel = event.target;
+		let corStyle = getComputedStyle(cor);
+		let pixelStyle = getComputedStyle(pixel);
+		console.log('Classe da cor escolhida: '+ cor.classList)
+		
+		console.log('Cor escolhida: ' + corStyle.backgroundColor);
+		// console.log('Cor do pixel: ' + event.target.style.backgroundColor);
+		// console.log('Cor do pixel: ' + pixel.style.backgroundColor);
+			console.log('Cor do pixel: ' + pixelStyle.backgroundColor);
+		pixel.style.backgroundColor = corStyle.backgroundColor;
+	});
+}
+
+function start() {
+  for (let i = 0; i < linhas.length; i += 1) {
+		linhas[i].style.backgroundColor = 'white';
+	}
+	let corBlack = document.querySelector('.color');
+	corBlack.style.backgroundColor = 'black';
+	corBlack.classList.add('selected'); // classlist / add() Stackoverflow: https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro
+	selected = corBlack;
+	selecionar();
+	pintar();
+}
+
+
 
 window.onload = start;
