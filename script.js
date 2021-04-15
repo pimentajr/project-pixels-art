b3e674050ee21161c7eaf5a4b288adaf6bbba743
Thiago Gomes <thiagog.macedom@gmail.com>
2021-04-15 01:43:03 -0300

    requisito 6 finalizado

--- script.js
+++ script.js
@@ -6,16 +6,17 @@ function randomColor() {
 
   return `rgb(${r}, ${g}, ${b})`;
 }
-
+// Cor aleatória para as 3 cores subsequentes
 const pallet = document.querySelectorAll('.genColor');
 for (let index = 0; index < pallet.length; index += 1) {
   const tempColor = randomColor();
   pallet[index].style.backgroundColor = tempColor;
 }
 
+// Cria o quadro com a largura dinamica.
 const board = document.querySelector('#pixel-board');
 const squareSide = 5;
-let squareSideWidht = `${squareSide * 40 + 50}px`;
+const squareSideWidht = `${squareSide * 40 + 50}px`;
 
 board.style.width = squareSideWidht;
 for (let index = 0; index < squareSide ** 2; index += 1) {
@@ -24,4 +25,11 @@ for (let index = 0; index < squareSide ** 2; index += 1) {
   square.className = 'pixel box';
 
   board.appendChild(square);
+}
+
+// Define a cor preta como selecionada se nenhuma estiver selecionada
+const selected = document.querySelector('.selected');
+if (selected === null) {
+  const blackSquare = document.querySelector('#black');
+  blackSquare.className = 'color box black selected';
 }
