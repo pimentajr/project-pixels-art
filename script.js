01e06b0dd805e6811fcbcc0485154e59b5f0a26a
Taís Carvalho <taispcarvalho@gmail.com>
2021-04-15 10:05:17 -0300

    exercicio 6 e 7 - selecionar cor - melhora no codigo

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-function createColorPalette() {
-  const colorPalette = document.getElementById('color-palette');
+// cor preta no 1 square
+const colorBlack = document.getElementsByClassName('color')[0];
+colorBlack.style.backgroundColor = 'black';
 
-  const colorBlack = document.createElement('div');
-  colorBlack.className = 'color';
-  colorBlack.style.backgroundColor = 'black';
-  colorPalette.appendChild(colorBlack); 
+// cor no 2 square
+const secondColor = document.getElementsByClassName('color')[1];
+secondColor.style.backgroundColor = 'rgb(204, 204, 102)';
 
-  const secondColor = document.createElement('div');
-  secondColor.className = 'color';
-  secondColor.style.backgroundColor = 'rgb(204, 204, 102)';
-  colorPalette.appendChild(secondColor);
+// cor no 3 square
+const thirdColor = document.getElementsByClassName('color')[2];
+thirdColor.style.backgroundColor = 'rgb(121, 140, 53)';
+  
+// cor no 4 square
+const forthColor = document.getElementsByClassName('color')[3];
+forthColor.style.backgroundColor = 'rgb(72, 89, 34)';
 
-  const thirdColor = document.createElement('div');
-  thirdColor.className = 'color';
-  thirdColor.style.backgroundColor = 'rgb(121, 140, 53)';
-  colorPalette.appendChild(thirdColor);
-
-  const forthColor = document.createElement('div');
-  forthColor.className = 'color';
-  forthColor.style.backgroundColor = 'rgb(72, 89, 34)';
-  colorPalette.appendChild(forthColor);
-}
-
-createColorPalette();
-
-
+// cria os 25 pixels em formade um quadrado 5x5
 const pixelBoard = document.getElementById('pixel-board');
 
 function createColumnLines() {
@@ -41,3 +31,26 @@ function createColumnLines() {
   }
 }
 createColumnLines();
+
+// primeira cor selected preta e mudar quando outra cor for selecionada
+
+window.onload = blackColorFirst
+
+function blackColorFirst() {
+  colorBlack.className = 'color selected';
+}
+
+const colorPalette = document.querySelector('#color-palette');
+
+function selectedColor() {
+  colorPalette.addEventListener('click', (event) => {
+    const colorSelected = document.querySelector('.selected');
+    if (event.target.id !== 'color selected') {
+      colorSelected.classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  })
+}
+
+selectedColor();
+
