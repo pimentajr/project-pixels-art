4e97b7080db48ca39053bcc5f1ff74379f9a4d4a
Rafael Schmitz <schimitaum@gmail.com>
2021-04-14 21:17:01 -0300

    quase la

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+const buttonClear = document.getElementById('clear-board');
+const buttonBoard = document.getElementById('generate-board');
+const input = document.getElementById('board-size');
+const palette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
 const tableGrid = document.getElementById('grid');
-const palette = document.getElementById('color-palette');
-const buttonClear = document.getElementById('clear-board');
-const inputSize = document.getElementById('board-size');
-const buttonBoard = document.getElementById('generate-board');
-const tableRows = document.querySelectorAll('tr');
 
 function setPixelColor(color) {
   pixelBoard.addEventListener('click', (e) => {
@@ -15,7 +14,7 @@ function setPixelColor(color) {
 setPixelColor('black');
 
 function selectThis(e) {
-  if (e.target.classList[0] === 'color') {
+  if (e.target.classList[0] === 'color') { // certifica que é um elemento da palheta
     document.getElementsByClassName('selected')[0].classList.remove('selected');
     e.target.classList.add('selected');
     const selectedColor = e.target.classList[1];
@@ -26,15 +25,17 @@ function selectThis(e) {
 palette.addEventListener('click', selectThis);
 
 buttonClear.addEventListener('click', () => {
-  for (let i = 0; i < tableRows.length; i += 1) {
-    const pixels = tableRows[i].children;
-    for (let n = 0; n < pixels.length; n += 1) {
-      pixels[n].style.backgroundColor = 'white';
+  const tableRows = document.querySelectorAll('tr');
+  for (let r = 0; r < tableRows.length; r += 1) {
+    const pixels = tableRows[r].children;
+    for (let i = 0; i < pixels.length; i += 1) {
+      pixels[i].style.backgroundColor = 'white';
     }
   }
 });
 
 function removeBoard() {
+  const tableRows = document.querySelectorAll('tr');
   for (let i = 0; i < tableRows.length; i += 1) {
     tableGrid.removeChild(tableRows[i]);
   }
@@ -42,10 +43,11 @@ function removeBoard() {
 
 function createBoard(boardSize) {
   for (let l = 0; l < boardSize; l += 1) {
-    tableGrid.appendChild(tableRows);
+    const boardRows = document.createElement('tr');
+    tableGrid.appendChild(boardRows);
     for (let c = 0; c < boardSize; c += 1) {
       const pixels = document.createElement('td');
-      tableRows.appendChild(pixels);
+      boardRows.appendChild(pixels);
       pixels.classList.add('pixel');
     }
   }
@@ -53,8 +55,13 @@ function createBoard(boardSize) {
 createBoard(5);
 
 buttonBoard.addEventListener('click', () => {
-  const sizeValue = inputSize.value;
-  if (sizeValue > 0) {
+  let sizeValue = input.value;
+  if (sizeValue !== '') {
+    if (sizeValue < 5) {
+      sizeValue = 5;
+    } else if (sizeValue > 50) {
+      sizeValue = 50;
+    }
     removeBoard();
     createBoard(sizeValue);
   } else {
