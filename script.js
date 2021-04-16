501ec1b1848700e90e9f3e615b5265142745751b
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-16 17:58:05 -0300

    Exercicio 8

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+function showColor(){
+    console.log(corSelecionada);
+}
+
 let black = document.getElementById('black');
 let red = document.getElementById('red');
 let blue = document.getElementById('blue');
 let green = document.getElementById('green');
 let cores = document.getElementById('color-palette');
 
+let corSelecionada = 'black';
+
 black.addEventListener('click', function(event){
     black.className = 'color selected';
     red.className = 'color';
     blue.className = 'color';
     green.className = 'color';
+    corSelecionada = 'black';
+    showColor();
 });
 
 red.addEventListener('click', function(event){
@@ -16,6 +24,8 @@ red.addEventListener('click', function(event){
     black.className = 'color';
     blue.className = 'color';
     green.className = 'color';
+    corSelecionada = 'red';
+    showColor();
 });
 
 blue.addEventListener('click', function(event){
@@ -23,6 +33,8 @@ blue.addEventListener('click', function(event){
     black.className = 'color';
     blue.className = 'color selected';
     green.className = 'color';
+    corSelecionada = 'blue';
+    showColor();
 });
 
 green.addEventListener('click', function(event){
@@ -30,4 +42,13 @@ green.addEventListener('click', function(event){
     black.className = 'color';
     blue.className = 'color';
     green.className = 'color selected';
+    corSelecionada = 'green';
+    showColor();
+});
+
+let selectedColor = document.getElementsByClassName('color selected');
+let changeColor = document.querySelector('#pixel-board');
+
+changeColor.addEventListener('click', function(event){
+    event.target.style.backgroundColor = corSelecionada;
 });
