004effa2ca616e21b8f53cc49588c8c300d70ecc
Lais Simenikim <l.simenikim@gmail.com>
2021-04-17 19:58:51 -0300

    add requisito 10 e refatora js

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+let table = document.getElementById('pixel-board');
+let generateBoard = document.getElementById('generate-board');
+let boardSize = document.getElementById('board-size');
+let clearBoard = document.getElementById('clear-board');
 let colors = document.getElementById('color-palette').children;
+
+generateBoard.addEventListener('click', onGenerateButton);
+clearBoard.addEventListener('click', onClearButton);
 
 for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', onColorClick);
+}
+
+renderPixelBoard(5);
+
+function renderPixelBoard(size) {
+  table.innerHTML = '';
+
+  for (let index = 0; index < size; index += 1) {
+    let row = document.createElement('tr');
+    table.appendChild(row);
+
+    for (let index = 0; index < size; index += 1) {
+      let pixel = document.createElement('td');
+      pixel.classList.add('pixel');
+      pixel.addEventListener('click', onPixelClick);
+      row.appendChild(pixel);
+    }
+  }
+}
+
+function onGenerateButton() {
+  if (boardSize.value === '') {
+    alert('Board inválido!');
+  } else {
+    renderPixelBoard(boardSize.value);
+  }
 }
 
 function onColorClick(event) {
@@ -12,12 +45,6 @@ function onColorClick(event) {
   clickedColor.classList.add('selected');
 }
 
-let pixels = document.querySelectorAll('#pixel-board .pixel');
-
-for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener('click', onPixelClick);
-}
-
 function onPixelClick(event) {
   let selectedColor = document.querySelector('.selected');
   let selectedColorValue = selectedColor.style.backgroundColor;
@@ -25,10 +52,9 @@ function onPixelClick(event) {
   event.target.style.backgroundColor = selectedColorValue;
 }
 
-let clearBoard = document.getElementById('clear-board');
-clearBoard.addEventListener('click', onClearButton);
-
 function onClearButton() {
+  let pixels = document.querySelectorAll('#pixel-board .pixel');
+
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].style.backgroundColor = '';
   }
