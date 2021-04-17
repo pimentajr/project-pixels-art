7026e8f52d9e630b5fa24ea5782d62ebb8847bdd
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-17 15:52:06 -0300

    Teste

--- script.js
+++ script.js
@@ -11,23 +11,3 @@ for (let indexLine = 0; indexLine < 5; indexLine += 1) {
     pixelCell.style.backgroundColor = 'white';
   }
 }
-const colorPalette = document.querySelector('#color-palette');
-
-function selectColor() {
-  colorPalette.addEventListener('click', (event) => {
-    const color = document.querySelector('.selected');
-    if (event.target.id !== 'color-palette') {
-      color.classList.remove('selected');
-      event.target.classList.add('selected');
-    }
-  });
-}
-selectColor();
-
-pixBoard.addEventListener('click', (event) => {
-  const selectedColor = document.querySelector('.selected').id;
-  if (event.target.className === 'pixel') {
-    const pixelSelected = event.target;
-    pixelSelected.style.backgroundColor = selectedColor;
-  }
-});
