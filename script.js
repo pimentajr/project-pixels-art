a3f454ed215a3715d352aa1e759911d37e987a2b
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-17 15:43:24 -0300

    cor preta no primeiro quadro

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ const colorPalette = document.querySelector('#color-palette');
 
 function selectColor() {
   colorPalette.addEventListener('click', (event) => {
-    const color = document.querySelector('.selected color');
+    const color = document.querySelector('.selected');
     if (event.target.id !== 'color-palette') {
-      color.classList.remove('selected color');
-      event.target.classList.add('selected color');
+      color.classList.remove('selected');
+      event.target.classList.add('selected');
     }
   });
 }
 selectColor();
 
 pixBoard.addEventListener('click', (event) => {
-  const selectedColor = document.querySelector('.selected color').id;
+  const selectedColor = document.querySelector('.selected').id;
   if (event.target.className === 'pixel') {
     const pixelSelected = event.target;
     pixelSelected.style.backgroundColor = selectedColor;
