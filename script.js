450eb87568eab1982ec0cd43283b003e2e59314f
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-18 00:00:48 -0300

    lint

--- script.js
+++ script.js
@@ -7,22 +7,9 @@ for (let indexLine = 0; indexLine < 5; indexLine += 1) {
   pixelLine.className = 'pixelLine';
   pixelBoard.appendChild(pixelLine);
   for (let indexColumn = 0; indexColumn < 5; indexColumn += 1) {
-  const pixelCell = document.createElement('div');
-  pixelCell.className = 'pixel';
-  pixelLine.appendChild(pixelCell);
-  pixelCell.style.backgroundColor = 'white';
+    const pixelCell = document.createElement('div');
+    pixelCell.className = 'pixel';
+    pixelLine.appendChild(pixelCell);
+    pixelCell.style.backgroundColor = 'white';
   }
 }
-
-// 9
-
-const cleartableButton = document.getElementById('clear-board');
-
-cleartableButton.innerText = 'Limpar';
-
-cleartableButton.addEventListener('click', () => {
-  const pixel = document.querySelectorAll('.pixel');
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].style.backgroundColor = 'white';
-  }
-});
