0788ab67db718a15820b9f175572b3eb829fdc7e
Antonio Malato <malatoneto@gmail.com>
2021-04-18 18:22:30 -0300

    Requisito

--- script.js
+++ script.js
@@ -84,15 +84,15 @@ const sizeButton = document.getElementById('generate-board');
 const input = document.getElementById('board-size');
 
 function changeSizePixel() {
-  if (input.value > 0 && input.value <= 5) {
-    input.value = 5;
-  } else if (input.value >= 50) {
-    input.value = 50;
-  } else if (input.value > 5 && input.value < 50) {
-    input.value;
+  if (input.value > 0 && input.value  <= 5) {
+    input.value  = 5;
+  } else if (input.value  >= 50) {
+    input.value  = 50;
+  } else if (input.value  > 5 && input.value  < 50) {
+    input.value  = input.value ;
   } else {
     alert('Board inválido!');
-    input.value = '';
+    input.value  = '';
   }
   createTable(input.value, input.value);
 }
