31ec3ce2d61606ac542643405775c08ae9855f2a
heyynat <natalifplima@gmail.com>
2021-04-18 17:35:57 -0400

    Alteraçao para elementos dinamicos, comentarios, refatorada de codigo e requisito 9. COMMITAO BOMBA EH POUCO

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-const frame = document.querySelector('#pixel-board');
-function createPixels() {
+// Requisito 2 e 6
+function createPalette() {
+  const colorPalette = document.createElement('div');
+  colorPalette.id = 'color-palette';
+  document.body.appendChild(colorPalette);
+  for (let index = 0; index < 4; index += 1) {
+    const colors = document.createElement('div');
+    colors.classList.add('color');
+    colorPalette.appendChild(colors);
+  }
+  colorPalette.firstElementChild.classList.add('selected');
+}
+createPalette();
+
+// Requisito 4
+function createPixelBoard() {
+  const frame = document.createElement('div');
+  frame.id = 'pixel-board';
+  document.body.appendChild(frame);
+  const frameX = document.querySelector('#pixel-board');
   for (let lines = 0; lines < 5; lines += 1) {
     let box = document.createElement('div');
-    frame.appendChild(box);
+    frameX.appendChild(box);
     for (let index = 0; index < 5; index += 1) {
       box = document.createElement('div');
       box.classList.add('pixel');
-      frame.appendChild(box);
+      frameX.appendChild(box);
       box.style.backgroundColor = 'white';
-      box.style.display = 'inline-block';
-      box.style.height = '40px';
-      box.style.width = '40px';
-      box.style.border = 'solid 1px black';
     }
   }
 }
-window.onload = createPixels;
-
-const elementColorBlack = document.querySelector('#color-palette').firstElementChild;
-elementColorBlack.classList.add('selected');
-
-const color = document.querySelectorAll('.color');
-const colorPalette = document.querySelector('#color-palette');
+createPixelBoard();
 
 // Requisito 7
 function changeSelection(event) {
+  const color = document.querySelectorAll('.color');
   for (let index = 0; index < color.length; index += 1) {
     color[index].classList.remove('selected');
   }
   event.target.classList.add('selected');
 }
+const colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', changeSelection);
 
 // Requisito 8
@@ -42,4 +52,25 @@ function paintPixels(event) {
   const colorCurrent = getComputedStyle(colorClassSelected).backgroundColor; // A função getComputedStyle retorna todos as propriedades do CSS que foram usadas para renderizar um elemento na tela.
   event.target.style.backgroundColor = colorCurrent;
 }
-frame.addEventListener('click', paintPixels);
+const framePwd = document.querySelector('#pixel-board');
+framePwd.addEventListener('click', paintPixels);
+
+// Requisito 9
+function createButtonClearPixels() {
+  const section = document.createElement('section');
+  const colorPalette2 = document.querySelector('#color-palette');
+  colorPalette2.appendChild(section);
+  const btnClearPixels = document.createElement('button');
+  section.appendChild(btnClearPixels);
+  btnClearPixels.id = 'clear-board';
+  btnClearPixels.innerText = 'Limpar';
+}
+createButtonClearPixels();
+
+const btnClear = document.getElementById('clear-board');
+btnClear.addEventListener('click', () => {
+  const box = document.querySelectorAll('.pixel');
+  for (let index = 0; index < box.length; index += 1) {
+    box[index].style.backgroundColor = 'white';
+  }
+});
