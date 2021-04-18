ad99d6ca5df3affc529b35256f9f2f241eed5bd1
Aline Hofner <alinehofner@gmail.com>
2021-04-18 19:04:45 -0300

    Item 6 getDocument

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function selectColorPixel(pixel) { 
-  pixel.style.backgroundColor = 'black';
+const black = document.getElementById('black');
+const pink = document.getElementById('pink');
+const blue = document.getElementById('blue');
+const red = document.getElementById('red');
+
+const paletaDeCores = [black, pink, blue, red];
+
+function selectColorPixel(event) { 
+  for (let index = 0; index < paletaDeCores; index += 1) {
+    if (paletaDeCores[index].classList.contains('selected')) {
+      paletaDeCores[index].classList.remove('selected');
+    }
+  }
+  event.target.classList.add('selected');
+  console.log(event.target.className);
 }
