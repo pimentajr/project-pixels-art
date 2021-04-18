a01016fd9c792fd75d5b3b05ba69ac552e2546e2
anaclarabck <ackyotoku@gmail.com>
2021-04-18 16:34:07 -0300

    11 [ADD] Adicionado funçao para dimensionar pixel board [UPDATE] Ajustado codigo para facilitar leitura

--- script.js
+++ script.js
@@ -1,58 +1,99 @@
+const title = document.getElementById('title');
 const colorsPalette = document.getElementById('color-palette');
-const linePixelBoard = document.querySelectorAll('.line');
-const basePixelBoard = 5;
 const pixelBoard = document.getElementById('pixel-board');
 const pixel = document.getElementsByClassName('pixel');
-const clearBoard = document.getElementById('clear-board');
-const button = document.getElementsByTagName('button')[0];
+const buttonClearBoard = document.getElementById('clear-board');
+const inputBoardSize = document.getElementById('board-size');
+const buttonGenerateBoard = document.getElementById('generate-board');
 
-document.getElementById('title').innerText = 'Paleta de Cores';
-clearBoard.innerText = 'Limpar';
+title.innerText = 'Paleta de Cores';
+buttonClearBoard.innerText = 'Limpar';
+buttonGenerateBoard.innerText = 'VQV';
 
-// Gera cores da paleta aleatoriamentem, exceto a primeira
-function randomColors() {
-  return `RGB(${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},${Math.round(Math.random()*255)})`
+// Gera um número aleatório de 0 a 255
+function randomNumber() {
+  return Math.round(Math.random() * 255);
 }
 
-const listOfColors = ['black', randomColors(), randomColors(), randomColors()];
-console.log(listOfColors)
+// Gera uma cor aleatória
+function generateColor() {
+  return `RGB(${randomNumber()},${randomNumber()},${randomNumber()}`
+}
 
 // Cria paleta de cores
-function createPalette(arrayColors) {
-  for (let index = 0; index < arrayColors.length; index += 1) {
-    const box = document.createElement('div');
-    box.className = 'color';
-    box.style.background = listOfColors[index];
-    colorsPalette.appendChild(box);
+function createPalette() {
+  const listOfColors = ['black', generateColor(), generateColor(), generateColor()];
+  for (let index = 0; index < listOfColors.length; index += 1) {
+    const randomColor = document.createElement('div');
+    randomColor.className = 'color';
+    randomColor.style.background = listOfColors[index];
+    randomColor.style.width = '40px';
+    colorsPalette.appendChild(randomColor);
+  }
+  document.getElementsByClassName('color')[0].classList.add('selected');
+}
+
+// Recupera e corrige valor da dimensão do pixel board
+function dimensionPixelBoard() {
+  const checkValueBoardSize = Number(inputBoardSize.value);
+  let valueBoardSize = 0;
+
+  if ((checkValueBoardSize) <= 5 && (checkValueBoardSize) > 0) {
+    valueBoardSize = 5;
+  } else if ((checkValueBoardSize) >= 50) {
+    valueBoardSize = 50;
+  } else if ((checkValueBoardSize) < 50 && (checkValueBoardSize) > 5) {
+    valueBoardSize = (checkValueBoardSize);
+  } else {
+    alert('Board inválido!');
+    valueBoardSize = 5;
+  }
+  return valueBoardSize;
+}
+
+// Apaga pixel board
+function erasePixelBoard() {
+  const linesPixelBoard = document.getElementsByClassName('line');
+
+  for (let index = linesPixelBoard.length - 1; index >= 0; index -= 1) {
+    linesPixelBoard[index].remove();
   }
 }
 
-// Preenche as linhas com as caixas vazias
-function fillLinePixelBoard(divline) {
-  for (let index = 0; index < basePixelBoard; index += 1) {
-    const box = document.createElement('div');
-    box.className = 'pixel';
-    box.style.width = '40px';
-    box.style.height = '40px';
-    box.style.background = 'white';
-    divline.style.width = '210px';
-    divline.style.height = box.style.height;
-    pixelBoard.style.width = divline.style.width;
-    divline.appendChild(box);
+// Preenche as linhas do pixel board
+function createColumnsPixelBoard(divLine, valueBoardSize) {
+  for (let index = 0; index < valueBoardSize; index += 1) {
+    const pixelSquare = document.createElement('div');
+    pixelSquare.classList.add('pixel');
+    pixelSquare.style.width = '40px';
+    divLine.appendChild(pixelSquare);
   }
 }
 
-// Passa por todas as linhas
-function createPixelBoard(lines) {
-  for (let index = 0; index < lines.length; index += 1) {
-    fillLinePixelBoard(lines[index]);
+// Cria pixel board padrão 5x5
+function createStandarPixelBoard() {
+  const valueBoardSize = 5;
+
+  for (let index = 0; index < valueBoardSize; index += 1) {
+    const lineBoard = document.createElement('div');
+    lineBoard.classList.add('line');
+    pixelBoard.appendChild(lineBoard);
+    createColumnsPixelBoard(lineBoard, valueBoardSize);
   }
 }
 
-createPalette(listOfColors);
-createPixelBoard(linePixelBoard);
+// Cria linhas para o pixel board, tendo como parâmetro a dimensão do mesmo
+function createPixelBoardVQV() {
+  erasePixelBoard();
+  const valueBoardSize = dimensionPixelBoard();
 
-document.getElementsByClassName('color')[0].classList.add('selected');
+  for (let index = 0; index < valueBoardSize; index += 1) {
+    const lineBoard = document.createElement('div');
+    lineBoard.classList.add('line');
+    pixelBoard.appendChild(lineBoard);
+    createColumnsPixelBoard(lineBoard, valueBoardSize);
+  }
+}
 
 function getColor(colorX) {
   document.getElementsByClassName('selected')[0].classList.remove('selected');
@@ -60,16 +101,25 @@ function getColor(colorX) {
 }
 
 function colorPixel(pixelX) {
+  const pixelBeColored = pixelX.target;
   const newColor = document.getElementsByClassName('selected')[0].style.background;
-  pixelX.target.style.background = newColor;
+  if (pixelBeColored.classList.contains('pixel')) {
+    pixelBeColored.style.background = newColor;
+  }
 }
 
-colorsPalette.addEventListener('click', getColor);
-
-pixelBoard.addEventListener('click', colorPixel);
-
-button.addEventListener('click', function () {
+function clearBoard() {
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].style.backgroundColor = 'white';
   }
-})
+}
+
+window.onload = function () {
+  createPalette();
+  createStandarPixelBoard();
+};
+
+colorsPalette.addEventListener('click', getColor);
+pixelBoard.addEventListener('click', colorPixel);
+buttonClearBoard.addEventListener('click', clearBoard);
+buttonGenerateBoard.addEventListener('click', createPixelBoardVQV);
