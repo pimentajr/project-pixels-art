49ea9918d4b22febd645a0ee20a93ec59d7c28a5
nick <pereiranicolas713@gmail.com>
2021-04-18 17:52:43 -0300

    refatorando html e fazendo requisito 4

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const pallete = document.getElementsByClassName('color');
-console.log(pallete);
+const boardSize = document.getElementById('board-size');
+const pixelBoard = document.getElementById('pixel-board');
+const button = document.getElementById('generate-board');
 
-
+// criando pixel board  
+function createPixelBoard(input){
+  for(let r = 0; r < input; r += 1){
+    const row = document.createElement('div');
+    pixelBoard.appendChild(row);
+    row.className = 'row';
+  for(let c = 0; c < input; c +=1){
+    const cell = document.createElement('div');
+    row.appendChild(cell);
+    cell.className = 'pixel';
+  }
+  }
+}
+createPixelBoard(5);
