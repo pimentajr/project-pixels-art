9ff5c8ef2005b0b31f5ff8494a16fbaafc75ed06
Felipe Carvalho <fecarvalho066@gmail.com>
2021-04-19 15:42:19 -0300

    [Felipe Carvalho] - Requisito 7 e 8

---
+++ script.js
@@ -0,0 +1,18 @@
+const selectableColors = document.querySelectorAll('.color');
+function makeSelected(event) {
+  const colorSelected = event;
+  document.querySelectorAll('.selected')[0].className = 'color';
+  colorSelected.target.className = 'color selected';
+}
+for (let index = 0; index < selectableColors.length; index += 1) {
+  selectableColors[index].addEventListener('click', makeSelected);
+}
+
+function Coloring(event) {
+  const pixel = event.target;
+  pixel.style.backgroundColor = document.querySelectorAll('.selected')[0].id;
+}
+const pixels = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', Coloring);
+}
