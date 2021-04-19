e6f84a40068ca3c6321fc8e013c63aed9ab0ebcc
lucasmulato <cobain.lucas23@gmail.com>
2021-04-19 17:33:29 -0300

    update -  0419173312021

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const cores = document.querySelectorAll('.color');
 for (let i = 0; i < cores.length; i += 1){
   cores [i].addEventListener('click', selecColor);
 }
+//função troca cor
+const pixels = document.getElementsByClassName('pixel');
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', () => {
+    const newColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
+    pixels[i].tsyle.backgroundColor = corNova;
+  })
+}
