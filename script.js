ce8337302309104443b7a8eeb542e4f4f7a4b2e3
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-04-19 16:11:15 -0300

    7 fix2

--- script.js
+++ script.js
@@ -2,27 +2,3 @@ document.getElementById('black').style.backgroundColor = 'black';
 document.getElementById('red').style.backgroundColor = 'red';
 document.getElementById('blue').style.backgroundColor = 'blue';
 document.getElementById('yellow').style.backgroundColor = 'yellow';
-
-const colorPalette = document.querySelector('#color-palette');
-const pixelBoard = documennnt.querySelector('#pixel-board');
-
-let savedColor = 'black';
-function selectionColor(color) {
-    const colorTarget = color.target;
-    if (colorTarget.classList.contains('color')) {
-        const removeColor = document.querySelector('.selected'):
-        removeColor.classList.remove('selected');
-
-        savedColor = colorTarget.style.backgroundColor.classList.add('selected');
-    }
-}
-
-function setColor(color) {
-    const newColor = color.target;
-    if (newColor.className === 'pixel') {
-        newColor.style.backgroundColor = savedColor;
-    }
-}
-
-window.onnload = selectingColor;
-colorPalette.addEventListener('click', selectingColor)
