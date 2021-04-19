92c3a24b216f5664388dcfe0f702d60d697dc3f1
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-04-19 14:34:16 -0300

    requisito 6/7

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ document.getElementById('black').style.backgroundColor = 'black';
 document.getElementById('red').style.backgroundColor = 'red';
 document.getElementById('blue').style.backgroundColor = 'blue';
 document.getElementById('yellow').style.backgroundColor = 'yellow';
+
+function colorSelection () {
+
+    let selectedBefore = document.getElementsByClassName('selected');
+    let colorPalette = document.querySelectorAll('.color');
+
+    for (let index = 0; index < colorPalette.length; index += 1) {
+        colorPalette[index].addEventListener('click', function() {
+            colorPalette[index].classList.add = 'selected';
+            selectedBefore.className = 'color';
+        });
+        }
+        let select = document.getElementsByClassName('selected');
+        return select
+    }
