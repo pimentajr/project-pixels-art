9614983813ddedeef47fa106d8d62f1f0bd8b2dd
Lucas Felipe Ribeiro <luc.felipe42@gmail.com>
2021-04-19 11:38:11 -0300

    requisitos 1 ao 3

---
+++ script.js
@@ -0,0 +1,9 @@
+// requisito 2
+const black = document.querySelectorAll('.color')[0];
+black.style.backgroundColor= 'black';
+const red = document.querySelectorAll('.color')[1];
+red.style.backgroundColor= 'red';
+const yellow = document.querySelectorAll('.color')[2];
+yellow.style.backgroundColor= 'yellow';
+const blue = document.querySelectorAll('.color')[3];
+blue.style.backgroundColor= 'blue';
