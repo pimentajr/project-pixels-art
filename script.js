70b28179e52fd857e8aa1602438afa22b82941cf
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-04-19 19:06:56 -0300

    ultimos acertos

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ const redColor = document.getElementById('red');
 const blueColor = document.getElementById('blue');
 const purpleColor = document.getElementById('purple');
 
-blackColor.addEventListener('click', selectedColor);
-redColor.addEventListener('click', selectedColor);
-blueColor.addEventListener('click', selectedColor);
-purpleColor.addEventListener('click', selectedColor);
-
 function selectedColor(param) {
   blackColor.className = 'color';
   redColor.className = 'color';
@@ -15,26 +10,31 @@ function selectedColor(param) {
   purpleColor.className = 'color';
   param.target.className = 'color selected';
 }
+blackColor.addEventListener('click', selectedColor);
+redColor.addEventListener('click', selectedColor);
+blueColor.addEventListener('click', selectedColor);
+purpleColor.addEventListener('click', selectedColor);
 
 // https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 // etapa 8 - Pintar os quadradinhos
+
 const allBoard = document.querySelector('#pixel-board');
-allBoard.addEventListener('click', colorPixel);
 
 function colorPixel (event) {
  const selected = document.querySelector('.selected');
- const selectedColor = window.getComputedStyle(selected).getPropertyValue('background-color');
- event.target.style.backgroundColor = selectedColor;
+  const selectColor = window.getComputedStyle(selected).getPropertyValue('background-color');
+  event.target.style.backgroundColor = selectColor;
 }
+allBoard.addEventListener('click', colorPixel);
 
 // etapa 9 - criar botão
 
 const clearButton = document.getElementById('clear-board');
 
-function clearPixels () {
-const pixels = document.querySelectorAll('.pixel');
-for(let index = 0; index < pixels.length; index += 1) {
-  pixels[index].style.backgroundColor = 'white';
-}
+function clearPixels() {
+  const pixels = document.querySelectorAll('.pixel');
+    for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
 }
 clearButton.addEventListener('click', clearPixels);
