775dd37a627898cd5f562e4afcc0e662ebd7d450
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-04-20 03:11:04 -0300

    fix 56

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-function createColorElements() {
-    const genericAttribute1 = document.getElementById('color-palette');
+function paintPixel () {
+  let genericAttribute1 = document.getElementsByClassName('pixel')
 
-    for (i = 0; i < 4; i += 1) {
-      const genericAttribute2 = document.createElement('li');
-      genericAttribute2.className = 'color';
-      genericAttribute1.appendChild(genericAttribute2);
-    }
-    const color = document.getElementsByClassName('color');
-    const black = color[0];
-    const green = color[1];
-    const red = color[2];
-    const blue = color[3];
-
-
-
-    black.style.backgroundColor = 'black';
-    green.style.backgroundColor = 'green';
-    red.style.backgroundColor = 'red';
-    blue.style.backgroundColor = 'blue';
+  for (let i = 0; i < genericAttribute1.length; i += 1) {
+      genericAttribute1[i].addEventListener('click',function() {
+      let color = document.querySelector('.selected')
+      genericAttribute1[i].style.backgroundColor = window.getComputedStyle(color).getPropertyValue('background-color');
+    })
   }
-  createColorElements();
+}
+paintPixel ();
 
   function createPixelBoard() {
     for (let i = 0; i < 5; i += 1) {
@@ -51,15 +40,26 @@ function createColorElements() {
     })
   }
   selectedClass()
+  
+  function createColorElements() {
+    const genericAttribute1 = document.getElementById('color-palette');
 
-  function paintPixel () {
-    let genericAttribute1 = document.getElementsByClassName('pixel')
-
-    for (let i = 0; i < genericAttribute1.length; i += 1) {
-        genericAttribute1[i].addEventListener('click',function() {
-        let color = document.querySelector('.selected')
-        genericAttribute1[i].style.backgroundColor = window.getComputedStyle(color).getPropertyValue('background-color');
-      })
+    for (i = 0; i < 4; i += 1) {
+      const genericAttribute2 = document.createElement('li');
+      genericAttribute2.className = 'color';
+      genericAttribute1.appendChild(genericAttribute2);
     }
+    const color = document.getElementsByClassName('color');
+    const black = color[0];
+    const green = color[1];
+    const red = color[2];
+    const blue = color[3];
+
+
+
+    black.style.backgroundColor = 'black';
+    green.style.backgroundColor = 'green';
+    red.style.backgroundColor = 'red';
+    blue.style.backgroundColor = 'blue';
   }
-  paintPixel ();
+  createColorElements();
