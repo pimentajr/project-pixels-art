c360bc6c0f5a0932fd66913a91519515c10e1ead
JoaoCabrera <joaocabrerati@gmail.com>
2021-04-20 01:27:14 -0300

    Resolvendo requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let pixelBoard = document.querySelector('.pixel-board');
+let pixelBoard = document.getElementById('pixel-board');
 let linesBoard = document.querySelectorAll('.tr');
 let allPixels = document.querySelectorAll('.pixel');
 let colorPalette = document.getElementById('color-palette');
@@ -14,5 +14,23 @@ colorPalette.addEventListener('click', function(event) {
   colorSelected.classList.remove('selected');
   for (let i = 0; i < paletteItens.length; i += 1) {
     event.target.classList.add('selected')
+  }
+});
+
+pixelBoard.addEventListener('click', function(event) {
+  let colorSelected = document.querySelector('.selected');
+  for (let i = 0; i < allPixels.length; i += 1) {
+    if (colorSelected === document.getElementById('color1')) {
+    event.target.style.backgroundColor = 'black'
+    }
+    if (colorSelected === document.getElementById('color2')) {
+      event.target.style.backgroundColor = 'red'
+    }
+    if (colorSelected === document.getElementById('color3')) {
+      event.target.style.backgroundColor = 'green'
+    }
+    if (colorSelected === document.getElementById('color4')) {
+      event.target.style.backgroundColor = 'blue'
+    }   
   }
 });
