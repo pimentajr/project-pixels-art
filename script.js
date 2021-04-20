cd9719b819ab1a9591f99ec66c099c3daf1c2ef0
lucasmulato <cobain.lucas23@gmail.com>
2021-04-20 13:05:32 -0300

    update - 0420130512021

--- script.js
+++ script.js
@@ -1,47 +1,66 @@
 let tamanhoGrid = 5;
-const cores = document.querySelectorAll('.color'); // atribui um array com as divs da paleta de cores
+const cores = document.querySelectorAll('.color'); 
 const pixels = document.getElementsByClassName('pixel'); 
 const button = document.getElementById('clear-board'); 
-const vqvBtt = document.getElementById('generate-board'); 
-const divisaoTop = document.getElementById('pixel-board'); 
+const criarBtt = document.getElementById('generate-board'); 
+const divTop = document.getElementById('pixel-board'); 
 const inputUser = document.querySelector('#board-size');
 
-//função selecionar cor
-function selecColor(event) {
-  document.querySelector('.selected').classList.remove('selected');
-  event.target.classList.add('selected');
-}
-const cores = document.querySelectorAll('.color');
-for (let i = 0; i < cores.length; i += 1){
-  cores [i].addEventListener('click', selecColor);
-}
-//função troca cor
-const pixels = document.getElementsByClassName('pixel');
-for (let i = 0; i < pixels.length; i += 1) {
-  pixels[i].addEventListener('click', () => {
-    const newColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
-    pixels[i].tsyle.backgroundColor = corNova;
-  })
-}
-// limpa cor pixel
-function clean() {
-  for (let i = 0; i < pixels.length; i += 1) {
-    document.getElementsByClassName('pixel')[i].style.backgroundColor = 'rgb(255,255,255)';
-  }
-}
-//randomiza as cores dos pixels
+// randomiza cores a ser selecionada
 document.getElementsByClassName('color')[0].style.backgroundColor = 'rgb(0,0,0)';
 function corRandom() {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
-
+  
   return `rgb(${r}, ${g}, ${b})`;
 }
 for (let i = 1; i < cores.length; i += 1) {
   document.getElementsByClassName('color')[i].style.backgroundColor = corRandom();
 }
-//criando tables
-function criarTable() {
+// cria a grid de pixel
+function criarGrid() {
   const tagTable = document.createElement('table'); // <table> <table/>
+  // declara a div que vamos usar
+  divTop.appendChild(tagTable);
+  for (let n = 1; n <= tamanhoGrid; n += 1) {
+    const tagTR = document.createElement('tr');
+  for (let i = 0; i < cores.length; i += 1) {
+      cores[i].addEventListener('click', selecionaCor);
+    }
+  }
 }
+// print a grade
+function printPixels() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', () => {
+      const corNova = document.getElementsByClassName('selected')[0].style.backgroundColor;
+      pixels[i].style.backgroundColor = corNova;
+    });
+  }
+}
+printPixels();
+
+// muda a grade de acordo com o input
+function criarGridValue() {
+  tamanhoGrid = inputUser.value;
+  criarGrid();
+}
+
+// limpar grid da grid
+function limpaGrid() {
+  divTop.innerHTML = '';
+
+}
+// limpa cor grid
+function clearBtt() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    document.getElementsByClassName('pixel')[i].style.backgroundColor = 'rgb(255,255,255)';
+  }
+}
+
+button.addEventListener('click', clearBtt);
+criarBtt.addEventListener('click', limpaGrid);
+criarBtt.addEventListener('click', verificaInput);
+criarBtt.addEventListener('click', verificaGridValeu);
+criarBtt.addEventListener('click', criarGridValue);
