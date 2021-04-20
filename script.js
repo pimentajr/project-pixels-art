72e1240c034136cd7778665428ddbc86cd4df3fe
JoaoCabrera <joaocabrerati@gmail.com>
2021-04-20 00:55:13 -0300

    Resolvendo requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let pixelBoard = document.querySelector('.pixel-board');
 let linesBoard = document.querySelectorAll('.tr');
 let allPixels = document.querySelectorAll('.pixel');
+let colorPalette = document.getElementById('color-palette');
+let paletteItens = document.querySelectorAll('.color')
 
 window.onload = function () {
   let blackColor = document.getElementById('color1');
   blackColor.classList.add('selected');
 }
+
+colorPalette.addEventListener('click', function(event) {
+  let colorSelected = document.querySelector('.selected');
+  colorSelected.classList.remove('selected');
+  for (let i = 0; i < paletteItens.length; i += 1) {
+    event.target.classList.add('selected')
+  }
+});
