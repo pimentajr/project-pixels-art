120e1b922f3e45f9d955858e6f02f086714dccac
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-04-19 23:07:20 -0300

    fix 26

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ document.getElementById('red').style.backgroundColor = 'red';
 document.getElementById('blue').style.backgroundColor = 'blue';
 document.getElementById('yellow').style.backgroundColor = 'yellow';
 
-const captureClassColor= document.querySelectorAll('.color');
-for (index= 0; index < captureClassColor.lenght; index += 1) {
+const captureClassColor = document.querySelectorAll('.color');
+for (index = 0; index < captureClassColor.lenght; index += 1) {
     let liColor = captureClassColor[index]
     liColor.addEventListener('click', selectedClass);
-    function selectedClass (event) {
+    // eslint-disable-next-line no-inner-declarations
+    function selectedClass(event) {
         if (event.target.classList.contains ('selected')) {
             event.target.classList.remove('selected');
+        } else {
             event.target.classList.add('selected');
         }
     }
