94bfc637e5f49a4ef39939c845165ef3d7528cb6
JoaoCabrera <joaocabrerati@gmail.com>
2021-04-20 00:25:40 -0300

    Resolve requisito 6

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-window.onload = function firstload () {
-  document.getElementById('color1').classList.add('selected');
+let pixelBoard = document.querySelector('.pixel-board');
+let linesBoard = document.querySelectorAll('.tr');
+let allPixels = document.querySelectorAll('.pixel');
+
+window.onload = function () {
+  let blackColor = document.getElementById('color1');
+  blackColor.classList = 'selected';
 }
-firstload();
