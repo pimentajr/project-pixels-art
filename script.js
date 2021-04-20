70664e4f4108e7ccf682b7bc8790cd00de58662a
IgorMendez <igor.emp@hotmail.com>
2021-04-20 09:34:32 -0300

    Exercício 10

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const colorSelected = document.getElementsByClassName('color selected');
 const pixelBoard = document.getElementById('pixel-board');
 const clearButtom = document.getElementById('clear-board');
 const pixelGet = document.getElementsByClassName('pixel');
+const generateBoard = document.getElementById('generate-board');
+const inputButton = document.getElementById('board-size');
+inputButton.value = 5;
 
 // Functions
 function clearPage() {
@@ -27,7 +30,7 @@ function setClassColorPallet({ target }) {
 }
 
 function setColor(e) {
-  if (e.target.id !== 'pixel-board') {
+  if (e.target.classList[0] === 'pixel') {
     e.target.style.backgroundColor = colorSelected[0].style.backgroundColor;
   }
 }
@@ -36,19 +39,43 @@ for (let index = 0; index < colors.length; index += 1) {
   colors[index].style.backgroundColor = colors[index].id;
 }
 
-(function createPixel() {
-  for (let index = 0; index < 5; index += 1) {
+function removePixels() {
+  pixelBoard.innerText = '';
+}
+
+function createPixel() {
+  removePixels();
+  for (let index = 0; index < inputButton.value; index += 1) {
     const createDivPixelRow = document.createElement('tr');
     pixelBoard.appendChild(createDivPixelRow);
     createDivPixelRow.classList.add('pixel-row');
-    for (let j = 0; j < 5; j += 1) {
+    for (let j = 0; j < inputButton.value; j += 1) {
       const createDivPixelColum = document.createElement('td');
       createDivPixelRow.appendChild(createDivPixelColum);
       createDivPixelColum.classList.add('pixel', 'white');
     }
   }
-}());
+}
 
+function conditionalSize() {
+  if (inputButton.value < 8) {
+    createPixel();
+  }
+}
+
+function resizeBoard() {
+  if (inputButton.value === '') {
+    alert('Board inválido!');
+  } else {
+    conditionalSize();
+  }
+}
+
+window.onload = function onlo() {
+  resizeBoard();
+};
+
+generateBoard.addEventListener('click', resizeBoard);
 pixelBoard.addEventListener('click', setColor);
 colorPalette.addEventListener('click', setClassColorPallet);
 clearButtom.addEventListener('click', clearPage);
