d408ce86df0f52395419561e8c488a3112d4a414
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-04-20 00:16:40 -0300

    fix 40

--- script.js
+++ script.js
@@ -1,64 +1,28 @@
-function criarCores() {
-    const pai = document.getElementById('color-palette');
-  
-    for (i = 0; i < 4; i += 1) {
-      const filho = document.createElement('li');
-      filho.className = 'color';
-      pai.appendChild(filho);
+document.getElementById("black").style.backgroundColor = 'black';
+document.getElementById("red").style.backgroundColor = 'red';
+document.getElementById("blue").style.backgroundColor = 'blue';
+document.getElementById("yellow").style.backgroundColor = 'yellow';
+
+const colorPalette = document.querySelector('#color-palette');
+const pixelBoard = document.querySelector('#pixel-board');
+
+let savedColor = 'black';
+function selectionColor(color) {
+    const colorTarget = color.target;
+    if (colorTarget.classList.contains('color')) {
+        const removeColor = document.querySelector('.selected');
+        removeColor.classList.remove('selected');
+
+        savedColor = colorTarget.style.backgroundColor.classList.add('selected');
     }
-    const cor = document.getElementsByClassName('color');
-    const black = cor[0];
-    const green = cor[1];
-    const red = cor[2];
-    const blue = cor[3];
-   
-  
-    black.style.backgroundColor = 'black';
-    green.style.backgroundColor = 'green';
-    red.style.backgroundColor = 'red';
-    blue.style.backgroundColor = 'blue';
-  }
-  criarCores();
-  
-  function criarQuadro() {
-    for (let i = 0; i < 5; i += 1) {
-      for (let j = 0; j < 5; j += 1) {
-        const filho = document.createElement('li');
-        filho.className = 'pixel';
-        document.getElementById('pixel-board').appendChild(filho);
-      }
+}
+
+function setColor(color) {
+    const newColor = color.target;
+    if (newColor.className === 'pixel') {
+        newColor.style.backgroundColor = savedColor;
     }
-  }
-  criarQuadro();
-  
-  function CorPretaSelected() {
-    const blackColor = document.getElementsByClassName('color')[0];
-    blackColor.className = 'color selected';
-  }
-  CorPretaSelected();
-  
-  function selectedClass() {
-    const pai = document.getElementById('color-palette');
-    pai.addEventListener('click', function(event) {
-      const filho = document.getElementsByClassName('color');
-      for (i = 0; i < filho.length; i += 1) {
-        if(filho[i].className !== 'color') {
-          filho[i].className = 'color';
-        }
-        event.target.className = 'color selected';
-      }
-    })
-  }
-  selectedClass()
-  
-  function pintarPixeis () {
-    let pai = document.getElementsByClassName('pixel')
-  
-    for (let i = 0; i < pai.length; i += 1) {
-      pai[i].addEventListener('click',function() {
-        let cor = document.querySelector('.selected')
-          pai[i].style.backgroundColor = window.getComputedStyle(cor).getPropertyValue('background-color');
-      })
-    }
-  }
-  pintarPixeis ();
+}
+
+window.onload = selectingColor;
+colorPalette.addEventListener('click', selectingColor)
