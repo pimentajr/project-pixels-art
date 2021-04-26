5e692400376347a0d09173ed5ed93b939b323013
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-26 18:23:45 -0300

    100% + LINT

--- script.js
+++ script.js
@@ -1,123 +1,97 @@
-function showColor(){
-    console.log(corSelecionada);
+const black = document.getElementById('black');
+const red = document.getElementById('cor2');
+const blue = document.getElementById('cor3');
+const green = document.getElementById('cor4');
+
+// Cores randomicas 1 até 256
+const random2 = Math.floor((Math.random() * 30)); // 2
+const random3 = Math.ceil((Math.random() * 150)); // 3
+const random4 = Math.floor((Math.random() * 300)); // 4
+const randomString2 = random2.toString(); // 2
+const randomString3 = random3.toString(); // 3
+const randomString4 = random4.toString(); // 4
+
+red.style.backgroundColor = `rgb(${randomString2},${randomString3},${randomString4})`; // 2
+blue.style.backgroundColor = `rgb(${randomString3},${randomString2},${randomString4})`; // 3
+green.style.backgroundColor = `rgb(${randomString4},${randomString3},${randomString2})`; // 4
+let corSelecionada = 'black';
+const classeColorSelected = 'color selected';
+
+black.addEventListener('click', () => {
+  black.className = classeColorSelected;
+  red.className = 'color';
+  blue.className = 'color';
+  green.className = 'color';
+  corSelecionada = 'black';
+});
+
+red.addEventListener('click', () => {
+  red.className = classeColorSelected;
+  black.className = 'color';
+  blue.className = 'color';
+  green.className = 'color';
+  corSelecionada = `rgb(${randomString2},${randomString3},${randomString4})`;
+});
+
+blue.addEventListener('click', () => {
+  red.className = 'color';
+  black.className = 'color';
+  blue.className = classeColorSelected;
+  green.className = 'color';
+  corSelecionada = `rgb(${randomString3},${randomString2},${randomString4})`;
+});
+
+green.addEventListener('click', () => {
+  red.className = 'color';
+  black.className = 'color';
+  blue.className = 'color';
+  green.className = classeColorSelected;
+  corSelecionada = `rgb(${randomString4},${randomString3},${randomString2})`;
+});
+
+// const selectedColor = document.getElementsByClassName('color selected');
+const changeColor = document.querySelector('#pixel-board');
+
+changeColor.addEventListener('click', (e) => {
+  e.target.style.backgroundColor = corSelecionada;
+});
+
+// Botão Limpar
+const limparButton = document.getElementById('clear-board');
+limparButton.addEventListener('click', () => {
+  const capturarPixels = document.getElementsByClassName('pixel');
+  for (let i = 0; i < capturarPixels.length; i += 1) {
+    capturarPixels[i].style.backgroundColor = 'white';
+  }
+});
+
+const numeroNovosPixels = document.getElementById('board-size');
+const buttonNovosPixels = document.getElementById('generate-board');
+const paizão = document.getElementById('pixel-board');
+const remover1 = document.getElementById('linha1');
+const remover2 = document.getElementById('linha2');
+const remover3 = document.getElementById('linha3');
+const remover4 = document.getElementById('linha4');
+const remover5 = document.getElementById('linha5');
+
+function createNewPixels(tamanho) {
+  for (let i = 1; i <= tamanho; i += 1) {
+    for (let j = 1; j <= tamanho; j += 1) {
+      const novosPixels = document.createElement('div'); novosPixels.className = 'pixel';
+      novosPixels.style.backgroundColor = 'white'; novosPixels.style.display = 'inline-block';
+      novosPixels.style.height = '40px'; novosPixels.style.width = '40px';
+      paizão.appendChild(novosPixels);
+    }
+    const mudarLinha = document.createElement('br'); paizão.appendChild(mudarLinha);
+  }
 }
 
-let black = document.getElementById('black');
-
-let red = document.getElementById('cor2');
-let blue = document.getElementById('cor3');
-let green = document.getElementById('cor4');
-
-let cores = document.getElementById('color-palette');
-
-//Cores randomicas 1 até 256
-let random2 = Math.floor((Math.random()*30)); //2
-let random3 = Math.ceil((Math.random()*150)); //3
-let random4 = Math.floor((Math.random()*300)); //4
-let randomString2 = random2.toString(); //2
-let randomString3 = random3.toString(); //3
-let randomString4 = random4.toString(); //4
-red.style.backgroundColor = 'rgb('+randomString2+','+randomString3+','+randomString4+')'; //2
-blue.style.backgroundColor = 'rgb('+randomString3+','+randomString2+','+randomString4+')'; //3
-green.style.backgroundColor = 'rgb('+randomString4+','+randomString3+','+randomString2+')'; //4
-
-let corSelecionada = 'black';
-
-black.addEventListener('click', function(event){
-    black.className = 'color selected';
-    red.className = 'color';
-    blue.className = 'color';
-    green.className = 'color';
-    corSelecionada = 'black';
-    showColor();
+buttonNovosPixels.addEventListener('click', () => {
+  remover1.parentNode.removeChild(remover1); remover2.parentNode.removeChild(remover2);
+  remover3.parentNode.removeChild(remover3); remover4.parentNode.removeChild(remover4);
+  remover5.parentNode.removeChild(remover5); let tamanho = parseInt(numeroNovosPixels.value, 10);
+  if (tamanho < 5) { tamanho = 5; }
+  if (tamanho > 50) { tamanho = 50; }
+  if (numeroNovosPixels.value === '') { alert('Board inválido!'); }
+  createNewPixels(tamanho);
 });
-
-red.addEventListener('click', function(event){
-    red.className = 'color selected';
-    black.className = 'color';
-    blue.className = 'color';
-    green.className = 'color';
-    corSelecionada = 'rgb('+randomString2+','+randomString3+','+randomString4+')';
-    showColor();
-});
-
-blue.addEventListener('click', function(event){
-    red.className = 'color';
-    black.className = 'color';
-    blue.className = 'color selected';
-    green.className = 'color';
-    corSelecionada = 'rgb('+randomString3+','+randomString2+','+randomString4+')';
-    showColor();
-});
-
-green.addEventListener('click', function(event){
-    red.className = 'color';
-    black.className = 'color';
-    blue.className = 'color';
-    green.className = 'color selected';
-    corSelecionada = 'rgb('+randomString4+','+randomString3+','+randomString2+')';
-    showColor();
-});
-
-let selectedColor = document.getElementsByClassName('color selected');
-let changeColor = document.querySelector('#pixel-board');
-
-changeColor.addEventListener('click', function(event){
-    event.target.style.backgroundColor = corSelecionada;
-});
-
-//Botão Limpar
-let limparButton = document.getElementById('clear-board');
-limparButton.addEventListener('click', function(){
-    let capturarPixels = document.getElementsByClassName('pixel');
-    for(let i=0;i<capturarPixels.length;i++){
-        capturarPixels[i].style.backgroundColor='white';
-    }
-});
-
-//Botão gerar novos pixel 
-// pais a serem destruídos
-//<div id='linha1'></div>
-//<div id='linha2'></div>
-//<div id='linha3'></div>
-//<div id='linha4'></div>
-//<div id='linha5'></div>
-let numeroNovosPixels = document.getElementById('board-size');
-let buttonNovosPixels = document.getElementById('generate-board');
-let paizão = document.getElementById('pixel-board');
-buttonNovosPixels.addEventListener('click', function(){
-    let remover1 = document.getElementById('linha1');
-    remover1.parentNode.removeChild(remover1);
-    let remover2 = document.getElementById('linha2');
-    remover2.parentNode.removeChild(remover2);
-    let remover3 = document.getElementById('linha3');
-    remover3.parentNode.removeChild(remover3);
-    let remover4 = document.getElementById('linha4');
-    remover4.parentNode.removeChild(remover4);
-    let remover5 = document.getElementById('linha5');
-    remover5.parentNode.removeChild(remover5);   
-    let tamanho = parseInt(numeroNovosPixels.value);
-    if(numeroNovosPixels.value<5){
-        tamanho=5;
-    }
-    if(numeroNovosPixels.value>50){
-        tamanho=50;
-    }
-    if(tamanho < 0 || tamanho > 50 || tamanho < 5 || numeroNovosPixels.value == ''){
-        alert('Board inválido!');
-    }else{
-        for(let i=1;i<=tamanho;i++){
-            for(let j=1;j<=tamanho;j++){
-                let novosPixels = document.createElement('div');
-                novosPixels.className='pixel';
-                novosPixels.style.backgroundColor='white';
-                novosPixels.style.display='inline-block';
-                novosPixels.style.height='40px';
-                novosPixels.style.width='40px';
-                paizão.appendChild(novosPixels);
-            }
-            let mudarLinha = document.createElement('br');
-            paizão.appendChild(mudarLinha);
-        }
-    }
-}); 
