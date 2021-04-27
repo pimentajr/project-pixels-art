a77c5feaaaee2e05d1d3b1f6fdb71c6c96a2ef6f
Caio-Veloso <caio.veloso@gmail.com>
2021-04-27 12:23:48 -0300

    Questao 6

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-window.addEventListener('load', function() {
-let colorBlack = document.querySelector('.selected');
-});
+window.onload = function selectBlackColor() {
+  document.querySelector('#black').classList.add('selected');
+};
+
+function setClassSelectd(event) {
+  let colorsSelected = document.querySelectorAll('.selected');
+  for (let index = 0; index < colorsSelected.length; index += 0) {
+    colorsSelected[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+setClassSelectd();
