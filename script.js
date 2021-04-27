964c18392989aa9414384ed4c1820251eab7fd7d
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-04-27 01:54:41 -0400

    testes para o desafio 7

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const startColor = document.getElementById('black');
 startColor.classList.add('selected');
 
-console.log(startColor);
+// atribuir a clase selected a cor que for clicada
+// criar um evento de click para identificar de onde veio o click
+
+let clickTest = document.getElementsByClassName('color');
+
+function listenerFun (event) {
+  for (let index = 0; index < clickTest.length; index += 1) {
+    clickTest[index].addEventListener("click", listenerFun);
+  }
+}
+console.log(listenerFun());
