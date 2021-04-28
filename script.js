dbd084d2e6a0655f4c318e43777c34dd84a4aae8
Xenon-Neko <darkankher@gmail.com>
2021-04-28 02:00:04 -0300

    Projeto,pronto para push

---
+++ script.js
@@ -0,0 +1,124 @@
+//Declaração das constantes que serão utilizadas.
+
+const paletteColor = document.querySelector('#palette');
+const quadroPixel = document.querySelector('#board');
+const tamanhoQuadro = document.getElementById('size');
+const botaoGerar = document.getElementById('generate');
+const botaoLimpar = document.querySelector('#clear');
+
+//Função para criar a paleta de cores.
+
+function criaPaleta() {
+    const cores = color();
+    let index = ' ';
+    for (index = 0; index < cores.length; index += 1) {
+      const paleta = document.createElement('div');
+      paleta.style.backgroundColor = cores[index];
+      paleta.classList.add('color');
+      if (index === 0) {
+        paleta.classList.add('selected');
+      }
+      paletteColor.appendChild(paleta);
+    }
+}
+criaPaleta();
+
+//Função para gerar um número aleatório que será utilizado para gerar um rgb aleatório.
+
+function geraNumero() {
+  return Math.floor(Math.random() * 256);
+}
+
+//Função para gerar o rgb aleatório.
+
+function geraRgb() {
+  const red = geraNumero();
+  const green = geraNumero();
+  const blue = geraNumero();
+  return `rgb( ${red} , ${green} , ${blue})`;
+}
+
+//Função que adiciona o rgb aleatorio aos blocos da paleta de cores.
+
+function color() {
+  const colors = ['black'];
+  while (colors.length < 4) {
+    colors.push(geraRgb());
+  }
+  return colors;
+}
+
+//Função que gera o quadro de pixels.
+
+function quadroPixels(capacidade) {
+    let linhas = ' ';
+    for (linhas = 0; linhas < capacidade; linhas += 1) {
+      const quadro = document.createElement('div');
+      quadro.classList.add('linhas');
+      quadroPixel.appendChild(quadro);
+      let colunas = ' ';
+      for (colunas = 0; colunas < capacidade; colunas += 1) {
+        const pixel = document.createElement('div');
+        quadro.appendChild(pixel);
+        pixel.className = 'pixel';
+      }
+    }
+}
+quadroPixels(5);
+
+//Função que muda a cor selecionada na paleta.
+
+let cor = 'black';
+function mudaPaleta(color) {
+  const remover = document.getElementsByClassName('selected');
+let index = ' ';
+  for (index = 0; index < remover.length; index += 1) {
+    remover[index].classList.remove('selected');
+}
+  const selected = color.target;
+  cor = selected.style.backgroundColor;
+  selected.classList.add('selected');
+}
+paletteColor.addEventListener('click', mudaPaleta);
+
+//Função que colore  pixel com a cor seleciona anteriormente.
+
+function colorir(event) {
+  event.target.style.backgroundColor = cor;
+}
+quadroPixel.addEventListener('click', colorir);
+
+//Função que redefine o quadro de pixels para sua cor original, inteiramente branco.
+
+function redefinir() {
+  const pixels = document.getElementsByClassName('pixel');
+
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
+botaoLimpar.addEventListener('click', redefinir);
+
+//Função que define o número mínimo e máximo de colunas X linhas no quadro de pixels.
+
+function minSizeAndMaxSize(tamanho) {
+  if (tamanho < 5 && tamanho > 0) {
+    tamanho = tamanho;
+  }
+  if (tamanho > 50) {
+    tamanho = 50;
+  }
+  return tamanho;
+}
+
+//Função que gera a tabela através do número digitado pelo usuário.
+
+function gerarTabela() {
+  const tamanho = tamanhoQuadro.value;
+  if (!tamanho) {
+    return alert('Seleção de tamanho inválida!');
+  }
+  const  tamanhoDefinido= minSizeAndMaxSize(tamanho);
+  quadroPixel.innerHTML = '';
+  return quadroPixels(tamanhoDefinido);
+}
